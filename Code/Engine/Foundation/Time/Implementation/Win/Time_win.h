878f431202544b45a474f8439b665cc6392c232b
jankrassnigg <jan@krassnigg.de>
2013-11-07 18:15:39 +0000

    Fixed potential race condition in ezSystemTime::Now().

--- Code/Engine/Foundation/Time/Implementation/Win/Time_win.h
+++ Code/Engine/Foundation/Time/Implementation/Win/Time_win.h
@@ -16,12 +16,15 @@ void ezSystemTime::Shutdown()
 
 ezTime ezSystemTime::Now()
 {
-  LARGE_INTEGER temp;
+#if EZ_ENABLED(EZ_COMPILE_FOR_DEVELOPMENT)
+  volatile LARGE_INTEGER LastTime = s_LastTime;
+#endif
 
+  LARGE_INTEGER temp;
   QueryPerformanceCounter(&temp);
 
 #if EZ_ENABLED(EZ_COMPILE_FOR_DEVELOPMENT)
-  EZ_ASSERT(s_LastTime.QuadPart <= temp.QuadPart, "Serious problem, Steve. This is like \"Houston, forget that other thing\".\n\n\n"
+  EZ_ASSERT(LastTime.QuadPart <= temp.QuadPart, "Serious problem, Steve. This is like \"Houston, forget that other thing\".\n\n\n"
     "When this happens the PC timer is unreliable. It was probably called from different threads and the clocks on different CPU cores seem to return different results.\n"
     "Under these conditions the engine cannot run reliably, it might crash or act weird.");
   s_LastTime = temp;
