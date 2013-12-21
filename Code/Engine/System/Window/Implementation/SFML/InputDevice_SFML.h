9e1ea70bc863955bbf53610d87cd8d14602814fd
jankrassnigg <jan@krassnigg.de>
2013-12-21 23:05:50 +0000

    Finished the SFML port on Windows.
    The link libraries for MacOS and Linux still need to be set up. Then it should work on all platforms.

---
+++ Code/Engine/System/Window/Implementation/SFML/InputDevice_SFML.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <System/Basics.h>
+#include <SFML/Window.hpp>
+#include <Core/Input/DeviceTypes/MouseKeyboard.h>
+
+/// \brief Implements an input device abstraction on top of the SFML library.
+///
+/// Unfortunately SFML does neither support relative mouse motion, nor clipping a mouse cursor to the window area.
+/// Thus relative and absolute mouse position are 'emulated' by this device. As a consequence, relative mouse motion
+/// only works without limits, when the mouse cursor is clipped (in that case it is reset to the window center after each
+/// movement).
+///
+/// SFML has several other limitations, as well. For example keyboard input is not language setting independent.
+/// To see the effect, just try out what different things happen when you use an English, German and Russion language setting :-(
+/// Therefore, it is highly advised to make all key-mappings configurable, such that users with other language settings can
+/// 'fix' this on their end.
+///
+/// Also many special keys are not properly supported by SFML, most notably Numpad Enter is handled like Return.
+class EZ_SYSTEM_DLL ezStandardInputDevice : public ezInputDeviceMouseKeyboard
+{
+public:
+  ezStandardInputDevice(sf::Window* pWindow, ezUInt32 uiWindowNumber);
+  ~ezStandardInputDevice();
+
+  /// \brief Returns an ezStandardInputDevice device for the given window.
+  static ezStandardInputDevice* GetDevice(ezUInt32 uiWindowNumber = 0);
+
+  /// \brief This function needs to be called whenever an SFML event arrives, such that the input device can handle it.
+  void WindowMessage(const sf::Event& TheEvent);
+
+  /// \brief Destroys all devices of this type. Automatically called at engine shutdown.
+  static void DestroyAllDevices();
+
+  /// \brief Returns "MouseKeyboardSFML".
+  virtual const char* GetDeviceName() const { return "MouseKeyboardSFML"; }
+
+  /// \brief Will trap the mouse inside the application window. Should usually be enabled, to prevent accidental task switches.
+  ///
+  /// Especially on multi-monitor systems, the mouse can easily leave the application window (even in fullscreen mode).
+  /// Do NOT use this function when you have multiple windows and require absolute mouse positions.
+  ///
+  /// \note Unfortunately the SFML implementation has some restrictions regarding relative and absolute mouse positions.
+  /// Therefore when the mouse cursor is clipped to the window area, it is actually always centered in the window.
+  /// This enables relative mouse motions without limits (ie. the cursor cannot reach the screen border, thus it can be moved
+  /// indefinitely). On the other hand, it does not make much sense to show the mouse cursor and have it clipped, as the
+  /// actual OS cursor position will be quite useless. However, ez then 'emulates' the absolute cursor position, thus
+  /// retrieving the absolute mouse position from the input device still works as expected.
+  ///
+  /// If, however, you need to allow the user to move the mouse cursor outside the window, be aware that the relative mouse
+  /// motions will stop working, once the mouse cursor reaches the screen borders.
+  void SetClipMouseCursor(bool bEnable);
+
+  /// \brief Returns whether the mouse is confined to the application window or not.
+  bool GetClipMouseCursor() const { return m_bClipCursor; }
+
+  /// \brief Shows or hides the mouse cursor inside the application window.
+  ///
+  /// \note When SetClipMouseCursor is enabled, the mouse will always be centered in the window.
+  /// In that case it should also always be hidden.
+  virtual void SetShowMouseCursor(bool bShow);
+
+  /// \brief Returns whether the mouse cursor is shown.
+  virtual bool GetShowMouseCursor() const { return m_bShowCursor; }
+
+private:
+  virtual void InitializeDevice() EZ_OVERRIDE { }
+  virtual void UpdateInputSlotValues() EZ_OVERRIDE;
+  virtual void RegisterInputSlots() EZ_OVERRIDE;
+  virtual void ResetInputSlotValues() EZ_OVERRIDE;
+
+  static bool s_bMainWindowUsed;
+
+  sf::Window* m_pWindow;
+
+  ezUInt32 m_uiWindowNumber;
+  ezVec2I32 m_vEmulatedMousePos;
+  ezVec2I32 m_vLastMousePos;
+
+  bool m_bShowCursor;
+  bool m_bClipCursor;
+
+  void UpdateMouseCursor();
+};
+
+
--- Code/Engine/System/Window/Implementation/SFML/Window_SFML.inl
+++ Code/Engine/System/Window/Implementation/SFML/Window_SFML.inl
@@ -1,14 +1,135 @@
 
 #include <System/PCH.h>
 #include <System/Window/Window.h>
+#include <Foundation/Math/Size.h>
 
 
 ezResult ezWindow::Initialize()
 {
-  return EZ_FAILURE;
+  EZ_LOG_BLOCK("ezWindow");
+
+  if (m_bInitialized)
+    Destroy();
+
+  EZ_ASSERT_API(m_CreationDescription.m_ClientAreaSize.HasNonZeroArea(), "The client area size can't be zero sized!");
+
+  m_WindowHandle = EZ_DEFAULT_NEW(sf::Window);
+
+  sf::VideoMode Mode;
+  Mode.bitsPerPixel = 32;
+  Mode.width  = m_CreationDescription.m_ClientAreaSize.width;
+  Mode.height = m_CreationDescription.m_ClientAreaSize.height;
+
+  sf::Uint32 uiStyle = 0;
+
+  if (m_CreationDescription.m_bFullscreenWindow)
+    uiStyle |= sf::Style::Fullscreen;
+  else
+  if (m_CreationDescription.m_bResizable)
+    uiStyle |= sf::Style::Resize;
+  else
+    uiStyle |= sf::Style::Titlebar;
+
+  sf::ContextSettings context;
+  context.depthBits = 24;
+  context.stencilBits = 8;
+  context.majorVersion = 4;
+  context.minorVersion = 0;
+  context.antialiasingLevel = 0;
+
+  m_WindowHandle->create(Mode, m_CreationDescription.m_Title.GetData(), uiStyle, context);
+  
+  m_bInitialized = true;
+  ezLog::Success("Created window successfully.");
+
+  m_pInputDevice = EZ_DEFAULT_NEW(ezStandardInputDevice)(m_WindowHandle, m_CreationDescription.m_uiWindowNumber);
+
+  // make sure the system knows about the actual window dimensions (especially for fullscreen windows)
+  OnResizeMessage(ezSizeU32(m_WindowHandle->getSize().x, m_WindowHandle->getSize().y));
+
+  return CreateGraphicsContext();
 }
 
 ezResult ezWindow::Destroy()
 {
+  EZ_DEFAULT_DELETE(m_pInputDevice);
+
+  DestroyGraphicsContext();
+
+  m_WindowHandle->close();
+
+  EZ_DEFAULT_DELETE(m_WindowHandle);
+  m_WindowHandle = INVALID_WINDOW_HANDLE_VALUE;
+
+  m_bInitialized = false;
+
+  ezLog::Success("Window destroyed.");
+  return EZ_SUCCESS;
+
+}
+
+ezWindow::WindowMessageResult ezWindow::ProcessWindowMessages()
+{
+  if (!m_bInitialized)
+    return Quit;
+
+  sf::Event event;
+  while (m_WindowHandle->pollEvent(event))
+  {
+    switch (event.type)
+    {
+    case sf::Event::Closed:
+      Destroy();
+      return Quit;
+
+    case sf::Event::Resized:
+      OnResizeMessage(ezSizeU32(event.size.width, event.size.height));
+      break;
+
+    case sf::Event::LostFocus:
+      OnFocusMessage(false);
+      break;
+
+    case sf::Event::GainedFocus:
+      OnFocusMessage(true);
+      break;
+    }
+
+    m_pInputDevice->WindowMessage(event);
+  }
+
+  return Continue;
+}
+
+void ezWindow::PresentFrame()
+{
+  m_WindowHandle->display();
+}
+
+ezResult ezWindow::CreateGraphicsContext()
+{
+  switch (m_CreationDescription.m_GraphicsAPI)
+  {
+  case ezGraphicsAPI::OpenGL:
+    return EZ_SUCCESS;
+
+  default:
+    EZ_REPORT_FAILURE("Unknown Graphics API selected.");
+  }
+
+  return EZ_FAILURE;
+}
+
+ezResult ezWindow::DestroyGraphicsContext()
+{
+  switch (m_CreationDescription.m_GraphicsAPI)
+  {
+  case ezGraphicsAPI::OpenGL:
+    return EZ_SUCCESS;
+
+  default:
+    EZ_REPORT_FAILURE("Unknown Graphics API selected.");
+  }
+
   return EZ_FAILURE;
 }
