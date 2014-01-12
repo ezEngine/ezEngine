286b8a7c9eb120ddaf456291284b68c19488ac4b
marcappelsmeier <derpudeldestodes@gmail.com>
2014-01-12 14:58:06 +0000

    - updated GPA libraries / header files to 2013 R4 (current release)

--- Code/ThirdParty/gpa_ITT/include/gpa_api.h
+++ Code/ThirdParty/gpa_ITT/include/gpa_api.h
@@ -1,23 +1,23 @@
 /******************************************************************************
 
-                              INTEL CONFIDENTIAL
-           Copyright (C) 2011 Intel Corporation All Rights Reserved. 
+Copyright 2011 Intel Corporation All Rights Reserved.
 
- The source code contained or described herein and all documents related to the
- source code ("Material") are owned by Intel Corporation or its suppliers or
- licensors. Title to the Material remains with Intel Corporation or its 
- suppliers and licensors. The Material contains trade secrets and proprietary 
- and confidential information of Intel or its suppliers and licensors. The 
- Material is protected by worldwide copyright and trade secret laws and treaty 
- provisions. No part of the Material may be used, copied, reproduced, modified, 
- published, uploaded, posted, transmitted, distributed, or disclosed in any way 
- without Intel’s prior express written permission.
+The source code, information and material ("Material") contained herein is owned 
+by Intel Corporation or its suppliers or licensors, and title to such Material 
+remains with Intel Corporation or its suppliers or licensors. The Material 
+contains proprietary information of Intel or its suppliers and licensors. The 
+Material is protected by worldwide copyright laws and treaty provisions. No part 
+of the Material may be used, copied, reproduced, modified, published, uploaded, 
+posted, transmitted, distributed or disclosed in any way without Intel's prior 
+express written permission. No license under any patent, copyright or other 
+intellectual property rights in the Material is granted to or conferred upon you, 
+either expressly, by implication, inducement, estoppel or otherwise. Any license 
+under such intellectual property rights must be express and approved by Intel 
+in writing.
 
- No license under any patent, copyright, trade secret or other intellectual 
- property right is granted to or conferred upon you by disclosure or delivery 
- of the Materials, either expressly, by implication, inducement, estoppel or 
- otherwise. Any license under such intellectual property rights must be 
- express and approved by Intel in writing.
+Unless otherwise agreed by Intel in writing, you may not remove or alter this 
+notice or any other notice embedded in Materials by Intel or Intel’s suppliers 
+or licensors in any way
 
 ******************************************************************************/
 
