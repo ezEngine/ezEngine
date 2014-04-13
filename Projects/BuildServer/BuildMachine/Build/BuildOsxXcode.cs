2704818f59255822454b89639b6c79d014184801
Sanakan8472 <christopher.manthei@gmail.com>
2014-04-13 08:59:53 +0000

    * BuildMachine: Simple OSX output parser to remove bloat.

--- Projects/BuildServer/BuildMachine/Build/BuildOsxXcode.cs
+++ Projects/BuildServer/BuildMachine/Build/BuildOsxXcode.cs
@@ -23,6 +23,7 @@ namespace BuildMachine
 
       res.ProcessRes = ezProcessHelper.RunExternalExe("xcodebuild", string.Format("-configuration RelWithDebInfo -target {0}", target.Name), sAbsWorkingDir, res);
       res.Success = (res.ProcessRes.ExitCode == 0);
+      CleanUpXcodeOutput(res.ProcessRes);
       return res;
     }
 
@@ -31,5 +32,37 @@ namespace BuildMachine
       ezProcessHelper.ProcessResult res = ezProcessHelper.RunExternalExe("xcodebuild", "-alltargets clean", sAbsWorkingDir, null);
       return res.ExitCode == 0;
     }
+
+    private void CleanUpXcodeOutput(ezProcessHelper.ProcessResult processRes)
+    {
+      if (processRes == null || String.IsNullOrEmpty(processRes.StdOut))
+        return;
+
+      List<string> newLines = new List<string>();
+      string[] lines = processRes.StdOut.Split('\n');
+      int iLineCount = lines.Count();
+      for (int i = 0; i < iLineCount; )
+      {
+        // Skip the mega verbose output of CompileC.
+        if ((i + 3 < iLineCount) &&
+          lines[i + 0].StartsWith("CompileC") &&
+          lines[i + 1].StartsWith("    cd") &&
+          lines[i + 2].StartsWith("    setenv") &&
+          lines[i + 3].StartsWith("    /"))
+        {
+          newLines.Add(lines[i]);
+          i = i + 4;
+        }
+        else
+        {
+          newLines.Add(lines[i]);
+          i++;
+        }
+      }
+
+      string newStdOut = string.Join("\n", newLines);
+      processRes.StdOut = newStdOut;
+    }
+
   }
 }
