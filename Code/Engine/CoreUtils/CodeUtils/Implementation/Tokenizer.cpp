cf4b9312562dcb66ad1c699111f346817cb1f381
jankrassnigg <jan@krassnigg.de>
2014-04-26 11:34:57 +0000

    [changelog] Added ezTokenizer, a class that helps tokenize a C language like piece of text.

---
+++ Code/Engine/CoreUtils/CodeUtils/Implementation/Tokenizer.cpp
@@ -0,0 +1,415 @@
+#include <CoreUtils/PCH.h>
+#include <CoreUtils/CodeUtils/Tokenizer.h>
+
+const char* ezTokenType::EnumNames[ezTokenType::ENUM_COUNT] =
+{
+  "Unknown",
+  "Whitespace",
+  "Identifier",
+  "NonIdentifier",
+  "Newline",
+  "LineComment",
+  "BlockComment",
+  "String1",
+  "String2",
+};
+
+
+ezTokenizer::ezTokenizer()
+{
+  m_CurMode = ezTokenType::Unknown;
+  m_uiCurLine = 1;
+  m_uiCurColumn = -1;
+  m_uiCurChar = '\0';
+  m_uiNextChar = '\0';
+  m_uiLastLine = 1;
+  m_uiLastColumn = 1;
+
+  m_szCurCharStart = nullptr;
+  m_szNextCharStart = nullptr;
+  m_szTokenStart = nullptr;
+}
+
+void ezTokenizer::NextChar()
+{
+  m_uiCurChar = m_uiNextChar;
+  m_szCurCharStart = m_szNextCharStart;
+  ++m_uiCurColumn;
+
+  if (m_uiCurChar == '\n')
+  {
+    ++m_uiCurLine;
+    m_uiCurColumn = 0;
+  }
+
+  if (!m_Iterator.IsValid())
+  {
+    m_szNextCharStart = m_Iterator.GetEnd();
+    m_uiNextChar = '\0';
+    return;
+  }
+
+  m_uiNextChar = m_Iterator.GetCharacter();
+  m_szNextCharStart = m_Iterator.GetData();
+
+  ++m_Iterator;
+}
+
+void ezTokenizer::AddToken()
+{
+  const char* szEnd = m_szCurCharStart;
+
+  ezToken t;
+  t.m_uiLine = m_uiLastLine;
+  t.m_uiColumn = m_uiLastColumn;
+  t.m_Type = m_CurMode;
+  t.m_DataView = ezStringIterator(m_szTokenStart, szEnd, m_szTokenStart);
+
+  m_uiLastLine = m_uiCurLine;
+  m_uiLastColumn = m_uiCurColumn;
+
+  m_Tokens.PushBack(t);
+
+  m_szTokenStart = szEnd;
+
+  ezStringBuilder s = t.m_DataView;
+  ezLog::Info("Line %u:%u, Token '%s': '%s'", t.m_uiLine, t.m_uiColumn, ezTokenType::EnumNames[t.m_Type], s.GetData());
+
+  m_CurMode = ezTokenType::Unknown;
+}
+
+void ezTokenizer::Tokenize(const ezDynamicArray<ezUInt8>& Data, ezLogInterface* pLog)
+{
+  /// \test Add unit test for this.
+
+  m_Data.Clear();
+  m_Data.Reserve(m_Data.GetCount() + 1);
+  m_Data = Data;
+  m_Data.PushBack('\0'); // make sure the string is zero terminated
+  m_Tokens.Clear();
+  m_pLog = pLog;
+
+  {
+    m_CurMode = ezTokenType::Unknown;
+    m_uiCurLine = 1;
+    m_uiCurColumn = -1;
+    m_uiCurChar = '\0';
+    m_uiNextChar = '\0';
+    m_uiLastLine = 1;
+    m_uiLastColumn = 1;
+
+    m_szCurCharStart = nullptr;
+    m_szNextCharStart = nullptr;
+    m_szTokenStart = nullptr;
+  }
+
+  m_Iterator = ezStringIterator((const char*) &m_Data[0], (const char*) &m_Data[0] + m_Data.GetCount() - 1, (const char*) &m_Data[0]);
+
+  if (!m_Iterator.IsValid())
+  {
+    ezToken t;
+    t.m_uiLine = 1;
+    t.m_Type = ezTokenType::EndOfFile;
+    m_Tokens.PushBack(t);
+    return;
+  }
+
+  NextChar();
+  NextChar();
+
+  m_szTokenStart = m_szCurCharStart;
+
+  while (m_szTokenStart != nullptr && *m_szTokenStart != '\0')
+  {
+    switch (m_CurMode)
+    {
+    case ezTokenType::Unknown:
+      HandleUnknown();
+      break;
+
+    case ezTokenType::String1:
+      HandleString1();
+      break;
+
+    case ezTokenType::String2:
+      HandleString2();
+      break;
+
+    case ezTokenType::LineComment:
+      HandleLineComment();
+      break;
+
+    case ezTokenType::BlockComment:
+      HandleBlockComment();
+      break;
+
+    case ezTokenType::Whitespace:
+      HandleWhitespace();
+      break;
+
+    case ezTokenType::Identifier:
+      HandleIdentifier();
+      break;
+
+    case ezTokenType::NonIdentifier:
+      HandleNonIdentifier();
+      break;
+    }
+  }
+
+  ezToken t;
+  t.m_uiLine = m_uiCurLine;
+  t.m_Type = ezTokenType::EndOfFile;
+  m_Tokens.PushBack(t);
+}
+
+void ezTokenizer::HandleUnknown()
+{
+  m_szTokenStart = m_szCurCharStart;
+
+  if ((m_uiCurChar == '/') && (m_uiNextChar == '/'))
+  {
+    m_CurMode = ezTokenType::LineComment;
+    NextChar();
+    NextChar();
+    return;
+  }
+
+  if ((m_uiCurChar == '/') && (m_uiNextChar == '*'))
+  {
+    m_CurMode = ezTokenType::BlockComment;
+    NextChar();
+    NextChar();
+    return;
+  }
+
+  if (m_uiCurChar == '\"')
+  {
+    m_CurMode = ezTokenType::String1;
+    NextChar();
+    return;
+  }
+
+  if (m_uiCurChar == '\'')
+  {
+    m_CurMode = ezTokenType::String2;
+    NextChar();
+    return;
+  }
+
+  if ((m_uiCurChar == ' ') || (m_uiCurChar == '\t'))
+  {
+    m_CurMode = ezTokenType::Whitespace;
+    NextChar();
+    return;
+  }
+
+  if (!ezStringUtils::IsIdentifierDelimiter_C_Code(m_uiCurChar))
+  {
+    m_CurMode = ezTokenType::Identifier;
+    NextChar();
+    return;
+  }
+
+  if (m_uiCurChar == '\n')
+  {
+    m_CurMode = ezTokenType::Newline;
+    NextChar();
+    AddToken();
+    return;
+  }
+
+  if ((m_uiCurChar == '\r') && (m_uiNextChar == '\n'))
+  {
+    NextChar();
+    NextChar();
+    m_CurMode = ezTokenType::Newline;
+    AddToken();
+    return;
+  }
+
+  // else
+  m_CurMode = ezTokenType::NonIdentifier;
+  NextChar();
+}
+
+void ezTokenizer::HandleString1()
+{
+  while (m_uiCurChar != '\0')
+  {
+    while ((m_uiCurChar == '\\') && (m_uiNextChar == '\"'))
+    {
+      // skip this one
+      NextChar();
+      NextChar();
+    }
+
+    if (m_uiCurChar == '\"')
+    {
+      NextChar();
+      AddToken();
+      return;
+    }
+
+    NextChar();
+  }
+
+  ezLog::Error(m_pLog, "String not closed at end of file.");
+  AddToken();
+}
+
+void ezTokenizer::HandleString2()
+{
+  while (m_uiCurChar != '\0')
+  {
+    while ((m_uiCurChar == '\\') && (m_uiNextChar == '\''))
+    {
+      // skip this one
+      NextChar();
+      NextChar();
+    }
+
+    if (m_uiCurChar == '\'')
+    {
+      NextChar();
+      AddToken();
+      return;
+    }
+
+    NextChar();
+  }
+
+  ezLog::Error(m_pLog, "String not closed at end of file.");
+  AddToken();
+}
+
+void ezTokenizer::HandleLineComment()
+{
+  while (m_uiCurChar != '\0')
+  {
+    if ((m_uiCurChar == '\r') || (m_uiCurChar == '\n'))
+    {
+      AddToken();
+      return;
+    }
+
+    NextChar();
+  }
+
+  // comment at end of file
+  AddToken();
+}
+
+void ezTokenizer::HandleBlockComment()
+{
+  while (m_uiCurChar != '\0')
+  {
+    if ((m_uiCurChar == '*') && (m_uiNextChar == '/'))
+    {
+      NextChar();
+      NextChar();
+      AddToken();
+      return;
+    }
+
+    NextChar();
+  }
+
+  ezLog::Error(m_pLog, "Block comment not closed at end of file.");
+  AddToken();
+}
+
+void ezTokenizer::HandleWhitespace()
+{
+  while (m_uiCurChar != '\0')
+  {
+    if (m_uiCurChar != ' ' && m_uiCurChar != '\t')
+    {
+      AddToken();
+      return;
+    }
+
+    NextChar();
+  }
+
+  // whitespace at end of file
+  AddToken();
+}
+
+void ezTokenizer::HandleIdentifier()
+{
+  while (m_uiCurChar != '\0')
+  {
+    if (ezStringUtils::IsIdentifierDelimiter_C_Code(m_uiCurChar))
+    {
+      AddToken();
+      return;
+    }
+
+    NextChar();
+  }
+
+  // identifier at end of file
+  AddToken();
+}
+
+void ezTokenizer::HandleNonIdentifier()
+{
+  AddToken();
+}
+
+ezResult ezTokenizer::GetNextLine(ezUInt32& uiFirstToken, ezHybridArray<const ezToken*, 32>& Tokens)
+{
+  Tokens.Clear();
+
+  const ezUInt32 uiMaxTokens = m_Tokens.GetCount() - 1;
+
+  while (uiFirstToken < uiMaxTokens)
+  {
+    const ezToken& tCur = m_Tokens[uiFirstToken];
+
+    // found a backslash
+    if (tCur.m_Type == ezTokenType::NonIdentifier && tCur.m_DataView == "\\")
+    {
+      const ezToken& tNext = m_Tokens[uiFirstToken + 1];
+
+      // and a newline!
+      if (tNext.m_Type == ezTokenType::Newline)
+      {
+        /// \todo Theoretically, if the line ends with an identifier, and the next directly starts with one again,
+        // we would need to merge the two into one identifier name, because the \ \n combo means it is not a
+        // real line break
+        // for now we ignore this and assume there is a 'whitespace' between such identifiers
+
+        // we could maybe at least output a warning, if we detect it
+        if (uiFirstToken > 0 && m_Tokens[uiFirstToken - 1].m_Type == ezTokenType::Identifier &&
+            uiFirstToken + 2 < uiMaxTokens && m_Tokens[uiFirstToken + 2].m_Type == ezTokenType::Identifier)
+        {
+          ezStringBuilder s1 = m_Tokens[uiFirstToken - 1].m_DataView;
+          ezStringBuilder s2 = m_Tokens[uiFirstToken + 2].m_DataView;
+          ezLog::Warning("Line %u: The \\ at the line end is in the middle of an identifier name ('%s' and '%s'). However, merging identifier names is currently not supported.", m_Tokens[uiFirstToken].m_uiLine, s1.GetData(), s2.GetData());
+        }
+
+        // ignore this
+        uiFirstToken += 2;
+        continue;
+      }
+    }
+
+    Tokens.PushBack(&tCur);
+
+    if (m_Tokens[uiFirstToken].m_Type == ezTokenType::Newline)
+    {
+      ++uiFirstToken;
+      return EZ_SUCCESS;
+    }
+
+    ++uiFirstToken;
+  }
+
+  if (Tokens.IsEmpty())
+    return EZ_FAILURE;
+
+  return EZ_SUCCESS;
+}
+
---
+++ Code/Engine/CoreUtils/CodeUtils/Tokenizer.h
@@ -0,0 +1,117 @@
+#include <CoreUtils/Basics.h>
+#include <Foundation/IO/Stream.h>
+#include <Foundation/Logging/Log.h>
+#include <Foundation/Containers/Deque.h>
+
+/// \brief Describes which kind of token an ezToken is.
+struct EZ_COREUTILS_DLL ezTokenType
+{
+  enum Enum
+  {
+    Unknown,        ///< for internal use
+    Whitespace,     ///< The token is a space or tab
+    Identifier,     ///< a series of alphanumerics or underscores
+    NonIdentifier,  ///< Everthing else
+    Newline,        ///< Either '\n' or '\r\n'
+    LineComment,    ///< A comment that starts with two slashes and ends at the next newline (or end of file)
+    BlockComment,   ///< A comment that starts with a slash and a star, and ends at the next star/slash combination (or end of file)
+    String1,        ///< A string enclosed in "
+    String2,        ///< A string enclosed in '
+    EndOfFile,      ///< End-of-file marker
+    ENUM_COUNT,
+  };
+
+  static const char* EnumNames[ENUM_COUNT];
+};
+
+struct EZ_COREUTILS_DLL ezToken
+{
+  ezToken()
+  {
+    m_Type = ezTokenType::Unknown;
+    m_uiLine = 0;
+    m_uiColumn = 0;
+  }
+
+  ezTokenType::Enum m_Type;
+
+  /// The line in which the token appeared
+  ezUInt32 m_uiLine;
+
+  /// The column in the line, at which the token string started.
+  ezUInt32 m_uiColumn;
+
+  /// The actual string data that represents the token. Note that this is a view to a substring of some larger text data.
+  /// To get only the relevant piece as one zero-terminated string, assign m_DataView to an ezStringBuilder and read that instead.
+  ezStringIterator m_DataView;
+};
+
+/// \brief Takes text and splits it up into ezToken objects. The result can be used for easier parsing.
+///
+/// The tokenizer is built to work on code that is similar to C. That means it will tokenize comments and
+/// strings as they are defined in the C language. Also line breaks that end with a backslash are not
+/// really considered as line breaks.\n
+/// White space is defined as spaces and tabs.\n
+/// Identifiers are names that consist of alphanumerics and underscores.\n
+/// Non-Identifiers are everything else. However, they will currently never consist of more than a single character.
+/// Ie. '++' will be tokenized as two consecutive non-Identifiers.\n
+/// Parenthesis etc. will not be tokenized in any special way, they are all considered as non-Identifiers.
+///
+/// The token stream will always end with an end-of-file token.
+class EZ_COREUTILS_DLL ezTokenizer
+{
+public:
+  /// \brief Constructor.
+  ezTokenizer();
+
+  /// \brief Clears any previous result and creates a new token stream for the given array.
+  void Tokenize(const ezDynamicArray<ezUInt8>& Data, ezLogInterface* pLog);
+
+  /// \brief Gives read access to the token stream.
+  const ezDeque<ezToken>& GetTokens() const { return m_Tokens; }
+
+  /// \brief Returns an array of tokens that represent the next line in the file.
+  ///
+  /// Returns EZ_SUCCESS when there was more data to return, EZ_FAILURE if the end of the file was reached already.
+  /// uiFirstToken is the index from where to start. It will be updated automatically. Consecutive calls to GetNextLine()
+  /// with the same uiFirstToken variable will give one line after the other.
+  ///
+  /// \note This function takes care of handling the 'backslash/newline' combination, as defined in the C language.
+  /// That means all such sequences will be ignored. Therefore the tokens that are returned as one line might not
+  /// contain all tokens that are actually in the stream. Also the tokens might have different line numbers, when
+  /// two or more lines from the file are merged into one logical line.
+  ezResult GetNextLine(ezUInt32& uiFirstToken, ezHybridArray<const ezToken*, 32>& Tokens);
+
+private:
+  void NextChar();
+  void AddToken();
+
+  void HandleUnknown();
+  void HandleString1();
+  void HandleString2();
+  void HandleLineComment();
+  void HandleBlockComment();
+  void HandleWhitespace();
+  void HandleIdentifier();
+  void HandleNonIdentifier();
+
+  ezLogInterface* m_pLog;
+  ezTokenType::Enum m_CurMode;
+  ezStringIterator m_Iterator;
+  ezUInt32 m_uiCurLine;
+  ezUInt32 m_uiCurColumn;
+  ezUInt32 m_uiCurChar;
+  ezUInt32 m_uiNextChar;
+
+  ezUInt32 m_uiLastLine;
+  ezUInt32 m_uiLastColumn;
+
+  const char* m_szCurCharStart;
+  const char* m_szNextCharStart;
+  const char* m_szTokenStart;
+
+  ezDeque<ezToken> m_Tokens;
+  ezDynamicArray<ezUInt8> m_Data;
+};
+
+
