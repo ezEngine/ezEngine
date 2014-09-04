4d27ccebcc2378735b6a9949772690b3f68b9856
Ingrater <code@benjamin-thaut.de>
2014-09-04 05:39:41 +0000

    Fixed implementation of ezMath::Min including unittests.

--- Code/Engine/Foundation/Math/Implementation/Math_inl.h
+++ Code/Engine/Foundation/Math/Implementation/Math_inl.h
@@ -54,7 +54,7 @@ namespace ezMath
   template <typename T>
   EZ_FORCE_INLINE T Min(T f1, T f2)
   {
-    return (f1 < f2 ? f1 : f2);
+    return (f2 < f1 ? f2 : f1);
   }
 
   template <typename T>
