e8f6ff5516266cb1772397b84b4785d6059fdd87
Manuel Maier <mjmaier@gmx.de>
2014-11-18 13:54:35 +0100

    ezTransfrom => ezTransformTemplate is now a template

--- Code/Engine/Foundation/Math/Transform.h
+++ Code/Engine/Foundation/Math/Transform.h
@@ -5,10 +5,10 @@
 #include <Foundation/Math/Mat3.h>
 #include <Foundation/Math/Mat4.h>
 
-/// \brief A class that represents a complete position, rotation and scaling through an ezVec3 and an ezMat3.
+/// \brief A class that represents a complete position, rotation and scaling through an ezVec3Template<Type> and an ezMat3Template<Type>.
 ///
 /// The rotation is applied first and then the translation is added. Thus the rotation component is always in 'local space',
-/// i.e. applying a rotation to the ezTransform will rotate objects in place around their local center.
+/// i.e. applying a rotation to the ezTransformTemplate will rotate objects in place around their local center.
 /// Since the translation is added afterwards, the translation component is always the global center position, around which
 /// objects also rotate.
 ///
@@ -17,32 +17,33 @@
 /// from the parent's global transformation to the current global transformation of a child (i.e. only the difference).
 /// This is particularly useful when editing entities in a hierarchical structure.
 ///
-/// Since the 'm_Rotation' member is a full ezMat3, it may also contain scaling, including non-uniform scaling and shearing.
-class EZ_FOUNDATION_DLL ezTransform
+/// Since the 'm_Rotation' member is a full ezMat3Template<Type>, it may also contain scaling, including non-uniform scaling and shearing.
+template<typename Type>
+class ezTransformTemplate
 {
   // *** Data ***
 public:
 
-  ezMat3 m_Rotation;
-  ezVec3 m_vPosition;
+  ezMat3Template<Type> m_Rotation;
+  ezVec3Template<Type> m_vPosition;
 
   /// \brief Copies the 12 values of this matrix into the given array. 'layout' defines whether the data should end up in column-major or row-major format.
-  void GetAsArray(float* out_pData, ezMatrixLayout::Enum layout) const; // [tested]
+  void GetAsArray(Type* out_pData, ezMatrixLayout::Enum layout) const; // [tested]
 
   // *** Constructors ***
 public:
 
   /// \brief Default constructor: Does not do any initialization.
-  ezTransform() { } // [tested]
+  ezTransformTemplate() { } // [tested]
 
   /// \brief Sets position and rotation matrix, which might include scale as well.
-  explicit ezTransform(const ezVec3& vPosition, const ezMat3& Rotation); // [tested]
+  explicit ezTransformTemplate(const ezVec3Template<Type>& vPosition, const ezMat3Template<Type>& Rotation); // [tested]
 
   /// \brief Sets position and rotation.
-  explicit ezTransform(const ezVec3& vPosition, const ezQuat& qRotation = ezQuat::IdentityQuaternion()); // [tested]
+  explicit ezTransformTemplate(const ezVec3Template<Type>& vPosition, const ezQuatTemplate<Type>& qRotation = ezQuatTemplate<Type>::IdentityQuaternion()); // [tested]
 
   /// \brief Sets position, rotation and scale.
-  explicit ezTransform(const ezVec3& vPosition, const ezQuat& qRotation, const ezVec3& vScale); // [tested]
+  explicit ezTransformTemplate(const ezVec3Template<Type>& vPosition, const ezQuatTemplate<Type>& qRotation, const ezVec3Template<Type>& vScale); // [tested]
 
   /// \brief Sets the position to be zero and the rotation to identity.
   void SetIdentity(); // [tested]
@@ -51,10 +52,10 @@ public:
 public:
 
   /// \brief Equality Check (bitwise)
-  bool IsIdentical(const ezTransform& rhs) const; // [tested]
+  bool IsIdentical(const ezTransformTemplate& rhs) const; // [tested]
 
   /// \brief Equality Check with epsilon
-  bool IsEqual(const ezTransform& rhs, float fEpsilon) const; // [tested]
+  bool IsEqual(const ezTransformTemplate& rhs, float fEpsilon) const; // [tested]
 
   // *** Inverse ***
 public:
@@ -63,26 +64,26 @@ public:
   ezResult Invert(float fEpsilon = ezMath::BasicType<float>::SmallEpsilon()); // [tested]
 
   /// \brief Returns the inverse of this transform.
-  const ezTransform GetInverse() const; // [tested]
+  const ezTransformTemplate GetInverse() const; // [tested]
 
   // *** Conversion operations ***
 public:
 
   /// \brief Sets this transform to be the local transformation needed to get from the parent's transform to the child's.
-  void SetLocalTransform(const ezTransform& GlobalTransformParent, const ezTransform& GlobalTransformChild); // [tested]
+  void SetLocalTransform(const ezTransformTemplate& GlobalTransformParent, const ezTransformTemplate& GlobalTransformChild); // [tested]
 
   /// \brief Sets this transform to the global transform, that is reached by applying the child's local transform to the parent's global one.
-  void SetGlobalTransform(const ezTransform& GlobalTransformParent, const ezTransform& LocalTransformChild); // [tested]
+  void SetGlobalTransform(const ezTransformTemplate& GlobalTransformParent, const ezTransformTemplate& LocalTransformChild); // [tested]
 
   /// \brief Returns the transformation as a matrix.
-  const ezMat4 GetAsMat4() const; // [tested]
+  const ezMat4Template<Type> GetAsMat4() const; // [tested]
 
 
   // *** Operators ***
 public:
 
   /// \brief Multiplies \a q into the rotation component, thus rotating the entire transformation.
-  void operator*=(const ezQuat& q); // [tested]
+  void operator*=(const ezQuatTemplate<Type>& q); // [tested]
 
 };
 
@@ -90,25 +91,32 @@ public:
 // *** free functions ***
 
 /// \brief Transforms the vector v by the transform.
-const ezVec3 operator*(const ezTransform& t, const ezVec3& v); // [tested]
+template<typename Type>
+const ezVec3Template<Type> operator*(const ezTransformTemplate<Type>& t, const ezVec3Template<Type>& v); // [tested]
 
 /// \brief Rotates the transform by the given quaternion.
-const ezTransform operator*(const ezQuat& q, const ezTransform& t); // [tested]
+template<typename Type>
+const ezTransformTemplate<Type> operator*(const ezQuatTemplate<Type>& q, const ezTransformTemplate<Type>& t); // [tested]
 
 /// \brief Translates the ezTransform by the vector. This will move the object in global space.
-const ezTransform operator+(const ezTransform& t, const ezVec3& v); // [tested]
+template<typename Type>
+const ezTransformTemplate<Type> operator+(const ezTransformTemplate<Type>& t, const ezVec3Template<Type>& v); // [tested]
 
 /// \brief Translates the ezTransform by the vector. This will move the object in global space.
-const ezTransform operator-(const ezTransform& t, const ezVec3& v); // [tested]
+template<typename Type>
+const ezTransformTemplate<Type> operator-(const ezTransformTemplate<Type>& t, const ezVec3Template<Type>& v); // [tested]
 
 /// \brief Concatenates the two transforms. This is the same as a matrix multiplication, thus not commutative.
-const ezTransform operator*(const ezTransform& t1, const ezTransform& t2); // [tested]
+template<typename Type>
+const ezTransformTemplate<Type> operator*(const ezTransformTemplate<Type>& t1, const ezTransformTemplate<Type>& t2); // [tested]
 
 /// \brief Concatenates the two transforms. This is the same as a matrix multiplication, thus not commutative.
-const ezTransform operator*(const ezTransform& t1, const ezMat4& t2); // [tested]
+template<typename Type>
+const ezTransformTemplate<Type> operator*(const ezTransformTemplate<Type>& t1, const ezMat4& t2); // [tested]
 
 /// \brief Concatenates the two transforms. This is the same as a matrix multiplication, thus not commutative.
-const ezTransform operator*(const ezMat4& t1, const ezTransform& t2); // [tested]
+template<typename Type>
+const ezTransformTemplate<Type> operator*(const ezMat4& t1, const ezTransformTemplate<Type>& t2); // [tested]
 
 #include <Foundation/Math/Implementation/Transform_inl.h>
 
