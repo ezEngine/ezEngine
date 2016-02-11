6d5e803c8a32a3d9cd21970cff47789a2609defb
jankrassnigg <jan@krassnigg.de>
2016-02-11 12:13:10 +0000

    Some work on the input binding dialog.

--- Tools/EditorFramework/Dialogs/InputConfigDlg.cpp
+++ Tools/EditorFramework/Dialogs/InputConfigDlg.cpp
@@ -6,13 +6,101 @@
 #include <Foundation/Strings/String.h>
 #include <QTreeWidget>
 #include <QCheckBox>
+#include <QMessageBox>
+#include <GuiFoundation/UIServices/UIServices.moc.h>
+#include <QSpinBox>
+#include <QComboBox>
 
 InputConfigDlg::InputConfigDlg(QWidget* parent) : QDialog(parent)
 {
   setupUi(this);
 
   LoadActions();
+
+  // dummy
+  // todo: use array, sorted as they are given to the input manager
+  {
+	  m_AllInputSlots.Insert( "Key_A" );
+	  m_AllInputSlots.Insert( "Key_B" );
+	  m_AllInputSlots.Insert( "Key_C" );
+	  for ( const auto& action : m_Actions )
+	  {
+		  m_AllInputSlots.Insert( action.m_sInputSlotTrigger[0] );
+		  m_AllInputSlots.Insert( action.m_sInputSlotTrigger[1] );
+		  m_AllInputSlots.Insert( action.m_sInputSlotTrigger[2] );
+	  }
+  }
+
   FillList();
+
+  on_TreeActions_itemSelectionChanged();
+}
+
+void InputConfigDlg::on_ButtonNewInputSet_clicked()
+{
+
+}
+
+void InputConfigDlg::on_ButtonNewAction_clicked()
+{
+	auto pItem = TreeActions->currentItem();
+
+	if ( !pItem )
+		return;
+
+	if ( TreeActions->indexOfTopLevelItem( pItem ) < 0 )
+		pItem = pItem->parent();
+
+	ezGameAppInputConfig action;
+	auto pNewItem = CreateActionItem( pItem, action );
+
+	TreeActions->clearSelection();
+	pNewItem->setSelected( true );
+	TreeActions->editItem( pNewItem );
+}
+
+void InputConfigDlg::on_ButtonRemove_clicked()
+{
+	auto pItem = TreeActions->currentItem();
+
+	if ( !pItem )
+		return;
+
+	if ( TreeActions->indexOfTopLevelItem( pItem ) >= 0 )
+	{
+		if ( ezUIServices::GetInstance()->MessageBoxQuestion( "Do you really want to remove the entire Input Set?", QMessageBox::Yes | QMessageBox::No, QMessageBox::No ) == QMessageBox::No )
+			return;
+	}
+	else
+	{
+		if ( ezUIServices::GetInstance()->MessageBoxQuestion( "Do you really want to remove this action?", QMessageBox::Yes | QMessageBox::No, QMessageBox::No ) == QMessageBox::No )
+			return;
+	}
+
+	delete pItem;
+}
+
+void InputConfigDlg::on_ButtonBox_clicked( QAbstractButton* pButton )
+{
+	if ( pButton == ButtonBox->button( QDialogButtonBox::Ok ) )
+	{
+		accept();
+		return;
+	}
+
+	if ( pButton == ButtonBox->button( QDialogButtonBox::Cancel ) )
+	{
+		reject();
+		return;
+	}
+}
+
+void InputConfigDlg::on_TreeActions_itemSelectionChanged()
+{
+	const bool hasSelection = TreeActions->currentItem() != nullptr;
+
+	ButtonRemove->setEnabled( hasSelection );
+	ButtonNewAction->setEnabled( hasSelection );
 }
 
 void InputConfigDlg::LoadActions()
@@ -57,17 +145,67 @@ void InputConfigDlg::FillList()
   {
     QTreeWidgetItem* pParentItem = m_InputSetToItem[action.m_sInputSet];
 
-    auto* pItem = new QTreeWidgetItem(pParentItem);
-    pItem->setText(0, action.m_sInputAction.GetData());
-    pItem->setFlags(Qt::ItemFlag::ItemIsEnabled | Qt::ItemFlag::ItemIsSelectable | Qt::ItemFlag::ItemIsEditable);
-    
-    QCheckBox* pTimeScale = new QCheckBox(TreeActions);
-    pTimeScale->setChecked(action.m_bApplyTimeScaling);
-    TreeActions->setItemWidget(pItem, 1, pTimeScale);
+	CreateActionItem( pParentItem, action );
 
+
+	pParentItem->setExpanded( true );
   }
+
+  TreeActions->resizeColumnToContents( 0 );
+  TreeActions->resizeColumnToContents( 1 );
+  TreeActions->resizeColumnToContents( 2 );
+  TreeActions->resizeColumnToContents( 3 );
+  TreeActions->resizeColumnToContents( 4 );
+  TreeActions->resizeColumnToContents( 5 );
+  TreeActions->resizeColumnToContents( 6 );
+  TreeActions->resizeColumnToContents( 7 );
 }
 
+QTreeWidgetItem* InputConfigDlg::CreateActionItem( QTreeWidgetItem* pParentItem, const ezGameAppInputConfig& action )
+{
+	auto* pItem = new QTreeWidgetItem( pParentItem );
+	pItem->setText( 0, action.m_sInputAction.GetData() );
+	pItem->setFlags( Qt::ItemFlag::ItemIsEnabled | Qt::ItemFlag::ItemIsSelectable | Qt::ItemFlag::ItemIsEditable );
+
+	QCheckBox* pTimeScale = new QCheckBox( TreeActions );
+	pTimeScale->setChecked( action.m_bApplyTimeScaling );
+	TreeActions->setItemWidget( pItem, 1, pTimeScale );
+
+	for ( int i = 0; i < 3; ++i )
+	{
+		QDoubleSpinBox* spin = new QDoubleSpinBox( TreeActions );
+		spin->setDecimals( 3 );
+		spin->setMinimum( 0.0 );
+		spin->setMaximum( 100.0 );
+		spin->setSingleStep( 0.01 );
+		spin->setValue( action.m_fInputSlotScale[i] );
+
+		TreeActions->setItemWidget( pItem, 3 + 2 * i, spin );
+
+		QComboBox* combo = new QComboBox( TreeActions );
+		combo->setAutoCompletion( true );
+		combo->setAutoCompletionCaseSensitivity( Qt::CaseInsensitive );
+		combo->setEditable( false );
+		combo->setInsertPolicy( QComboBox::InsertAtBottom );
+		combo->setMaxVisibleItems( 15 );
+
+		for ( auto it = m_AllInputSlots.GetIterator(); it.IsValid(); ++it )
+		{
+			combo->addItem( it.Key().GetData() );
+		}
+
+		int index = combo->findText( action.m_sInputSlotTrigger[i].GetData() );
+
+		if ( index > 0 )
+			combo->setCurrentIndex( index );
+		else
+			combo->setCurrentIndex( 0 );
+
+		TreeActions->setItemWidget( pItem, 2 + 2 * i, combo );
+	}
+
+	return pItem;
+}
 
 
 
--- Tools/EditorFramework/Dialogs/InputConfigDlg.moc.h
+++ Tools/EditorFramework/Dialogs/InputConfigDlg.moc.h
@@ -5,6 +5,7 @@
 #include <Tools/EditorFramework/ui_InputConfigDlg.h>
 #include <GameFoundation/GameApplication/InputConfig.h>
 #include <Foundation/Containers/Map.h>
+#include <Foundation/Containers/Set.h>
 
 class QTreeWidgetItem;
 
@@ -17,13 +18,21 @@ public:
   InputConfigDlg(QWidget* parent);
 
 private slots:
+	void on_ButtonNewInputSet_clicked();
+	void on_ButtonNewAction_clicked();
+	void on_ButtonRemove_clicked();
+	void on_ButtonBox_clicked( QAbstractButton* pButton );
+	void on_TreeActions_itemSelectionChanged();
 
 private:
   void LoadActions();
   void FillList();
 
+  QTreeWidgetItem* CreateActionItem( QTreeWidgetItem* pParentItem, const ezGameAppInputConfig& action );
+
   ezMap<ezString, QTreeWidgetItem*> m_InputSetToItem;
   ezHybridArray<ezGameAppInputConfig, 32> m_Actions;
+  ezSet<ezString> m_AllInputSlots;
 };
 
 
