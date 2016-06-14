642a4a592b6de07f2e0b6e709ae142cce71f1e0d
Sanakan8472 <christopher.manthei@gmail.com>
2016-06-14 22:04:54 +0000

    Fmod fix.

--- Tools/EditorPluginFmod/SoundBankAsset/SoundBankAsset.h
+++ Tools/EditorPluginFmod/SoundBankAsset/SoundBankAsset.h
@@ -15,7 +15,6 @@ public:
   virtual const char* QueryAssetType() const override { return "Sound Bank"; }
 
 protected:
-  virtual ezUInt16 GetAssetTypeVersion() const override { return 1; }
   virtual void UpdateAssetDocumentInfo(ezAssetDocumentInfo* pInfo) override;
   virtual ezStatus InternalTransformAsset(ezStreamWriter& stream, const char* szPlatform) override;
 
