36a74ba98f2ee6441b9b0840826135085290a5d3
jankrassnigg <jan@krassnigg.de>
2017-01-28 19:31:51 +0000

    Making Subversion happy before moving code around

---
+++ Code/Engine/Utilities/PCH.cpp
@@ -0,0 +1,12 @@
+#include <GameEngine/PCH.h>
+
+EZ_STATICLINK_LIBRARY(Utilities)
+{
+  if (bReturn)
+    return;
+
+
+}
+
+
+
---
+++ Code/Engine/Utilities/PCH.h
@@ -0,0 +1,25 @@
+#pragma once
+
+// <StaticLinkUtil::StartHere>
+// all include's before this will be left alone and not replaced by the StaticLinkUtil
+// all include's AFTER this will be removed by the StaticLinkUtil and updated by what is actually used throughout the library
+
+
+
+#include <Core/PCH.h>
+#include <Core/World/Component.h>
+#include <Core/World/GameObject.h>
+#include <Core/World/World.h>
+#include <CoreUtils/PCH.h>
+#include <Foundation/Basics.h>
+#include <Foundation/Containers/Deque.h>
+#include <Foundation/Containers/DynamicArray.h>
+#include <Foundation/Containers/HashTable.h>
+#include <Foundation/Math/Mat3.h>
+#include <Foundation/Math/Math.h>
+#include <Foundation/Math/Rect.h>
+#include <Foundation/Math/Vec2.h>
+#include <Foundation/Math/Vec3.h>
+#include <Foundation/PCH.h>
+#include <Foundation/Time/Time.h>
+
