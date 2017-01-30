c51ba6c0a1707ce380093581240907744318df91
wumpf <r_andreas2@web.de>
2017-01-30 22:47:11 +0000

    Extended sorting test to use lambda and check for precedence of Less function over call operator.

--- UnitTests/FoundationTest/Algorithm/SortingTest.cpp
+++ UnitTests/FoundationTest/Algorithm/SortingTest.cpp
@@ -10,9 +10,10 @@ namespace
       return a > b;
     }
 
-    EZ_FORCE_INLINE bool Equal(ezInt32 a, ezInt32 b) const
+    // Comparision via operator. Sorting algorithm should prefer Less operator
+    bool operator ()(ezInt32 a, ezInt32 b) const
     {
-      return a == b;
+      return a < b;
     }
   };
 }
@@ -26,20 +27,34 @@ EZ_CREATE_SIMPLE_TEST(Algorithm, Sorting)
     a1.PushBack(rand() % 100000);
   }
 
-  ezDynamicArray<ezInt32> a2 = a1;
-
-  ezSorting::QuickSort(a1, CustomComparer()); // quicksort uses insertion sort for partitions smaller than 16 elements
-
-  for (ezUInt32 i = 1; i < a1.GetCount(); ++i)
+  EZ_TEST_BLOCK(true, "QuickSort")
   {
-    EZ_TEST_BOOL(a1[i-1] >= a1[i]);
+    ezDynamicArray<ezInt32> a2 = a1;
+
+    ezSorting::QuickSort(a1, CustomComparer()); // quicksort uses insertion sort for partitions smaller than 16 elements
+
+    for (ezUInt32 i = 1; i < a1.GetCount(); ++i)
+    {
+      EZ_TEST_BOOL(a1[i - 1] >= a1[i]);
+    }
+
+    ezArrayPtr<ezInt32> arrayPtr = a2;
+    ezSorting::QuickSort(arrayPtr, CustomComparer()); // quicksort uses insertion sort for partitions smaller than 16 elements
+
+    for (ezUInt32 i = 1; i < arrayPtr.GetCount(); ++i)
+    {
+      EZ_TEST_BOOL(arrayPtr[i - 1] >= arrayPtr[i]);
+    }
   }
 
-  ezArrayPtr<ezInt32> arrayPtr = a2;
-  ezSorting::QuickSort(arrayPtr, CustomComparer()); // quicksort uses insertion sort for partitions smaller than 16 elements
-
-  for (ezUInt32 i = 1; i < arrayPtr.GetCount(); ++i)
+  EZ_TEST_BLOCK(true, "QuickSort - Lambda")
   {
-    EZ_TEST_BOOL(arrayPtr[i-1] >= arrayPtr[i]);
+    ezDynamicArray<ezInt32> a2 = a1;
+    ezSorting::QuickSort(a2, [](const auto& a, const auto& b) { return a > b; });
+
+    for (ezUInt32 i = 1; i < a2.GetCount(); ++i)
+    {
+      EZ_TEST_BOOL(a2[i - 1] >= a2[i]);
+    }
   }
 }
