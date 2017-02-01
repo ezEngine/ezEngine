dbd46113b24d192be9165b4e8a06d1d62c9eb169
wumpf <r_andreas2@web.de>
2017-02-01 22:59:41 +0000

    Deactivated DirectoryWatcher for uwp.

--- Code/Engine/Foundation/IO/Implementation/DirectoryWatcher.cpp
+++ Code/Engine/Foundation/IO/Implementation/DirectoryWatcher.cpp
@@ -1,7 +1,9 @@
 #include <Foundation/PCH.h>
 
-#if EZ_ENABLED(EZ_PLATFORM_WINDOWS)
+#if EZ_ENABLED(EZ_PLATFORM_WINDOWS_DESKTOP)
 #include <Foundation/IO/Implementation/Win/DirectoryWatcher_win.h>
+#elif EZ_ENABLED(EZ_PLATFORM_WINDOWS_UWP)
+#include <Foundation/IO/Implementation/Win/DirectoryWatcher_uwp.h>
 #elif EZ_ENABLED(EZ_USE_POSIX_FILE_API)
 #include <Foundation/IO/Implementation/Posix/DirectoryWatcher_posix.h>
 #else
---
+++ Code/Engine/Foundation/IO/Implementation/Win/DirectoryWatcher_uwp.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Foundation/IO/DirectoryWatcher.h>
+
+ezDirectoryWatcher::ezDirectoryWatcher()
+  : m_pImpl(nullptr)
+{
+}
+
+ezResult ezDirectoryWatcher::OpenDirectory(const ezString& absolutePath, ezBitflags<Watch> whatToWatch)
+{
+  return EZ_FAILURE;
+}
+
+void ezDirectoryWatcher::CloseDirectory()
+{
+}
+
+ezDirectoryWatcher::~ezDirectoryWatcher()
+{
+  CloseDirectory();
+}
+
+void ezDirectoryWatcher::EnumerateChanges(ezDelegate<void(const char* filename, ezDirectoryWatcherAction action)> func)
+{
+}
