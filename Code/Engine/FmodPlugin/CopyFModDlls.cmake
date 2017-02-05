2f83df1007f4d116abce30b7153dc72f22499fc3
Sanakan8472 <christopher.manthei@gmail.com>
2017-02-05 13:30:00 +0000

    *********************** * PUBLIC ANNOUNCEMENT * *********************** Qt is not at 5.8.0, new builds are in shared folder.
    -Fixed physX dll copy for 86 bit.
    -Added fmod dll copy step.

---
+++ Code/Engine/FmodPlugin/CopyFModDlls.cmake
@@ -0,0 +1,39 @@
+
+# Function parameter output for debugging:
+#message(STATUS "${MSVC_BUILD_CONFIG_DIR}")
+#message(STATUS "${BUILDSYSTEM_PLATFORM_64BIT}")
+#message(STATUS "${FMOD_SOURCE}")
+#message(STATUS "${FMOD_TARGET}")
+
+SET (FMOD_EXT "")
+SET (FMOD_EXTL "")
+
+if (MSVC_BUILD_CONFIG_DIR STREQUAL "Debug")
+  # Enable if you need the debug libraries in a debug build
+  #SET (FMOD_EXTL "${FMOD_EXTL}L")
+endif ()
+
+if (BUILDSYSTEM_PLATFORM_64BIT STREQUAL "ON")
+  SET (FMOD_EXT "${FMOD_EXT}64")
+  SET (FMOD_EXTL "${FMOD_EXTL}64")
+endif ()
+
+SET (FMOD_EXT "${FMOD_EXT}.dll")
+SET (FMOD_EXTL "${FMOD_EXTL}.dll")
+
+file(COPY "${FMOD_SOURCE}/api/fsbank/lib/fsbank${FMOD_EXT}"
+        DESTINATION "${FMOD_TARGET}/")
+        
+file(COPY "${FMOD_SOURCE}/api/fsbank/lib/libfsbvorbis${FMOD_EXT}"
+        DESTINATION "${FMOD_TARGET}/")
+file(COPY "${FMOD_SOURCE}/api/fsbank/lib/libmp3lame${FMOD_EXT}"
+        DESTINATION "${FMOD_TARGET}/")
+file(COPY "${FMOD_SOURCE}/api/fsbank/lib/twolame${FMOD_EXT}"
+        DESTINATION "${FMOD_TARGET}/")
+
+file(COPY "${FMOD_SOURCE}/api/lowlevel/lib/fmod${FMOD_EXTL}"
+        DESTINATION "${FMOD_TARGET}/")
+        
+file(COPY "${FMOD_SOURCE}/api/studio/lib/fmodstudio${FMOD_EXTL}"
+        DESTINATION "${FMOD_TARGET}/")
+        
--- Code/Engine/PhysXPlugin/CopyPhysXDlls.cmake
+++ Code/Engine/PhysXPlugin/CopyPhysXDlls.cmake
@@ -1,29 +1,29 @@
 
 # Function parameter output for debugging:
 #message(STATUS "${MSVC_BUILD_CONFIG_DIR}")
+#message(STATUS "${BUILDSYSTEM_PLATFORM_64BIT}")
 #message(STATUS "${PHYSX_SOURCE}")
 #message(STATUS "${PHYSX_TARGET}")
 
 file(COPY "${PHYSX_SOURCE}/nvToolsExt64_1.dll"
         DESTINATION "${PHYSX_TARGET}/")
 
-if (MSVC_BUILD_CONFIG_DIR STREQUAL "Debug")
-  file(COPY "${PHYSX_SOURCE}/PhysX3CharacterKinematicDEBUG_x64.dll"
-          DESTINATION "${PHYSX_TARGET}/")
-  file(COPY "${PHYSX_SOURCE}/PhysX3CommonDEBUG_x64.dll"
-          DESTINATION "${PHYSX_TARGET}/")
-  file(COPY "${PHYSX_SOURCE}/PhysX3CookingDEBUG_x64.dll"
-          DESTINATION "${PHYSX_TARGET}/")
-  file(COPY "${PHYSX_SOURCE}/PhysX3DEBUG_x64.dll"
-          DESTINATION "${PHYSX_TARGET}/")
-else ()
-  file(COPY "${PHYSX_SOURCE}/PhysX3CharacterKinematic_x64.dll"
-          DESTINATION "${PHYSX_TARGET}/")
-  file(COPY "${PHYSX_SOURCE}/PhysX3Common_x64.dll"
-          DESTINATION "${PHYSX_TARGET}/")
-  file(COPY "${PHYSX_SOURCE}/PhysX3Cooking_x64.dll"
-          DESTINATION "${PHYSX_TARGET}/")
-  file(COPY "${PHYSX_SOURCE}/PhysX3_x64.dll"
-          DESTINATION "${PHYSX_TARGET}/")
+SET (PHYSX_EXT "")
+if (BUILDSYSTEM_PLATFORM_64BIT STREQUAL "Debug")
+  SET (PHYSX_EXT "${PHYSX_EXT}DEBUG")
 endif ()
 
+if (BUILDSYSTEM_PLATFORM_64BIT STREQUAL "ON")
+  SET (PHYSX_EXT "${PHYSX_EXT}_x64.dll")
+else ()
+  SET (PHYSX_EXT "${PHYSX_EXT}_x86.dll")
+endif ()
+
+file(COPY "${PHYSX_SOURCE}/PhysX3CharacterKinematic${PHYSX_EXT}"
+        DESTINATION "${PHYSX_TARGET}/")
+file(COPY "${PHYSX_SOURCE}/PhysX3Common${PHYSX_EXT}"
+        DESTINATION "${PHYSX_TARGET}/")
+file(COPY "${PHYSX_SOURCE}/PhysX3Cooking${PHYSX_EXT}"
+        DESTINATION "${PHYSX_TARGET}/")
+file(COPY "${PHYSX_SOURCE}/PhysX3${PHYSX_EXT}"
+        DESTINATION "${PHYSX_TARGET}/")
