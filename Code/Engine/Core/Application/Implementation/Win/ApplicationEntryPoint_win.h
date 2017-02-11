76c57616b564b8167bc6c5715dadd51a973db8c3
wumpf <r_andreas2@web.de>
2017-02-11 18:00:57 +0000

    Win32 application entry point is now a template function for the most part to be able to add #ifdef for UWP port.

--- Code/Engine/Core/Application/Implementation/Win/ApplicationEntryPoint_win.h
+++ Code/Engine/Core/Application/Implementation/Win/ApplicationEntryPoint_win.h
@@ -5,26 +5,70 @@
 
 #include <Foundation/Memory/MemoryTracker.h>
 
+namespace Details
+{
+  template<typename AppClass, typename ...Args>
+  int ConsoleEntry(int argc, const char** argv, Args&&... arguments)
+  {
+    static char appBuffer[sizeof(AppClass)]; // Not on the stack to cope with smaller stacks.
+
+    AppClass* pApp = new (appBuffer) AppClass(std::forward<Args>(arguments)...);
+    pApp->SetCommandLineArguments((ezUInt32)argc, argv);
+    ezRun(pApp); // Life cycle & run method calling
+
+    const int iReturnCode = pApp->GetReturnCode();
+    if (iReturnCode != 0)
+    {
+      std::string text = pApp->TranslateReturnCode();
+      if (!text.empty())
+        printf("Return Code: '%s'\n", text.c_str());
+    }
+
+    const bool memLeaks = pApp->IsMemoryLeakReportingEnalbed();
+    pApp->~AppClass();
+    memset(pApp, 0, sizeof(AppClass));
+    if (memLeaks)
+      ezMemoryTracker::DumpMemoryLeaks();
+
+    return iReturnCode;
+  }
+
+  template<typename AppClass, typename ...Args>
+  int ApplicationEntry(Args&&... arguments)
+  {
+    static char appBuffer[sizeof(AppClass)]; // Not on the stack to cope with smaller stacks.
+
+    AppClass* pApp = new (appBuffer) AppClass(std::forward<Args>(arguments)...);
+    // __argc/__argv is not available on UWP
+#if EZ_DISABLED(EZ_PLATFORM_WINDOWS_UWP)
+    pApp->SetCommandLineArguments((ezUInt32)__argc, const_cast<const char**>(__argv));
+#endif
+    ezRun(pApp); //Life cycle & run method calling
+
+    const int iReturnCode = pApp->GetReturnCode();
+    if (iReturnCode != 0)
+    {
+      std::string text = pApp->TranslateReturnCode(); \
+        if (!text.empty()) printf("Return Code: '%s'\n", text.c_str());
+    }
+
+    const bool memLeaks = pApp->IsMemoryLeakReportingEnalbed();
+    pApp->~AppClass();
+    memset(pApp, 0, sizeof(AppClass));
+    if (memLeaks)
+      ezMemoryTracker::DumpMemoryLeaks();
+
+    return iReturnCode;
+  }
+}
+
 /// \brief Same as EZ_APPLICATION_ENTRY_POINT but should be used for applications that shall always show a console window.
 #define EZ_CONSOLEAPP_ENTRY_POINT(AppClass, ...) \
   static char appBuffer[sizeof(AppClass)]; /* Not on the stack to cope with smaller stacks */ \
   \
   int main(int argc, const char** argv) \
   { \
-    \
-    AppClass* pApp = new (appBuffer) AppClass(__VA_ARGS__); \
-    pApp->SetCommandLineArguments((ezUInt32) argc, argv); \
-    ezRun(pApp); /* Life cycle & run method calling */ \
-    const int iReturnCode = pApp->GetReturnCode(); \
-    if (iReturnCode != 0) { \
-      std::string text = pApp->TranslateReturnCode(); \
-      if (!text.empty()) printf("Return Code: '%s'\n", text.c_str()); \
-    } \
-    const bool memLeaks = pApp->IsMemoryLeakReportingEnalbed(); \
-    pApp->~AppClass(); \
-    memset(pApp, 0, sizeof(AppClass)); \
-    if (memLeaks) ezMemoryTracker::DumpMemoryLeaks(); \
-    return iReturnCode; \
+    Details::ConsoleEntry<AppClass>(argc, argv, __VA_ARGS__); \
   }
 
 /// \brief This macro allows for easy creation of application entry points (since they can't be placed in DLLs)
@@ -32,23 +76,7 @@
 /// Just use the macro in a cpp file of your application and supply your app class (must be derived from ezApplication).
 /// The additional (optional) parameters are passed to the constructor of your app class.
 #define EZ_APPLICATION_ENTRY_POINT(AppClass, ...) \
-  static char appBuffer[sizeof(AppClass)]; /* Not on the stack to cope with smaller stacks */ \
-  \
   int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) \
   { \
-    \
-    AppClass* pApp = new (appBuffer) AppClass(__VA_ARGS__); \
-    pApp->SetCommandLineArguments((ezUInt32) __argc, const_cast<const char**>(__argv)); \
-    ezRun(pApp); /* Life cycle & run method calling */ \
-    const int iReturnCode = pApp->GetReturnCode(); \
-    if (iReturnCode != 0) { \
-      std::string text = pApp->TranslateReturnCode(); \
-      if (!text.empty()) printf("Return Code: '%s'\n", text.c_str()); \
-    } \
-    const bool memLeaks = pApp->IsMemoryLeakReportingEnalbed(); \
-    pApp->~AppClass(); \
-    memset(pApp, 0, sizeof(AppClass)); \
-    if (memLeaks) ezMemoryTracker::DumpMemoryLeaks(); \
-    return iReturnCode; \
+    return Details::ApplicationEntry<AppClass>(__VA_ARGS__); \
   }
-
