547c18a8a708a10bafc61bdbeedb701d0dc09354
wumpf <r_andreas2@web.de>
2017-06-18 11:50:34 +0000

    Cmake Hololens fixes: * UWP Apps target now windows anniversary update (instead of newest available version) * Added hololens as target platform in all appx manifests * SpatialPerception capability is now set if WindowsMixedReality library is linked in an application.

--- Code/BuildSystem/CMake/toolchain-winstore.cmake
+++ Code/BuildSystem/CMake/toolchain-winstore.cmake
@@ -1,2 +1,5 @@
 set(CMAKE_SYSTEM_NAME WindowsStore)
-set(CMAKE_SYSTEM_VERSION 10.0)
+
+# Windows anniversary update version. As of writing the newest hololens emulator does not support Windows Creator's update.
+set(CMAKE_SYSTEM_VERSION 10.0.14393.0)
+set(CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION 10.0.14393.0)
