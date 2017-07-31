3b37dcdf1244c864a9d243a2685923289babb42c
jankrassnigg <jan@krassnigg.de>
2017-07-31 20:57:32 +0000

    Fmod on UWP now compiles and links. Didn't do a functional test yet. DLLs are in the usual place.

--- Code/Engine/FmodPlugin/CopyFModDlls.cmake
+++ Code/Engine/FmodPlugin/CopyFModDlls.cmake
@@ -2,41 +2,66 @@
 # Function parameter output for debugging:
 #message(STATUS "${MSVC_BUILD_CONFIG_DIR}")
 #message(STATUS "${BUILDSYSTEM_PLATFORM_64BIT}")
-#message(STATUS "${FMOD_SOURCE}")
-#message(STATUS "${FMOD_TARGET}")
+#message(STATUS "${BUILDSYSTEM_PLATFORM_WINDOWS_DESKTOP}")
+#message(STATUS "${BUILDSYSTEM_PLATFORM_WINDOWS_UWP}")
+
+# ATTENTION: The parameters (BUILDSYSTEM_...) are not available automatically.
+# They must be passed through by the CMakeLists.txt that calls this script.
 
 SET (FMOD_EXT "")
 SET (FMOD_EXTL "")
 
-if (MSVC_BUILD_CONFIG_DIR STREQUAL "Debug")
-  # Enable if you need the debug libraries in a debug build
-  #SET (FMOD_EXTL "${FMOD_EXTL}L")
-endif ()
+if (BUILDSYSTEM_PLATFORM_WINDOWS_DESKTOP STREQUAL "ON")
 
-if (BUILDSYSTEM_PLATFORM_64BIT STREQUAL "ON")
-  SET (FMOD_EXT "${FMOD_EXT}64")
-  SET (FMOD_EXTL "${FMOD_EXTL}64")
-endif ()
+	if (MSVC_BUILD_CONFIG_DIR STREQUAL "Debug")
+	  # Enable if you need the debug libraries in a debug build
+	  #SET (FMOD_EXTL "${FMOD_EXTL}L")
+	endif ()
 
-SET (FMOD_EXT "${FMOD_EXT}.dll")
-SET (FMOD_EXTL "${FMOD_EXTL}.dll")
+	if (BUILDSYSTEM_PLATFORM_64BIT STREQUAL "ON")
+	  SET (FMOD_EXT "${FMOD_EXT}64")
+	  SET (FMOD_EXTL "${FMOD_EXTL}64")
+	endif ()
+
+	SET (FMOD_EXT "${FMOD_EXT}.dll")
+	SET (FMOD_EXTL "${FMOD_EXTL}.dll")
+
+elseif (BUILDSYSTEM_PLATFORM_WINDOWS_UWP STREQUAL "ON")
+
+	if (MSVC_BUILD_CONFIG_DIR STREQUAL "Debug")
+	  # Enable if you need the debug libraries in a debug build
+	  #SET (FMOD_EXTL "${FMOD_EXTL}L")
+	endif ()
+
+	if (BUILDSYSTEM_PLATFORM_64BIT STREQUAL "ON")
+	  SET (FMOD_EXT "${FMOD_EXT}64")
+	  SET (FMOD_EXTL "${FMOD_EXTL}_X64")
+	else()
+	  SET (FMOD_EXT "${FMOD_EXT}")
+	  SET (FMOD_EXTL "${FMOD_EXTL}_X86")
+	endif ()
+
+	SET (FMOD_EXT "${FMOD_EXT}.dll")
+	SET (FMOD_EXTL "${FMOD_EXTL}.dll")
+
+else()
+
+	#Unknown platform.
+
+endif()
+
 
 # fsbank libraries
 file(COPY "${FMOD_SOURCE}/api/fsbank/lib/fsbank${FMOD_EXT}"
         DESTINATION "${FMOD_TARGET}/")
 file(COPY "${FMOD_SOURCE}/api/fsbank/lib/libfsbvorbis${FMOD_EXT}"
         DESTINATION "${FMOD_TARGET}/")
-# mp3 not included in fmod anymore
-#file(COPY "${FMOD_SOURCE}/api/fsbank/lib/libmp3lame${FMOD_EXT}"
-#        DESTINATION "${FMOD_TARGET}/")
-#file(COPY "${FMOD_SOURCE}/api/fsbank/lib/twolame${FMOD_EXT}"
-#        DESTINATION "${FMOD_TARGET}/")
-
 # lowlevel libraries
 file(COPY "${FMOD_SOURCE}/api/lowlevel/lib/fmod${FMOD_EXTL}"
         DESTINATION "${FMOD_TARGET}/")
-        
 # fmod studio libraries
 file(COPY "${FMOD_SOURCE}/api/studio/lib/fmodstudio${FMOD_EXTL}"
         DESTINATION "${FMOD_TARGET}/")
-        
+		
+
+		
