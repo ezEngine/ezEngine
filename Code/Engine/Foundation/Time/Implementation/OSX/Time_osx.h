637f26ebd62d90a3dca778fea7bb76bd81a2b9c1
jankrassnigg <jan@krassnigg.de>
2018-04-19 17:22:17 +0000

    Precompute conversion in ezTime::Now(). May also make the tests work again.

--- Code/Engine/Foundation/Time/Implementation/OSX/Time_osx.h
+++ Code/Engine/Foundation/Time/Implementation/OSX/Time_osx.h
@@ -3,16 +3,18 @@
 #include <mach/mach.h>
 #include <mach/mach_time.h>
 
-static mach_timebase_info_data_t g_TimebaseInfo;
+static double g_TimeFactor = 0;
 
 void ezTime::Initialize()
 {
-  mach_timebase_info(&g_TimebaseInfo);
+  mach_timebase_info_data_t TimebaseInfo;
+  mach_timebase_info(&TimebaseInfo);
+  g_TimeFactor = (double)TimebaseInfo.numer / (double)TimebaseInfo.denom / (double)1000000000LL;
 }
 
 ezTime ezTime::Now()
 {
   // mach_absolute_time() returns nanoseconds after factoring in the mach_timebase_info_data_t
-  return ezTime::Seconds( (double)((mach_absolute_time() * g_TimebaseInfo.numer) / g_TimebaseInfo.denom) / 1000000000LL);
+  return ezTime::Seconds((double)mach_absolute_time() * g_TimeFactor);
 }
 
