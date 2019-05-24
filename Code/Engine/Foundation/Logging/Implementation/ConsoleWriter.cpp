cd3706ea2bb3c8cec2eebd2a8e49132aea354222
Jan Krassnigg <Jan@Krassnigg.de>
2019-05-24 11:50:43 +0200

    Console color test

--- Code/Engine/Foundation/Logging/Implementation/ConsoleWriter.cpp
+++ Code/Engine/Foundation/Logging/Implementation/ConsoleWriter.cpp
@@ -6,7 +6,7 @@
 static void SetConsoleColor(WORD ui)
 {
 #  if EZ_DISABLED(EZ_PLATFORM_WINDOWS_UWP)
-  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ui);
+  // SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ui);
 #  endif
 }
 #elif EZ_ENABLED(EZ_PLATFORM_OSX) || EZ_ENABLED(EZ_PLATFORM_LINUX)
@@ -15,6 +15,14 @@ static void SetConsoleColor(ezUInt8 ui) {}
 #  error "Unknown Platform."
 static void SetConsoleColor(ezUInt8 ui) {}
 #endif
+
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
 void ezLogWriter::Console::LogMessageHandler(const ezLoggingEventData& eventData)
 {
@@ -31,43 +39,43 @@ void ezLogWriter::Console::LogMessageHandler(const ezLoggingEventData& eventData
   {
     case ezLogMsgType::BeginGroup:
       SetConsoleColor(0x02);
-      printf("+++++ %s (%s) +++++\n", eventData.m_szText, eventData.m_szTag);
+      printf(ANSI_COLOR_MAGENTA "+++++ %s (%s) +++++\n", eventData.m_szText, eventData.m_szTag);
       break;
     case ezLogMsgType::EndGroup:
       SetConsoleColor(0x02);
 #if EZ_ENABLED(EZ_COMPILE_FOR_DEVELOPMENT)
-      printf("----- %s (%.6f sec)-----\n\n", eventData.m_szText, eventData.m_fSeconds);
+      printf(ANSI_COLOR_MAGENTA "----- %s (%.6f sec)-----\n\n", eventData.m_szText, eventData.m_fSeconds);
 #else
-      printf("----- %s (%s)-----\n\n", eventData.m_szText, "timing info not available");
+      printf(ANSI_COLOR_MAGENTA "----- %s (%s)-----\n\n", eventData.m_szText, "timing info not available");
 #endif
       break;
     case ezLogMsgType::ErrorMsg:
       SetConsoleColor(0x0C);
-      fprintf(stderr, "Error: %s\n", eventData.m_szText);
+      printf(ANSI_COLOR_RED "Error: %s\n", eventData.m_szText);
       break;
     case ezLogMsgType::SeriousWarningMsg:
       SetConsoleColor(0x0C);
-      fprintf(stderr, "Seriously: %s\n", eventData.m_szText);
+      printf(ANSI_COLOR_YELLOW "Seriously: %s\n", eventData.m_szText);
       break;
     case ezLogMsgType::WarningMsg:
       SetConsoleColor(0x0E);
-      printf("Warning: %s\n", eventData.m_szText);
+      printf(ANSI_COLOR_YELLOW "Warning: %s\n", eventData.m_szText);
       break;
     case ezLogMsgType::SuccessMsg:
       SetConsoleColor(0x0A);
-      printf("%s\n", eventData.m_szText);
+      printf(ANSI_COLOR_GREEN "%s\n", eventData.m_szText);
       break;
     case ezLogMsgType::InfoMsg:
       SetConsoleColor(0x07);
-      printf("%s\n", eventData.m_szText);
+      printf(ANSI_COLOR_RESET "%s\n", eventData.m_szText);
       break;
     case ezLogMsgType::DevMsg:
       SetConsoleColor(0x08);
-      printf("%s\n", eventData.m_szText);
+      printf(ANSI_COLOR_BLUE "%s\n", eventData.m_szText);
       break;
     case ezLogMsgType::DebugMsg:
       SetConsoleColor(0x09);
-      printf("%s\n", eventData.m_szText);
+      printf(ANSI_COLOR_CYAN "%s\n", eventData.m_szText);
       break;
     default:
       SetConsoleColor(0x0D);
@@ -78,6 +86,7 @@ void ezLogWriter::Console::LogMessageHandler(const ezLoggingEventData& eventData
   }
 
   SetConsoleColor(0x07);
+  printf(ANSI_COLOR_RESET);
 }
 
 
--- Code/Engine/TestFramework/Utilities/ConsoleOutput.h
+++ Code/Engine/TestFramework/Utilities/ConsoleOutput.h
@@ -6,12 +6,20 @@
 inline void SetConsoleColorInl(WORD ui)
 {
 #  if EZ_DISABLED(EZ_PLATFORM_WINDOWS_UWP)
-  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ui);
+  // SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ui);
 #  endif
 }
 #else
 inline void SetConsoleColorInl(ezUInt8 ui) {}
 #endif
+
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
 inline void OutputToConsole(ezTestOutput::Enum Type, const char* szMsg)
 {
@@ -32,29 +40,42 @@ inline void OutputToConsole(ezTestOutput::Enum Type, const char* szMsg)
       break;
     case ezTestOutput::Details:
       SetConsoleColorInl(0x07);
+      fprintf(outStream, ANSI_COLOR_CYAN);
       break;
     case ezTestOutput::ImportantInfo:
       SetConsoleColorInl(0x07);
+      fprintf(outStream, ANSI_COLOR_YELLOW);
       break;
     case ezTestOutput::Success:
       SetConsoleColorInl(0x0A);
+      fprintf(outStream, ANSI_COLOR_GREEN);
       break;
     case ezTestOutput::Message:
       SetConsoleColorInl(0x0E);
+      fprintf(outStream, ANSI_COLOR_RESET);
       break;
     case ezTestOutput::Warning:
       SetConsoleColorInl(0x0C);
+      fprintf(outStream, ANSI_COLOR_YELLOW);
       break;
     case ezTestOutput::Error:
       SetConsoleColorInl(0x0C);
       bAnyError = true;
       outStream = stderr;
+      fprintf(outStream, ANSI_COLOR_RED);
       break;
     case ezTestOutput::FinalResult:
       if (bAnyError)
+      {
         SetConsoleColorInl(0x0C);
+        fprintf(outStream, ANSI_COLOR_RED);
+      }
       else
+      {
+
+        fprintf(outStream, ANSI_COLOR_GREEN);
         SetConsoleColorInl(0x0A);
+      }
 
       // reset it for the next test round
       bAnyError = false;
@@ -65,6 +86,7 @@ inline void OutputToConsole(ezTestOutput::Enum Type, const char* szMsg)
 
   fprintf(outStream, "%*s%s\n", iIndentation, "", szMsg);
   SetConsoleColorInl(0x07);
+  fprintf(outStream, ANSI_COLOR_RESET);
 
 #if EZ_ENABLED(EZ_PLATFORM_WINDOWS)
   char sz[4096];
