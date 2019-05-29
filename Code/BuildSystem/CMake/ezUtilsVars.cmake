4a1e7f829c468cbbb97208dab2671a2e71879d22
Jan Krassnigg <Jan@Krassnigg.de>
2019-05-29 13:33:57 +0200

    Fixes CMake detection of DLL support

--- Code/BuildSystem/CMake/ezUtilsVars.cmake
+++ Code/BuildSystem/CMake/ezUtilsVars.cmake
@@ -1,3 +1,13 @@
+######################################
+### General settings
+######################################
+
+if (CMAKE_GENERATOR MATCHES "Visual Studio")
+    set (EZ_COMPILE_ENGINE_AS_DLL ON CACHE BOOL "Whether to compile the code as a shared libraries (DLL).")
+    mark_as_advanced(FORCE EZ_COMPILE_ENGINE_AS_DLL)
+else()
+    unset(EZ_COMPILE_ENGINE_AS_DLL CACHE)
+endif()
 
 ######################################
 ### Output directories
