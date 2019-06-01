ac933eca91daee2f79fe2a26ee04339fef077ce1
Sanakan8472 <christopher.manthei@gmail.com>
2019-06-01 21:55:15 +0200

    Lua fixes for UWP.

--- Code/ThirdParty/Lua/luaconf.h
+++ Code/ThirdParty/Lua/luaconf.h
@@ -11,6 +11,9 @@
 #include <limits.h>
 #include <stddef.h>
 
+#ifdef BUILDSYSTEM_COMPILE_ENGINE_AS_DLL
+  #define LUA_BUILD_AS_DLL
+#endif
 
 /*
 ** ===================================================================
@@ -48,7 +51,15 @@
 ** By default, Lua on Windows use (some) specific Windows features
 */
 #if !defined(LUA_USE_C89) && defined(_WIN32) && !defined(_WIN32_WCE)
+
+#if WINAPI_FAMILY != WINAPI_FAMILY_APP
 #define LUA_USE_WINDOWS  /* enable goodies for regular Windows */
+#else
+  // system and getenv are not supported on UWP
+  inline char* getenv(const char* name) { return "NOT SUPPORTED ON UWP"; }
+  inline int system(const char* command) { return 0; }
+#endif
+
 #endif
 
 
