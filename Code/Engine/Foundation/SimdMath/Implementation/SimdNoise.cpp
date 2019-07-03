18d506149f1197840ae6068d0eec18aff2a95333
C-Core <clemenskern@gmail.com>
2019-07-03 22:24:15 +0200

    Improved code readablilty, added some comments from original paper implementation

--- Code/Engine/Foundation/SimdMath/Implementation/SimdNoise.cpp
+++ Code/Engine/Foundation/SimdMath/Implementation/SimdNoise.cpp
@@ -54,25 +54,28 @@ namespace
 
   EZ_FORCE_INLINE ezSimdVec4f Grad(ezSimdVec4i hash, const ezSimdVec4f& x, const ezSimdVec4f& y, const ezSimdVec4f& z)
   {
+    // convert low 4 bits of hash code into 12 gradient directions.
     const ezSimdVec4i h = hash & ezSimdVec4i(15);
     const ezSimdVec4f u = ezSimdVec4f::Select(h < ezSimdVec4i(8), x, y);
     const ezSimdVec4f v = ezSimdVec4f::Select(h < ezSimdVec4i(4), y, ezSimdVec4f::Select(h == ezSimdVec4i(12) || h == ezSimdVec4i(14), x, z));
     return ezSimdVec4f::Select((h & ezSimdVec4i(1)) == ezSimdVec4i::ZeroVector(), u, -u) + ezSimdVec4f::Select((h & ezSimdVec4i(2)) == ezSimdVec4i::ZeroVector(), v, -v);
   }
 
-  EZ_FORCE_INLINE ezSimdVec4f Lerp(const ezSimdVec4f& t, const ezSimdVec4f& a, const ezSimdVec4f& b)
+  EZ_ALWAYS_INLINE ezSimdVec4f Lerp(const ezSimdVec4f& t, const ezSimdVec4f& a, const ezSimdVec4f& b)
   {
     return ezSimdVec4f::Lerp(a, b, t);
   }
 
 } // namespace
 
+// reference: https://mrl.nyu.edu/~perlin/noise/
 ezSimdVec4f ezSimdPerlinNoise::Noise(const ezSimdVec4f& inX, const ezSimdVec4f& inY, const ezSimdVec4f& inZ)
 {
   ezSimdVec4f x = inX;
   ezSimdVec4f y = inY;
   ezSimdVec4f z = inZ;
 
+  // find unit cube that contains point.
   const ezSimdVec4f xFloored = x.Floor();
   const ezSimdVec4f yFloored = y.Floor();
   const ezSimdVec4f zFloored = z.Floor();
@@ -82,14 +85,17 @@ ezSimdVec4f ezSimdPerlinNoise::Noise(const ezSimdVec4f& inX, const ezSimdVec4f& 
   const ezSimdVec4i Y = ezSimdVec4i::Truncate(yFloored) & maxIndex;
   const ezSimdVec4i Z = ezSimdVec4i::Truncate(zFloored) & maxIndex;
 
+  // find relative x,y,z of point in cube.
   x -= xFloored;
   y -= yFloored;
   z -= zFloored;
 
+  // compute fade curves for each of x,y,z.
   const ezSimdVec4f u = Fade(x);
   const ezSimdVec4f v = Fade(y);
   const ezSimdVec4f w = Fade(z);
 
+  // hash coordinates of the 8 cube corners
   const ezSimdVec4i i1 = ezSimdVec4i(1);
   const ezSimdVec4i A = Permute(X) + Y;
   const ezSimdVec4i AA = Permute(A) + Z;
@@ -99,8 +105,21 @@ ezSimdVec4f ezSimdPerlinNoise::Noise(const ezSimdVec4f& inX, const ezSimdVec4f& 
   const ezSimdVec4i BB = Permute(B + i1) + Z;
 
   const ezSimdVec4f f1 = ezSimdVec4f(1.0f);
-  return Lerp(w, Lerp(v, Lerp(u, Grad(Permute(AA), x, y, z), Grad(Permute(BA), x - f1, y, z)), Lerp(u, Grad(Permute(AB), x, y - f1, z), Grad(Permute(BB), x - f1, y - f1, z))),
-    Lerp(v, Lerp(u, Grad(Permute(AA + i1), x, y, z - f1), Grad(Permute(BA + i1), x - f1, y, z - f1)),
-      Lerp(u, Grad(Permute(AB + i1), x, y - f1, z - f1),
-        Grad(Permute(BB + i1), x - f1, y - f1, z - f1))));
+
+  // and add blended results from 8 corners of cube.
+  const ezSimdVec4f c000 = Grad(Permute(AA), x, y, z);
+  const ezSimdVec4f c100 = Grad(Permute(BA), x - f1, y, z);
+  const ezSimdVec4f c010 = Grad(Permute(AB), x, y - f1, z);
+  const ezSimdVec4f c110 = Grad(Permute(BB), x - f1, y - f1, z);
+  const ezSimdVec4f c001 = Grad(Permute(AA + i1), x, y, z - f1);
+  const ezSimdVec4f c101 = Grad(Permute(BA + i1), x - f1, y, z - f1);
+  const ezSimdVec4f c011 = Grad(Permute(AB + i1), x, y - f1, z - f1);
+  const ezSimdVec4f c111 = Grad(Permute(BB + i1), x - f1, y - f1, z - f1);
+
+  const ezSimdVec4f c000_c100 = Lerp(u, c000, c100);
+  const ezSimdVec4f c010_c110 = Lerp(u, c010, c110);
+  const ezSimdVec4f c001_c101 = Lerp(u, c001, c101);
+  const ezSimdVec4f c011_c111 = Lerp(u, c011, c111);
+
+  return Lerp(w, Lerp(v, c000_c100, c010_c110), Lerp(v, c001_c101, c011_c111));
 }
