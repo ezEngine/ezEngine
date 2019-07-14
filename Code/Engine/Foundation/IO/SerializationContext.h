713f814e2d2a589b185ebd17b96f7adf0e02bdb8
C-Core <clemenskern@gmail.com>
2019-07-14 15:11:43 +0200

    Potential unity build fix for linux and macOS (#66)

--- Code/Engine/Foundation/IO/SerializationContext.h
+++ Code/Engine/Foundation/IO/SerializationContext.h
@@ -11,27 +11,29 @@ class EZ_FOUNDATION_DLL ezSerializationContext
   EZ_DISALLOW_COPY_AND_ASSIGN(ezSerializationContext);
 
 public:
-  ezSerializationContext() { SetContext(this); }
-  ~ezSerializationContext() { SetContext(nullptr); }
-
-  static Derived* GetContext();
-
-protected:
-  static void SetContext(ezSerializationContext* pContext);
+  ezSerializationContext() { Derived::SetContext(this); }
+  ~ezSerializationContext() { Derived::SetContext(nullptr); }
 };
 
-/// \brief Implements the necessary functions to access a serialization context through GetContext.
-#define EZ_IMPLEMENT_SERIALIZATION_CONTEXT(type)                                                                                           \
-  thread_local type* EZ_CONCAT(s_pActiveContext, type);                                                                                    \
-  template <>                                                                                                                              \
-  type* ezSerializationContext<type>::GetContext()                                                                                         \
-  {                                                                                                                                        \
-    return EZ_CONCAT(s_pActiveContext, type);                                                                                              \
-  }                                                                                                                                        \
-  template <>                                                                                                                              \
-  void ezSerializationContext<type>::SetContext(ezSerializationContext* pContext)                                                          \
-  {                                                                                                                                        \
-    EZ_ASSERT_DEV(pContext == nullptr || EZ_CONCAT(s_pActiveContext, type) == nullptr, "Only one context can be active at a time.");       \
-    EZ_CONCAT(s_pActiveContext, type) = static_cast<type*>(pContext);                                                                      \
-  }
+/// \brief Declares the necessary functions to access a serialization context
+#define EZ_DECLARE_SERIALIZATION_CONTEXT(type) \
+public:                                        \
+  static type* GetContext();                   \
+                                               \
+protected:                                     \
+  friend class ezSerializationContext<type>;   \
+  static void SetContext(ezSerializationContext* pContext);
 
+
+/// \brief Implements the necessary functions to access a serialization context through GetContext.
+#define EZ_IMPLEMENT_SERIALIZATION_CONTEXT(type)                                                                                     \
+  thread_local type* EZ_CONCAT(s_pActiveContext, type);                                                                              \
+  type* type::GetContext()                                                                                                           \
+  {                                                                                                                                  \
+    return EZ_CONCAT(s_pActiveContext, type);                                                                                        \
+  }                                                                                                                                  \
+  void type::SetContext(ezSerializationContext* pContext)                                                                            \
+  {                                                                                                                                  \
+    EZ_ASSERT_DEV(pContext == nullptr || EZ_CONCAT(s_pActiveContext, type) == nullptr, "Only one context can be active at a time."); \
+    EZ_CONCAT(s_pActiveContext, type) = static_cast<type*>(pContext);                                                                \
+  }
