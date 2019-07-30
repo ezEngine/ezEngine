5bd0e3c23a9cc9c55739792a11ebc303c06d1f47
Jan Krassnigg <jakras@microsoft.com>
2019-07-30 09:20:52 +0200

    Adds ezMutex::TryAcquire

--- Code/Engine/Foundation/Threading/Implementation/Win/Mutex_win.h
+++ Code/Engine/Foundation/Threading/Implementation/Win/Mutex_win.h
@@ -14,14 +14,17 @@ extern "C"
   // First define two functions which are binary compatible with EnterCriticalSection and LeaveCriticalSection
   __declspec(dllimport) void __stdcall ezWinEnterCriticalSection(ezMutexHandle* handle);
   __declspec(dllimport) void __stdcall ezWinLeaveCriticalSection(ezMutexHandle* handle);
+  __declspec(dllimport) ezMinWindows::BOOL __stdcall ezWinTryEnterCriticalSection(ezMutexHandle* handle);
 
   // Now redirect them through linker flags to the correct implementation
 #  if EZ_ENABLED(EZ_PLATFORM_32BIT)
 #    pragma comment(linker, "/alternatename:__imp__ezWinEnterCriticalSection@4=__imp__EnterCriticalSection@4")
 #    pragma comment(linker, "/alternatename:__imp__ezWinLeaveCriticalSection@4=__imp__LeaveCriticalSection@4")
+#    pragma comment(linker, "/alternatename:__imp__ezWinTryEnterCriticalSection@4=__imp__TryEnterCriticalSection@4")
 #  else
 #    pragma comment(linker, "/alternatename:__imp_ezWinEnterCriticalSection=__imp_EnterCriticalSection")
 #    pragma comment(linker, "/alternatename:__imp_ezWinLeaveCriticalSection=__imp_LeaveCriticalSection")
+#    pragma comment(linker, "/alternatename:__imp_ezWinTryEnterCriticalSection=__imp_TryEnterCriticalSection")
 #  endif
 }
 
@@ -35,6 +38,17 @@ inline void ezMutex::Release()
 {
   --m_iLockCount;
   ezWinLeaveCriticalSection(&m_Handle);
+}
+
+inline bool ezMutex::TryAcquire()
+{
+  if (ezWinTryEnterCriticalSection(&m_Handle) != 0)
+  {
+    ++m_iLockCount;
+    return true;
+  }
+
+  return false;
 }
 
 #else
@@ -52,4 +66,16 @@ inline void ezMutex::Release()
   --m_iLockCount;
   LeaveCriticalSection((CRITICAL_SECTION*)&m_Handle);
 }
+
+inline bool ezMutex::TryAcquire()
+{
+  if (TryEnterCriticalSection((CRITICAL_SECTION*)&m_Handle) != 0)
+  {
+    ++m_iLockCount;
+    return true;
+  }
+
+  return false;
+}
+
 #endif
--- Code/UnitTests/FoundationTest/Threading/ThreadTest.cpp
+++ Code/UnitTests/FoundationTest/Threading/ThreadTest.cpp
@@ -2,6 +2,7 @@
 
 #include <Foundation/Profiling/Profiling.h>
 #include <Foundation/Threading/Thread.h>
+#include <Foundation/Threading/Mutex.h>
 #include <Foundation/Time/Time.h>
 
 namespace
@@ -17,8 +18,19 @@ namespace
     {
     }
 
+    ezMutex* m_pWaitMutex = nullptr;
+    ezMutex* m_pBlockedMutex = nullptr;
+
     virtual ezUInt32 Run()
     {
+      // test TryAcquire on a locked mutex
+      EZ_TEST_BOOL(m_pBlockedMutex->TryAcquire() == false);
+
+      {
+        // enter and leave the mutex once
+        EZ_LOCK(*m_pWaitMutex);
+      }
+
       EZ_PROFILE_SCOPE("Test Thread::Run");
 
       for (ezUInt32 i = 0; i < g_uiIncrementSteps; i++)
@@ -44,7 +56,7 @@ EZ_CREATE_SIMPLE_TEST(Threading, Thread)
 
   EZ_TEST_BLOCK(ezTestBlock::Enabled, "Thread")
   {
-    TestThread* pTestThread = nullptr;
+    TestThread* pTestThread1 = nullptr;
     TestThread* pTestThread2 = nullptr;
 
     /// the try-catch is necessary to quiet the static code analysis
@@ -52,7 +64,7 @@ EZ_CREATE_SIMPLE_TEST(Threading, Thread)
     try
 #endif
     {
-      pTestThread = new TestThread;
+      pTestThread1 = new TestThread;
       pTestThread2 = new TestThread;
     }
 #if EZ_ENABLED(EZ_TESTFRAMEWORK_SUPPORT_EXCEPTIONS)
@@ -61,22 +73,45 @@ EZ_CREATE_SIMPLE_TEST(Threading, Thread)
     }
 #endif
 
-    EZ_TEST_BOOL(pTestThread != nullptr);
+    EZ_TEST_BOOL(pTestThread1 != nullptr);
     EZ_TEST_BOOL(pTestThread2 != nullptr);
 
+    ezMutex waitMutex, blockedMutex;
+    pTestThread1->m_pWaitMutex = &waitMutex;
+    pTestThread2->m_pWaitMutex = &waitMutex;
+
+    pTestThread1->m_pBlockedMutex = &blockedMutex;
+    pTestThread2->m_pBlockedMutex = &blockedMutex;
+
+    // no one holds these mutexes yet, must succeed
+    EZ_TEST_BOOL(blockedMutex.TryAcquire() == true);
+    EZ_TEST_BOOL(waitMutex.TryAcquire() == true);
+
     // Both thread will increment the global variable via atomic operations
-    pTestThread->Start();
+    pTestThread1->Start();
     pTestThread2->Start();
+
+    // give the threads a bit of time to start
+    ezThreadUtils::Sleep(ezTime::Milliseconds(50));
+
+    // allow the threads to run now
+    waitMutex.Release();
 
     // Main thread will also increment the test variable
     ezAtomicUtils::Increment(g_iCrossThreadVariable);
 
     // Join with both threads
-    pTestThread->Join();
+    pTestThread1->Join();
     pTestThread2->Join();
 
+    // we are holding the mutex, another TryAcquire should work
+    EZ_TEST_BOOL(blockedMutex.TryAcquire() == true);
+
+    // The threads should have finished, no one holds the lock
+    EZ_TEST_BOOL(waitMutex.TryAcquire() == true);
+
     // Test deletion
-    delete pTestThread;
+    delete pTestThread1;
     delete pTestThread2;
 
     EZ_TEST_INT(g_iCrossThreadVariable, g_uiIncrementSteps * 2 + 1);
