56450041233a4770467021d79fe7631dbc1a0c34
Jan Krassnigg <Jan@Krassnigg.de>
2019-08-26 21:34:58 +0200

    spawn component

--- Code/Engine/GameEngine/Prefabs/SpawnComponent.h
+++ Code/Engine/GameEngine/Prefabs/SpawnComponent.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <GameEngine/GameEngineDLL.h>
 #include <Core/World/World.h>
+#include <GameEngine/GameEngineDLL.h>
 #include <GameEngine/Prefabs/PrefabResource.h>
 
 struct ezMsgComponentInternalTrigger;
@@ -13,10 +13,10 @@ struct ezSpawnComponentFlags
   enum Enum
   {
     None = 0,
-    SpawnAtStart = EZ_BIT(0), ///< The component will schedule a spawn once at creation time
+    SpawnAtStart = EZ_BIT(0),      ///< The component will schedule a spawn once at creation time
     SpawnContinuously = EZ_BIT(1), ///< Every time a scheduled spawn was done, a new one is scheduled
-    AttachAsChild = EZ_BIT(2), ///< All objects spawned will be attached as children to this node
-    SpawnInFlight = EZ_BIT(3), ///< [internal] A spawn trigger message has been posted.
+    AttachAsChild = EZ_BIT(2),     ///< All objects spawned will be attached as children to this node
+    SpawnInFlight = EZ_BIT(3),     ///< [internal] A spawn trigger message has been posted.
 
     Default = None
   };
@@ -33,15 +33,6 @@ EZ_DECLARE_FLAGS_OPERATORS(ezSpawnComponentFlags);
 
 typedef ezComponentManager<class ezSpawnComponent, ezBlockStorageType::Compact> ezSpawnComponentManager;
 
-/// \brief Message to initiate a spawn on ezSpawnComponent
-struct EZ_GAMEENGINE_DLL ezMsgTriggerSpawnComponent : public ezMessage
-{
-  EZ_DECLARE_MESSAGE_TYPE(ezMsgTriggerSpawnComponent, ezMessage);
-
-  /// If true ScheduleSpawn() is called, otherwise TriggerManualSpawn()
-  bool m_bContinuousSpawn = false;
-};
-
 class EZ_GAMEENGINE_DLL ezSpawnComponent : public ezComponent
 {
   EZ_DECLARE_COMPONENT_TYPE(ezSpawnComponent, ezComponent, ezSpawnComponentManager);
@@ -49,71 +40,67 @@ class EZ_GAMEENGINE_DLL ezSpawnComponent : public ezComponent
 public:
   ezSpawnComponent();
 
+  //////////////////////////////////////////////////////////////////////////
+  // ezComponent interface
+
+public:
   virtual void SerializeComponent(ezWorldWriter& stream) const override;
   virtual void DeserializeComponent(ezWorldReader& stream) override;
 
+protected:
+  virtual void OnSimulationStarted() override;
+
+  //////////////////////////////////////////////////////////////////////////
+  // ezSpawnComponent interface
+
+public:
   /// \brief Spawns a new object, unless the minimum spawn delay has not been reached between calls to this function.
   ///
   /// Manual spawns and continuous (scheduled) spawns are independent from each other regarding minimum spawn delays.
   /// If this function is called in too short intervals, it is ignored and false is returned.
   /// Returns true, if an object was spawned.
-  bool TriggerManualSpawn();
+  bool TriggerManualSpawn(); // [scriptable]
 
   /// \brief Unless a spawn is already scheduled, this will schedule one within the configured time frame.
   ///
   /// If continuous spawning is enabled, this will kick of the first spawn and then continue infinitely.
   /// To stop continuously spawning, remove the continuous spawn flag.
-  void ScheduleSpawn();
+  void ScheduleSpawn(); // [scriptable]
 
-  // ************************************* PROPERTIES ***********************************
+  void SetPrefabFile(const char* szFile); // [property]
+  const char* GetPrefabFile() const;      // [property]
 
-  void SetPrefabFile(const char* szFile);
-  const char* GetPrefabFile() const;
+  bool GetSpawnAtStart() const; // [property]
+  void SetSpawnAtStart(bool b); // [property]
 
-  bool GetSpawnAtStart() const { return m_SpawnFlags.IsAnySet(ezSpawnComponentFlags::SpawnAtStart); }
-  void SetSpawnAtStart(bool b) { m_SpawnFlags.AddOrRemove(ezSpawnComponentFlags::SpawnAtStart, b); }
+  bool GetSpawnContinuously() const; // [property]
+  void SetSpawnContinuously(bool b); // [property]
 
-  bool GetSpawnContinuously() const { return m_SpawnFlags.IsAnySet(ezSpawnComponentFlags::SpawnContinuously); }
-  void SetSpawnContinuously(bool b) { m_SpawnFlags.AddOrRemove(ezSpawnComponentFlags::SpawnContinuously, b); }
-
-  bool GetAttachAsChild() const { return m_SpawnFlags.IsAnySet(ezSpawnComponentFlags::AttachAsChild); }
-  void SetAttachAsChild(bool b) { m_SpawnFlags.AddOrRemove(ezSpawnComponentFlags::AttachAsChild, b); }
+  bool GetAttachAsChild() const; // [property]
+  void SetAttachAsChild(bool b); // [property]
 
   void SetPrefab(const ezPrefabResourceHandle& hPrefab);
   EZ_ALWAYS_INLINE const ezPrefabResourceHandle& GetPrefab() const { return m_hPrefab; }
 
   /// The minimum delay between spawning objects. This is also enforced for manually spawning things.
-  ezTime m_MinDelay;
+  ezTime m_MinDelay; // [property]
 
   /// For scheduled spawns (continuous / at start) this is an additional random range on top of the minimum spawn delay.
-  ezTime m_DelayRange;
+  ezTime m_DelayRange; // [property]
 
   /// The spawned object's orientation may deviate by this amount around the X axis. 180° is completely random orientation.
-  ezAngle m_MaxDeviation;
+  ezAngle m_MaxDeviation; // [property]
 
-  //////////////////////////////////////////////////////////////////////////
-  // Message Handlers
-
-  void OnSpawn(ezMsgTriggerSpawnComponent& msg);
-
-private:
-
-  void OnTriggered(ezMsgComponentInternalTrigger& msg);
 
 protected:
   ezBitflags<ezSpawnComponentFlags> m_SpawnFlags;
 
-  // ************************************* FUNCTIONS *****************************
-
-
-  virtual void OnSimulationStarted() override;
-
   bool SpawnOnce();
   virtual void DoSpawn(const ezTransform& tLocalSpawn);
 
-
 private:
+  void OnTriggered(ezMsgComponentInternalTrigger& msg);
+
   ezTime m_LastManualSpawn;
   ezPrefabResourceHandle m_hPrefab;
 };
-
