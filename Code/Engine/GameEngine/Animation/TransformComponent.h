1485bf8937463432b68d6eec2e4c939937587d3f
Jan Krassnigg <Jan@Krassnigg.de>
2019-08-26 20:12:24 +0200

    Improved transform component settings

--- Code/Engine/GameEngine/Animation/TransformComponent.h
+++ Code/Engine/GameEngine/Animation/TransformComponent.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <GameEngine/GameEngineDLL.h>
-#include <Core/World/World.h>
 #include <Core/World/Component.h>
+#include <Core/World/World.h>
 #include <Foundation/Time/Time.h>
+#include <GameEngine/GameEngineDLL.h>
 #include <GameEngine/VisualScript/VisualScriptNode.h>
 
 struct ezTransformComponentFlags
@@ -12,23 +12,21 @@ struct ezTransformComponentFlags
 
   enum Enum
   {
-    None                = 0,
-    Autorun             = EZ_BIT(0),
-    AutoReturnStart     = EZ_BIT(1),
-    AutoReturnEnd       = EZ_BIT(2),
-    AutoToggleDirection = EZ_BIT(3),
-    Paused              = EZ_BIT(4),
-    AnimationReversed   = EZ_BIT(5),
-    Default             = Autorun | AutoReturnStart | AutoReturnEnd | AutoToggleDirection
+    None = 0,
+    Running = EZ_BIT(0),
+    AutoReturnStart = EZ_BIT(1),
+    AutoReturnEnd = EZ_BIT(2),
+    AnimationReversed = EZ_BIT(5),
+    Default = Running | AutoReturnStart | AutoReturnEnd
   };
 
   struct Bits
   {
-    StorageType Autorun : 1;
+    StorageType Running : 1;
     StorageType AutoReturnStart : 1;
     StorageType AutoReturnEnd : 1;
-    StorageType AutoToggleDirection : 1;
-    StorageType Paused : 1;
+    StorageType Unused1 : 1;
+    StorageType Unused2 : 1;
     StorageType AnimationReversed : 1;
   };
 };
@@ -42,58 +40,33 @@ class EZ_GAMEENGINE_DLL ezTransformComponent : public ezComponent
 public:
   ezTransformComponent();
 
+  //////////////////////////////////////////////////////////////////////////
+  // ezComponent interface
+
+public:
   virtual void SerializeComponent(ezWorldWriter& stream) const override;
   virtual void DeserializeComponent(ezWorldReader& stream) override;
 
-  // ************************************* PROPERTIES ***********************************
+  //////////////////////////////////////////////////////////////////////////
+  // ezTransformComponent interface
 
-  bool GetAnimatingAtStartup(void) const { return (m_Flags.IsAnySet(ezTransformComponentFlags::Autorun)); }
-  void SetAnimatingAtStartup(bool b) { m_Flags.AddOrRemove(ezTransformComponentFlags::Autorun, b); }
+public:
+  void SetDirectionForwards(bool bForwards); // [scriptable]
+  void ToggleDirection();                    // [scriptable]
+  bool IsDirectionForwards() const;          // [scriptable]
 
-  bool GetAutoReturnStart(void) const { return (m_Flags.IsAnySet(ezTransformComponentFlags::AutoReturnStart)); }
-  void SetAutoReturnStart(bool b) { m_Flags.AddOrRemove(ezTransformComponentFlags::AutoReturnStart, b); }
+  bool IsRunning(void) const;     // [property]
+  void SetRunning(bool bRunning); // [property]
 
-  bool GetAutoReturnEnd(void) const { return (m_Flags.IsAnySet(ezTransformComponentFlags::AutoReturnEnd)); }
-  void SetAutoReturnEnd(bool b) { m_Flags.AddOrRemove(ezTransformComponentFlags::AutoReturnEnd, b); }
+  bool GetReverseAtStart(void) const; // [property]
+  void SetReverseAtStart(bool b);     // [property]
 
-  bool GetAutoToggleDirection(void) const { return (m_Flags.IsAnySet(ezTransformComponentFlags::AutoToggleDirection)); }
-  void SetAutoToggleDirection(bool b) { m_Flags.AddOrRemove(ezTransformComponentFlags::AutoToggleDirection, b); }
+  bool GetReverseAtEnd(void) const; // [property]
+  void SetReverseAtEnd(bool b);     // [property]
 
-  float Test_ReturnFloatNoParams() const;
-  void Test_StringUInt8(const char* szText, ezUInt8 val) const;
-  void Test_InOut(ezInt32& inout_Val, const ezInt32& constant, ezString& inoutString);
-  int Test_InAndOut(ezInt32& inRef, const ezInt32& inConstRef, ezInt32& outVal);
-
-  float m_fAnimationSpeed;
+  float m_fAnimationSpeed; // [property]
 
 protected:
   ezBitflags<ezTransformComponentFlags> m_Flags;
   ezTime m_AnimationTime;
-
-  // ************************************* FUNCTIONS *****************************
-
-public:
-  void ResumeAnimation();
-  void SetAnimationPaused(bool bPaused); // [scriptable]
-  void SetDirectionForwards(bool bForwards);
-  void ReverseDirection();
-  bool IsDirectionForwards() const;
-  bool IsAnimationRunning() const;
 };
-
-
-//////////////////////////////////////////////////////////////////////////
-
-class EZ_GAMEENGINE_DLL ezVisualScriptNode_TransformComponent : public ezVisualScriptNode
-{
-  EZ_ADD_DYNAMIC_REFLECTION(ezVisualScriptNode_TransformComponent, ezVisualScriptNode);
-public:
-  ezVisualScriptNode_TransformComponent();
-
-  virtual void Execute(ezVisualScriptInstance* pInstance, ezUInt8 uiExecPin) override;
-  virtual void* GetInputPinDataPointer(ezUInt8 uiPin) override;
-
-  ezComponentHandle m_hComponent;
-};
-
-
