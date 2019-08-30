203c2082a8576baec3d8e4e038dcafebf244a1ab
Jan Krassnigg <Jan@Krassnigg.de>
2019-08-30 23:04:48 +0200

    hacky proof of concept for interacting with the scene from a script

--- Code/EnginePlugins/TypeScriptPlugin/Components/TypeScriptComponentManager.cpp
+++ Code/EnginePlugins/TypeScriptPlugin/Components/TypeScriptComponentManager.cpp
@@ -1,5 +1,6 @@
 #include <TypeScriptPluginPCH.h>
 
+#include <Duktape/duktape.h>
 #include <TypeScriptPlugin/Components/TypeScriptComponent.h>
 
 ezTypeScriptComponentManager::ezTypeScriptComponentManager(ezWorld* pWorld)
@@ -20,6 +21,45 @@ int TS_ezLog_Info(duk_context* pContext)
   return wrapper.ReturnVoid();
 }
 
+int TS_GameObjectMove(duk_context* pContext)
+{
+  ezDuktapeFunction wrapper(pContext);
+  EZ_VERIFY(wrapper.IsParameterObject(0), "");
+
+  duk_get_prop_string(pContext, 0, "goPtr");
+  ezGameObject* pGo = (ezGameObject*)duk_get_pointer_default(pContext, -1, nullptr);
+
+  if (pGo)
+  {
+    ezVec3 pos(0, 0, 10);
+    pos.x = ezMath::Sin(ezAngle::Degree(ezTime::Now().GetSeconds() * 90)) * 5;
+    pGo->SetLocalPosition(pos);
+  }
+
+  return wrapper.ReturnVoid();
+}
+
+static ezWorld* s_pWorld = nullptr;
+static ezTypeScriptComponent* s_pTsComponent = nullptr;
+
+int TS_GetGameObject(duk_context* pContext)
+{
+  ezDuktapeFunction wrapper(pContext);
+
+  wrapper.BeginFunctionCall("_CreateGameObject");
+  wrapper.ExecuteFunctionCall();
+  EZ_VERIFY(wrapper.IsReturnValueObject(), "");
+
+  duk_dup_top(pContext);
+
+  wrapper.EndFunctionCall();
+
+  duk_push_pointer(pContext, s_pTsComponent->GetOwner());
+  duk_put_prop_string(pContext, -2, "goPtr");
+
+  return wrapper.ReturnCustom();
+}
+
 void ezTypeScriptComponentManager::Initialize()
 {
   SUPER::Initialize();
@@ -33,6 +73,8 @@ void ezTypeScriptComponentManager::Initialize()
   EZ_VERIFY(m_Script.ExecuteFile("TypeScript/typescriptServices.js").Succeeded(), "");
 
   m_Script.RegisterFunction("ezLog_Info", TS_ezLog_Info, 1);
+  m_Script.RegisterFunction("GetGameObject", TS_GetGameObject, 0);
+  m_Script.RegisterFunction("GameObjectMove", TS_GameObjectMove, 1);
 }
 
 void ezTypeScriptComponentManager::Deinitialize()
@@ -42,9 +84,14 @@ void ezTypeScriptComponentManager::Deinitialize()
 
 void ezTypeScriptComponentManager::Update(const ezWorldModule::UpdateContext& context)
 {
+  s_pWorld = GetWorld();
+
   for (auto it = this->m_ComponentStorage.GetIterator(context.m_uiFirstComponentIndex, context.m_uiComponentCount); it.IsValid(); ++it)
   {
     if (it->IsActiveAndSimulating())
+    {
+      s_pTsComponent = &(*it);
       it->Update(m_Script);
+    }
   }
 }
--- Data/Base/TypeScript/Component.ts
+++ Data/Base/TypeScript/Component.ts
@@ -1,6 +1,28 @@
 
+class ezGameObject
+{
+    Update() : void
+    {   
+        
+        ezLog_Info("ezGameObject::Update")
+        GameObjectMove(this)
+    }
+}
+
+let go : ezGameObject = null;
+
+function _CreateGameObject()
+{
+    return new ezGameObject;
+}
 
 function Update() : void
 {
-  ezLog_Info("Update")
+    if (go === null)
+    {
+        go = GetGameObject()
+        //go = new ezGameObject()
+    }
+
+    go.Update();
 }
