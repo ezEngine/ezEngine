9e3b6a28787ee37dffeee28865b6fa31c5a84abd
Jan Krassnigg <jakras@microsoft.com>
2019-10-01 14:18:55 +0200

    ezArchive's now also work for paths that would create hash collisions

--- Code/Engine/Foundation/IO/Archive/Archive.h
+++ Code/Engine/Foundation/IO/Archive/Archive.h
@@ -28,6 +28,74 @@ public:
   ezResult Deserialize(ezStreamReader& stream);
 };
 
+/// \brief Helper class to store a hashed string for quick lookup in the archive TOC
+///
+/// Stores a hash of the lower case string for quick comparison.
+/// Additionally stores an offset into the ezArchiveTOC::m_AllPathStrings array for final validation, to prevent hash collisions.
+/// The proper string lookup with hash collision check only works together with ezArchiveLookupString, which has the necessary context
+/// to index the ezArchiveTOC::m_AllPathStrings array.
+class EZ_FOUNDATION_DLL ezArchiveStoredString
+{
+public:
+  EZ_DECLARE_POD_TYPE();
+
+  ezArchiveStoredString() = default;
+
+  ezArchiveStoredString(ezUInt32 uiLowerCaseHash, ezUInt32 uiSrcStringOffset)
+    : m_uiLowerCaseHash(uiLowerCaseHash)
+    , m_uiSrcStringOffset(uiSrcStringOffset)
+  {
+  }
+
+  ezUInt32 m_uiLowerCaseHash;
+  ezUInt32 m_uiSrcStringOffset;
+};
+
+void operator<<(ezStreamWriter& stream, const ezArchiveStoredString& value);
+void operator>>(ezStreamReader& stream, ezArchiveStoredString& value);
+
+/// \brief Helper class for looking up path strings in ezArchiveTOC::FindEntry()
+///
+/// Only works together with ezArchiveStoredString.
+class ezArchiveLookupString
+{
+  EZ_DISALLOW_COPY_AND_ASSIGN(ezArchiveLookupString);
+
+public:
+  EZ_DECLARE_POD_TYPE();
+
+  ezArchiveLookupString(ezUInt32 uiLowerCaseHash, const char* string, const ezDynamicArray<ezUInt8>& ArchiveAllPathStrings)
+    : m_uiLowerCaseHash(uiLowerCaseHash)
+    , m_szString(string)
+    , m_ArchiveAllPathStrings(ArchiveAllPathStrings)
+  {
+  }
+
+  ezUInt32 m_uiLowerCaseHash;
+  const char* m_szString = nullptr;
+  const ezDynamicArray<ezUInt8>& m_ArchiveAllPathStrings;
+};
+
+/// \brief Functions to enable ezHashTable to 1) store ezArchiveStoredString and 2) lookup strings efficiently with a ezArchiveLookupString
+template <>
+struct ezHashHelper<ezArchiveStoredString>
+{
+  EZ_ALWAYS_INLINE static ezUInt32 Hash(const ezArchiveStoredString& hs) { return hs.m_uiLowerCaseHash; }
+  EZ_ALWAYS_INLINE static ezUInt32 Hash(const ezArchiveLookupString& hs) { return hs.m_uiLowerCaseHash; }
+
+  EZ_ALWAYS_INLINE static bool Equal(const ezArchiveStoredString& a, const ezArchiveStoredString& b)
+  {
+    return a.m_uiSrcStringOffset == b.m_uiSrcStringOffset;
+  }
+
+  EZ_ALWAYS_INLINE static bool Equal(const ezArchiveStoredString& a, const ezArchiveLookupString& b)
+  {
+    // in case that we want to lookup a string using a ezArchiveLookupString, we validate
+    // that the stored string is actually equal to the lookup string, to enable handling of hash collisions
+    return ezStringUtils::IsEqual_NoCase(reinterpret_cast<const char*>(&b.m_ArchiveAllPathStrings[a.m_uiSrcStringOffset]), b.m_szString);
+  }
+};
+
 /// \brief Table-of-contents for an ezArchive file
 class EZ_FOUNDATION_DLL ezArchiveTOC
 {
@@ -35,7 +103,7 @@ public:
   /// all files stored in the ezArchive
   ezDynamicArray<ezArchiveEntry> m_Entries;
   /// allows to map a hashed string to the index of the file entry for the file path
-  ezHashTable<ezTempHashedString, ezUInt32> m_PathToIndex;
+  ezHashTable<ezArchiveStoredString, ezUInt32> m_PathToEntryIndex;
   /// one large array holding all path strings for the file entries, to reduce allocations
   ezDynamicArray<ezUInt8> m_AllPathStrings;
 
@@ -47,5 +115,3 @@ public:
   ezResult Serialize(ezStreamWriter& stream) const;
   ezResult Deserialize(ezStreamReader& stream);
 };
-
-
