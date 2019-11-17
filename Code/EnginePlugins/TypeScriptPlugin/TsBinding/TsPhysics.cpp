f394c8028bde6b5ae68e7160c8544fa1b7b059bb
Jan Krassnigg <Jan@Krassnigg.de>
2019-11-17 16:00:11 +0100

    Remaining Physics Functions

--- Code/EnginePlugins/TypeScriptPlugin/TsBinding/TsPhysics.cpp
+++ Code/EnginePlugins/TypeScriptPlugin/TsBinding/TsPhysics.cpp
@@ -21,7 +21,6 @@ static int __CPP_Physics_GetGravity(duk_context* pDuk);
     return 0;                                                 \
   }
 
-
 ezResult ezTypeScriptBinding::Init_Physics()
 {
   m_Duk.RegisterGlobalFunction("__CPP_Physics_CastRay", __CPP_Physics_CastRay, 6);
@@ -33,6 +32,35 @@ ezResult ezTypeScriptBinding::Init_Physics()
   m_Duk.RegisterGlobalFunction("__CPP_Physics_GetGravity", __CPP_Physics_GetGravity, 0);
 
   return EZ_SUCCESS;
+}
+
+static void PutHitResult(ezDuktapeHelper& duk, const ezPhysicsHitResult& res)
+{
+  ezTypeScriptBinding* pBinding = ezTypeScriptBinding::RetrieveBinding(duk);
+
+  duk.PushGlobalObject();                                 // [ global ]
+  duk.PushLocalObject("__Physics");                       // [ global __Physics ]
+  duk.PushLocalObject("Physics");                         // [ global __Physics Physics ]
+  duk_get_prop_string(duk, -1, "HitResult");              // [ global __Physics Physics HitResult ]
+  duk_new(duk, 0);                                        // [ global __Physics Physics HitResultObj ]
+  duk_remove(duk, -2);                                    // [ global __Physics HitResultObj ]
+  duk_remove(duk, -2);                                    // [ global HitResultObj ]
+  duk_remove(duk, -2);                                    // [ HitResultObj ]
+  ;                                                       //
+  duk.SetNumberProperty("distance", res.m_fDistance, -1); // [ HitResultObj ]
+  duk.SetNumberProperty("shapeId", res.m_uiShapeId, -1);  // [ HitResultObj ]
+  ;                                                       //
+  ezTypeScriptBinding::PushVec3(duk, res.m_vPosition);    // [ HitResultObj pos ]
+  duk.SetCustomProperty("position", -1);                  // [ HitResultObj ]
+  ;                                                       //
+  ezTypeScriptBinding::PushVec3(duk, res.m_vNormal);      // [ HitResultObj normal ]
+  duk.SetCustomProperty("normal", -1);                    // [ HitResultObj ]
+  ;                                                       //
+  pBinding->DukPutGameObject(res.m_hShapeObject);         // [ HitResultObj GO ]
+  duk.SetCustomProperty("shapeObject", -1);               // [ HitResultObj ]
+  ;                                                       //
+  pBinding->DukPutGameObject(res.m_hActorObject);         // [ HitResultObj GO ]
+  duk.SetCustomProperty("actorObject", -1);               // [ HitResultObj ]
 }
 
 static int __CPP_Physics_CastRay(duk_context* pDuk)
@@ -55,31 +83,7 @@ static int __CPP_Physics_CastRay(duk_context* pDuk)
     return duk.ReturnNull();
   }
 
-  ezTypeScriptBinding* pBinding = ezTypeScriptBinding::RetrieveBinding(pDuk);
-
-  duk.PushGlobalObject();                                 // [ global ]
-  duk.PushLocalObject("__Physics");                       // [ global __Physics ]
-  duk.PushLocalObject("Physics");                         // [ global __Physics Physics ]
-  duk_get_prop_string(duk, -1, "HitResult");              // [ global __Physics Physics HitResult ]
-  duk_new(duk, 0);                                        // [ global __Physics Physics HitResultObj ]
-  duk_remove(duk, -2);                                    // [ global __Physics HitResultObj ]
-  duk_remove(duk, -2);                                    // [ global HitResultObj ]
-  duk_remove(duk, -2);                                    // [ HitResultObj ]
-  ;                                                       //
-  duk.SetNumberProperty("distance", res.m_fDistance, -1); // [ HitResultObj ]
-  duk.SetNumberProperty("shapeId", res.m_uiShapeId, -1);  // [ HitResultObj ]
-  ;                                                       //
-  ezTypeScriptBinding::PushVec3(pDuk, res.m_vPosition);   // [ HitResultObj pos ]
-  duk.SetCustomProperty("position", -1);                  // [ HitResultObj ]
-  ;                                                       //
-  ezTypeScriptBinding::PushVec3(pDuk, res.m_vNormal);     // [ HitResultObj normal ]
-  duk.SetCustomProperty("normal", -1);                    // [ HitResultObj ]
-  ;                                                       //
-  pBinding->DukPutGameObject(res.m_hShapeObject);         // [ HitResultObj GO ]
-  duk.SetCustomProperty("shapeObject", -1);               // [ HitResultObj ]
-  ;                                                       //
-  pBinding->DukPutGameObject(res.m_hActorObject);         // [ HitResultObj GO ]
-  duk.SetCustomProperty("actorObject", -1);               // [ HitResultObj ]
+  PutHitResult(duk, res);
 
   EZ_DUK_RETURN_AND_VERIFY_STACK(duk, duk.ReturnCustom(), +1);
 }
@@ -91,8 +95,22 @@ static int __CPP_Physics_SweepTestSphere(duk_context* pDuk)
 
   ezPhysicsWorldModuleInterface* pModule = GetPhysicsModule();
 
+  const float fRadius = duk.GetFloatValue(0);
+  const ezVec3 vStart = ezTypeScriptBinding::GetVec3(duk, 1);
+  const ezVec3 vDir = ezTypeScriptBinding::GetVec3(duk, 2);
+  const float fDistance = duk.GetFloatValue(3);
+  const ezUInt32 uiCollisionLayer = duk.GetUIntValue(4);
+  const ezUInt32 uiIgnoreShapeID = duk.GetIntValue(5);
 
-  return duk.ReturnCustom();
+  ezPhysicsHitResult res;
+  if (!pModule->SweepTestSphere(fRadius, vStart, vDir, fDistance, uiCollisionLayer, res, uiIgnoreShapeID))
+  {
+    return duk.ReturnNull();
+  }
+
+  PutHitResult(duk, res);
+
+  EZ_DUK_RETURN_AND_VERIFY_STACK(duk, duk.ReturnCustom(), +1);
 }
 
 static int __CPP_Physics_SweepTestBox(duk_context* pDuk)
@@ -102,8 +120,22 @@ static int __CPP_Physics_SweepTestBox(duk_context* pDuk)
 
   ezPhysicsWorldModuleInterface* pModule = GetPhysicsModule();
 
+  const ezVec3 vExtents = ezTypeScriptBinding::GetVec3(duk, 0);
+  const ezTransform transform = ezTypeScriptBinding::GetTransform(duk, 1);
+  const ezVec3 vDir = ezTypeScriptBinding::GetVec3(duk, 2);
+  const float fDistance = duk.GetFloatValue(3);
+  const ezUInt32 uiCollisionLayer = duk.GetUIntValue(4);
+  const ezUInt32 uiIgnoreShapeID = duk.GetIntValue(5);
 
-  return duk.ReturnCustom();
+  ezPhysicsHitResult res;
+  if (!pModule->SweepTestBox(vExtents, transform, vDir, fDistance, uiCollisionLayer, res, uiIgnoreShapeID))
+  {
+    return duk.ReturnNull();
+  }
+
+  PutHitResult(duk, res);
+
+  EZ_DUK_RETURN_AND_VERIFY_STACK(duk, duk.ReturnCustom(), +1);
 }
 
 static int __CPP_Physics_SweepTestCapsule(duk_context* pDuk)
@@ -113,8 +145,23 @@ static int __CPP_Physics_SweepTestCapsule(duk_context* pDuk)
 
   ezPhysicsWorldModuleInterface* pModule = GetPhysicsModule();
 
+  const float fCapsuleRadius = duk.GetFloatValue(0);
+  const float fCapsuleHeight = duk.GetFloatValue(1);
+  const ezTransform transform = ezTypeScriptBinding::GetTransform(duk, 2);
+  const ezVec3 vDir = ezTypeScriptBinding::GetVec3(duk, 3);
+  const float fDistance = duk.GetFloatValue(4);
+  const ezUInt32 uiCollisionLayer = duk.GetUIntValue(5);
+  const ezUInt32 uiIgnoreShapeID = duk.GetIntValue(6);
 
-  return duk.ReturnCustom();
+  ezPhysicsHitResult res;
+  if (!pModule->SweepTestCapsule(fCapsuleRadius, fCapsuleHeight, transform, vDir, fDistance, uiCollisionLayer, res, uiIgnoreShapeID))
+  {
+    return duk.ReturnNull();
+  }
+
+  PutHitResult(duk, res);
+
+  EZ_DUK_RETURN_AND_VERIFY_STACK(duk, duk.ReturnCustom(), +1);
 }
 
 static int __CPP_Physics_OverlapTestSphere(duk_context* pDuk)
@@ -124,8 +171,12 @@ static int __CPP_Physics_OverlapTestSphere(duk_context* pDuk)
 
   ezPhysicsWorldModuleInterface* pModule = GetPhysicsModule();
 
+  const float fRadius = duk.GetFloatValue(0);
+  const ezVec3 vPos = ezTypeScriptBinding::GetVec3(duk, 1);
+  const ezUInt32 uiCollisionLayer = duk.GetUIntValue(2);
+  const ezUInt32 uiIgnoreShapeID = duk.GetIntValue(3);
 
-  return duk.ReturnCustom();
+  return duk.ReturnBool(pModule->OverlapTestSphere(fRadius, vPos, uiCollisionLayer, uiIgnoreShapeID));
 }
 
 static int __CPP_Physics_OverlapTestCapsule(duk_context* pDuk)
@@ -135,8 +186,13 @@ static int __CPP_Physics_OverlapTestCapsule(duk_context* pDuk)
 
   ezPhysicsWorldModuleInterface* pModule = GetPhysicsModule();
 
+  const float fCapsuleRadius = duk.GetFloatValue(0);
+  const float fCapsuleHeight = duk.GetFloatValue(1);
+  const ezTransform transform = ezTypeScriptBinding::GetTransform(duk, 2);
+  const ezUInt32 uiCollisionLayer = duk.GetUIntValue(3);
+  const ezUInt32 uiIgnoreShapeID = duk.GetIntValue(4);
 
-  return duk.ReturnCustom();
+  return duk.ReturnBool(pModule->OverlapTestCapsule(fCapsuleRadius, fCapsuleHeight, transform, uiCollisionLayer, uiIgnoreShapeID));
 }
 
 static int __CPP_Physics_GetGravity(duk_context* pDuk)
