cfd76fbe34900cf16dc74695d2e6d6945f98a289
Jan Krassnigg <Jan@Krassnigg.de>
2019-12-11 00:20:15 +0100

    TS QueryDynamicShapesInSphere

--- Data/Samples/Testing Chambers/Scripts/Rotate.ts
+++ Data/Samples/Testing Chambers/Scripts/Rotate.ts
@@ -30,12 +30,32 @@ export class Rotate extends ez.TickedTypescriptComponent {
         return true;
     }
 
+    QueryShapes = (actor: ez.GameObject, shape: ez.GameObject, shapeId: number): boolean => {
+        let owner = this.GetOwner();
+        let ownerPos = owner.GetGlobalPosition();
+        let pos = shape.GetGlobalPosition();
+
+        let line = new ez.Debug.Line();
+        line.startX = ownerPos.x;
+        line.startY = ownerPos.y;
+        line.startZ = ownerPos.z;
+        line.endX = pos.x;
+        line.endY = pos.y;
+        line.endZ = pos.z;
+
+        this._lines.push(line);
+
+        return true;
+    }
+
+
     Tick(): number {
 
         let owner = this.GetOwner();
 
         this._lines = [];
-        ez.World.FindObjectsInSphere("NPC", owner.GetGlobalPosition(), 5, this.FindObjects)
+        //ez.World.FindObjectsInSphere("NPC", owner.GetGlobalPosition(), 5, this.FindObjects)
+        ez.Physics.QueryDynamicShapesInSphere(3, owner.GetGlobalPosition(), 0, this.QueryShapes, -1)
         ez.Debug.DrawLines(this._lines, ez.Color.OrangeRed());
 
         return ez.Time.Milliseconds(0);
