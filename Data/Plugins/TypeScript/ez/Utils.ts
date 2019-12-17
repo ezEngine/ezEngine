3ee0d7a63ed3cac3a833644cd37cb9c7bbebbf2d
Jan Krassnigg <Jan@Krassnigg.de>
2019-12-17 23:52:41 +0100

    More TS comments.

--- Data/Plugins/TypeScript/ez/Utils.ts
+++ Data/Plugins/TypeScript/ez/Utils.ts
@@ -1,14 +1,27 @@
 declare function __CPP_Utils_StringToHash(text: string): number;
 
+/**
+ * Common utility functions.
+ */
 export namespace Utils {
+
+    /**
+     * Returns whether f1 and f2 are equal within a given epsilon.
+     */
     export function IsNumberEqual(f1: number, f2: number, epsilon: number): boolean {
         return f1 >= f2 - epsilon && f1 <= f2 + epsilon;
     }
 
+    /**
+     * Checks whether f1 is within epsilon close to zero.
+     */
     export function IsNumberZero(f1: number, epsilon: number): boolean {
         return f1 >= -epsilon && f1 <= epsilon;
     }
 
+    /**
+     * Computes the hash value for a given string.
+     */
     export function StringToHash(text: string): number {
         return __CPP_Utils_StringToHash(text);
     }
