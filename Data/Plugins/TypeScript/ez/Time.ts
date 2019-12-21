bee69c8a165bb2b48704cb72ae3a123be442f1bf
Jan Krassnigg <Jan@Krassnigg.de>
2019-12-21 23:22:50 +0100

    TS world and time docs

--- Data/Plugins/TypeScript/ez/Time.ts
+++ Data/Plugins/TypeScript/ez/Time.ts
@@ -1,72 +1,105 @@
 declare function __CPP_Time_Now(): number;
 
+/**
+ * Utility functions to work with time values.
+ * Time is genreally measured in seconds, this class provides functions to convert between different units.
+ */
 export class Time {
 
+    /**
+     * Returns the current time.
+     */
     static Now(): number {
         return __CPP_Time_Now();
     }
 
-    // Converts nanoseconds to Time
+    /**
+     * Converts nanoseconds to seconds
+     */
     static Nanoseconds(fNanoseconds: number): number {
         return fNanoseconds * 0.000000001;
     }
 
-    // Converts microseconds to Time
+    /**
+     * Converts microseconds to seconds
+     */
     static Microseconds(fMicroseconds: number): number {
         return fMicroseconds * 0.000001;
     }
 
-    // Converts milliseconds to Time
+    /**
+     * Converts milliseconds to seconds
+     */
     static Milliseconds(fMilliseconds: number): number {
         return fMilliseconds * 0.001;
     }
 
-    // Converts seconds to Time
+    /**
+     * Converts seconds to seconds
+     */
     static Seconds(fSeconds: number): number {
         return fSeconds;
     }
 
-    // Converts minutes to Time
+    /**
+     * Converts minutes to seconds
+     */
     static Minutes(fMinutes: number): number {
         return fMinutes * 60;
     }
 
-    // Converts hours to Time
+    /**
+     * Converts hours to seconds
+     */
     static Hours(fHours: number): number {
         return fHours * 60 * 60;
     }
 
-    // Returns a zero Time value
+    /**
+     * Returns a zero time value
+     */
     static Zero(): number {
         return 0;
     }
 
-    // Converts Time to nanoseconds
+    /**
+     * Converts seconds to nanoseconds
+     */
     static GetNanoseconds(Time: number): number {
         return Time * 1000000000.0;
     }
 
-    // Converts Time to microseconds
+    /**
+     * Converts seconds to microseconds
+     */
     static GetMicroseconds(Time: number): number {
         return Time * 1000000.0;
     }
 
-    // Converts Time to milliseconds
+    /**
+     * Converts seconds to milliseconds
+     */
     static GetMilliseconds(Time: number): number {
         return Time * 1000.0;
     }
 
-    // Converts Time to seconds
+    /**
+     * Converts seconds to seconds
+     */
     static GetSeconds(Time: number): number {
         return Time;
     }
 
-    // Converts Time to minutes
+    /**
+     * Converts seconds to minutes
+     */
     static GetMinutes(Time: number): number {
         return Time / 60.0;
     }
 
-    // Converts Time to hours
+    /**
+     * Converts seconds to hours
+     */
     static GetHours(Time: number): number {
         return Time / (60.0 * 60.0);
     }
