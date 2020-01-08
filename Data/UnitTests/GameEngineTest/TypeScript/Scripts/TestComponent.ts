be970555558048b3c0c64b79eee1b1f9fd9572a2
Jan Krassnigg <Jan@Krassnigg.de>
2020-01-08 23:27:25 +0100

    TS Component tests

--- Data/UnitTests/GameEngineTest/TypeScript/Scripts/TestComponent.ts
+++ Data/UnitTests/GameEngineTest/TypeScript/Scripts/TestComponent.ts
@@ -16,6 +16,48 @@ export class TestComponent extends ez.TypescriptComponent {
 
     ExecuteTests(): void {
 
+        let owner = this.GetOwner();
+
+        let mesh = owner.TryGetComponentOfBaseType(ez.MeshComponent);
+        let text = owner.TryGetComponentOfBaseType(ez.DebugTextComponent);
+
+        // IsValid
+        {
+            EZ_TEST.BOOL(mesh != null && mesh.IsValid());
+            EZ_TEST.BOOL(text != null && text.IsValid());
+        }
+
+        // GetOWner
+        {
+            EZ_TEST.BOOL(mesh.GetOwner() == owner);
+            EZ_TEST.BOOL(text.GetOwner() == owner);
+        }
+
+        // Active
+        {
+            EZ_TEST.BOOL(mesh.IsActive());
+            EZ_TEST.BOOL(mesh.IsActiveAndInitialized());
+            EZ_TEST.BOOL(mesh.IsActiveAndSimulating());
+            
+            EZ_TEST.BOOL(!text.IsActive());
+            EZ_TEST.BOOL(!text.IsActiveAndInitialized());
+            
+            text.SetActive(true);
+            EZ_TEST.BOOL(text.IsActive());
+            EZ_TEST.BOOL(text.IsActiveAndInitialized());
+            
+            mesh.SetActive(false);
+            EZ_TEST.BOOL(!mesh.IsActive());
+            EZ_TEST.BOOL(!mesh.IsActiveAndInitialized());
+            EZ_TEST.BOOL(!mesh.IsActiveAndSimulating());
+        }
+
+        // GetUniqueID
+        {
+            // ezInvalidIndex
+            EZ_TEST.INT(mesh.GetUniqueID(), 4294967295);
+            EZ_TEST.INT(text.GetUniqueID(), 4294967295);
+        }
     }
 
     OnMsgGenericEvent(msg: ez.MsgGenericEvent): void {
