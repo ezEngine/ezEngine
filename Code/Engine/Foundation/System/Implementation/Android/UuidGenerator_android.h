34ec12dfbe556c07adf4bfd95aa6820811dae365
Jan Krassnigg <jakras@microsoft.com>
2020-03-02 15:39:06 +0100

    Added Android JNI wrappers

--- Code/Engine/Foundation/System/Implementation/Android/UuidGenerator_android.h
+++ Code/Engine/Foundation/System/Implementation/Android/UuidGenerator_android.h
@@ -3,26 +3,21 @@ EZ_FOUNDATION_INTERNAL_HEADER
 
 #  include <android_native_app_glue.h>
 #  include <Foundation/Basics/Platform/Android/AndroidUtils.h>
+#  include <Foundation/Basics/Platform/Android/AndroidJni.h>
 
 void ezUuid::CreateNewUuid()
 {
-  // Use jni as uuid.h is not available
-  android_app* app = ezAndroidUtils::GetAndroidApp();
-  JNIEnv* env = nullptr;
-  app->activity->vm->AttachCurrentThread(&env, nullptr);
+  ezJniAttachment attachment;
 
-  jclass uuidClass = env->FindClass("java/util/UUID");
-  jmethodID randomUUID = env->GetStaticMethodID(uuidClass, "randomUUID", "()Ljava/util/UUID;");
-  jmethodID getMostSignificantBits = env->GetMethodID(uuidClass, "getMostSignificantBits", "()J");
-  jmethodID getLeastSignificantBits = env->GetMethodID(uuidClass, "getLeastSignificantBits", "()J");
-
-  jobject javaUuid = env->CallStaticObjectMethod(uuidClass, randomUUID);
-  jlong mostSignificant = env->CallLongMethod(javaUuid, getMostSignificantBits);
-  jlong leastSignificant = env->CallLongMethod(javaUuid, getLeastSignificantBits);
+  ezJniClass uuidClass("java/util/UUID");
+  EZ_ASSERT_DEBUG(!uuidClass.IsNull(), "UUID class not found.");
+  ezJniObject javaUuid = uuidClass.CallStatic<ezJniObject>("randomUUID");
+  jlong mostSignificant = javaUuid.Call<jlong>("getMostSignificantBits");
+  jlong leastSignificant = javaUuid.Call<jlong>("getLeastSignificantBits");
 
   m_uiHigh = mostSignificant;
   m_uiLow = leastSignificant;
-  app->activity->vm->DetachCurrentThread();
+
   //#TODO maybe faster to read /proc/sys/kernel/random/uuid, but that can't be done via ezOSFile
   // see https://stackoverflow.com/questions/11888055/include-uuid-h-into-android-ndk-project
 }
