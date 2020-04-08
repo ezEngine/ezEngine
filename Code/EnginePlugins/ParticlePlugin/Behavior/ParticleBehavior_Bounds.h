263befa4f9d200374a0a94e79403d3c3ffa40e8b
Jan Krassnigg <Jan@Krassnigg.de>
2020-04-08 22:42:21 +0200

    Added a behavior that keeps particles within a limited volume
    It teleports particles from one side of the box to the other, when they leave it.
    Also added a rain effect for demonstration.

---
+++ Code/EnginePlugins/ParticlePlugin/Behavior/ParticleBehavior_Bounds.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <ParticlePlugin/Behavior/ParticleBehavior.h>
+
+class ezPhysicsWorldModuleInterface;
+
+class EZ_PARTICLEPLUGIN_DLL ezParticleBehaviorFactory_Bounds final : public ezParticleBehaviorFactory
+{
+  EZ_ADD_DYNAMIC_REFLECTION(ezParticleBehaviorFactory_Bounds, ezParticleBehaviorFactory);
+
+public:
+  ezParticleBehaviorFactory_Bounds();
+
+  virtual const ezRTTI* GetBehaviorType() const override;
+  virtual void CopyBehaviorProperties(ezParticleBehavior* pObject, bool bFirstTime) const override;
+
+  virtual void Save(ezStreamWriter& stream) const override;
+  virtual void Load(ezStreamReader& stream) override;
+
+  ezVec3 m_vPositionOffset;
+  ezVec3 m_vBoxExtents;
+};
+
+
+class EZ_PARTICLEPLUGIN_DLL ezParticleBehavior_Bounds final : public ezParticleBehavior
+{
+  EZ_ADD_DYNAMIC_REFLECTION(ezParticleBehavior_Bounds, ezParticleBehavior);
+
+public:
+
+  ezVec3 m_vPositionOffset;
+  ezVec3 m_vBoxExtents;
+
+protected:
+  virtual void Process(ezUInt64 uiNumElements) override;
+
+  virtual void CreateRequiredStreams() override;
+  virtual void QueryOptionalStreams() override;
+
+  ezProcessingStream* m_pStreamPosition = nullptr;
+  ezProcessingStream* m_pStreamLastPosition = nullptr;
+};
+
