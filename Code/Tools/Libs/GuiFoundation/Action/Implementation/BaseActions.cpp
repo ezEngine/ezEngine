2a943a12f01d540be5281069ef5a7b1fae5c08b4
Jan Krassnigg <Jan@Krassnigg.de>
2020-04-22 12:21:24 +0200

    ezGroupAttribute can now be used on enums to sort menu items

--- Code/Tools/Libs/GuiFoundation/Action/Implementation/BaseActions.cpp
+++ Code/Tools/Libs/GuiFoundation/Action/Implementation/BaseActions.cpp
@@ -53,35 +53,92 @@ void ezEnumerationMenuAction::GetEntries(ezHybridArray<ezDynamicMenuAction::Item
   out_Entries.Reserve(m_pEnumerationType->GetProperties().GetCount() - 1);
   ezInt64 iCurrentValue = ezReflectionUtils::MakeEnumerationValid(m_pEnumerationType, GetValue());
 
+  // sort entries by group / category
+  // categories appear in the order in which they are used on the reflected properties
+  // within each category, items are sorted by 'order'
+  // all items that have the same 'order' are sorted alphabetically by display string
+
+  ezStringBuilder sCurGroup;
+  float fPrevOrder = -1;
+  struct ItemWithOrder
+  {
+    float m_fOrder = -1;
+    ezDynamicMenuAction::Item m_Item;
+
+    bool operator<(const ItemWithOrder& rhs) const
+    {
+      if (m_fOrder == rhs.m_fOrder)
+      {
+        return m_Item.m_sDisplay < rhs.m_Item.m_sDisplay;
+      }
+
+      return m_fOrder < rhs.m_fOrder;
+    }
+  };
+
+  ezHybridArray<ItemWithOrder, 16> unsortedItems;
+
+  auto appendToOutput = [&]() {
+    if (unsortedItems.IsEmpty())
+      return;
+
+    unsortedItems.Sort();
+
+    if (!out_Entries.IsEmpty())
+    {
+      // add a separator between groups
+      out_Entries.ExpandAndGetRef().m_ItemFlags.Add(ezDynamicMenuAction::Item::ItemFlags::Separator);
+    }
+
+    for (const auto& sortedItem : unsortedItems)
+    {
+      out_Entries.PushBack(sortedItem.m_Item);
+    }
+
+    unsortedItems.Clear();
+  };
+
   for (auto pProp : m_pEnumerationType->GetProperties().GetSubArray(1))
   {
     if (pProp->GetCategory() == ezPropertyCategory::Constant)
     {
-      ezInt64 iValue = static_cast<const ezAbstractConstantProperty*>(pProp)->GetConstant().ConvertTo<ezInt64>();
-      ezDynamicMenuAction::Item item;
-
-      item.m_sDisplay = ezTranslate(pProp->GetPropertyName());
-
-      item.m_UserValue = iValue;
-      if (m_pEnumerationType->IsDerivedFrom<ezEnumBase>())
+      if (const ezGroupAttribute* pGroup = pProp->GetAttributeByType<ezGroupAttribute>())
       {
-        item.m_CheckState =
-          (iCurrentValue == iValue) ? ezDynamicMenuAction::Item::CheckMark::Checked : ezDynamicMenuAction::Item::CheckMark::Unchecked;
-      }
-      else if (m_pEnumerationType->IsDerivedFrom<ezBitflagsBase>())
-      {
-        item.m_CheckState = ((iCurrentValue & iValue) != 0) ? ezDynamicMenuAction::Item::CheckMark::Checked
-                                                            : ezDynamicMenuAction::Item::CheckMark::Unchecked;
+        if (sCurGroup != pGroup->GetGroup())
+        {
+          sCurGroup = pGroup->GetGroup();
+
+          appendToOutput();
+        }
+
+        fPrevOrder = pGroup->GetOrder();
       }
 
-      out_Entries.PushBack(item);
+      ItemWithOrder& newItem = unsortedItems.ExpandAndGetRef();
+      newItem.m_fOrder = fPrevOrder;
+      auto& item = newItem.m_Item;
+
+      {
+        ezInt64 iValue = static_cast<const ezAbstractConstantProperty*>(pProp)->GetConstant().ConvertTo<ezInt64>();
+
+        item.m_sDisplay = ezTranslate(pProp->GetPropertyName());
+
+        item.m_UserValue = iValue;
+        if (m_pEnumerationType->IsDerivedFrom<ezEnumBase>())
+        {
+          item.m_CheckState =
+            (iCurrentValue == iValue) ? ezDynamicMenuAction::Item::CheckMark::Checked : ezDynamicMenuAction::Item::CheckMark::Unchecked;
+        }
+        else if (m_pEnumerationType->IsDerivedFrom<ezBitflagsBase>())
+        {
+          item.m_CheckState = ((iCurrentValue & iValue) != 0) ? ezDynamicMenuAction::Item::CheckMark::Checked
+                                                              : ezDynamicMenuAction::Item::CheckMark::Unchecked;
+        }
+      }
     }
   }
 
-  // sort the items to appear alphabetically by display name
-  out_Entries.Sort([](const ezDynamicMenuAction::Item& lhs, const ezDynamicMenuAction::Item& rhs) -> bool {
-    return lhs.m_sDisplay < rhs.m_sDisplay;
-  });
+  appendToOutput();
 }
 
 ezButtonAction::ezButtonAction(const ezActionContext& context, const char* szName, bool bCheckable, const char* szIconPath)
