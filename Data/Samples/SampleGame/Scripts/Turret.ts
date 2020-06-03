15716b7f4eac2e3be54e67cd7e5d09b9721ea355
Jan Krassnigg <Jan@Krassnigg.de>
2020-06-03 20:05:49 +0200

    Sample scripts

--- Data/Samples/SampleGame/Scripts/Turret.ts
+++ Data/Samples/SampleGame/Scripts/Turret.ts
@@ -6,27 +6,19 @@ export class Turret extends ez.TickedTypescriptComponent {
     Range: number = 3;
     /* END AUTO-GENERATED: VARIABLES */
 
+    allTargets: ez.GameObject[] = [];
+    lastDamageTime: number;
+
     constructor() {
         super()
-    }
-
-    static RegisterMessageHandlers() {
-
-        // you can only call "RegisterMessageHandler" from within this function
-        //ez.TypescriptComponent.RegisterMessageHandler(ez.MsgSetColor, "OnMsgSetColor");
     }
 
     OnSimulationStarted(): void {
 
         // update this component every frame
         this.SetTickInterval(ez.Time.Zero());
+        this.lastDamageTime = ez.Time.GetGameTime();
     }
-
-    // OnMsgSetColor(msg: ez.MsgSetColor): void {
-    //     ez.Log.Info("MsgSetColor: " + msg.Color.r + ", " + msg.Color.g + ", " + msg.Color.b + ", " + msg.Color.a);
-    // }
-
-    allTargets: ez.GameObject[] = [];
 
     FoundTargetCallback = (go: ez.GameObject): boolean => {
         this.allTargets.push(go);
@@ -42,6 +34,13 @@ export class Turret extends ez.TickedTypescriptComponent {
         ez.World.FindObjectsInSphere("TurretTarget", owner.GetGlobalPosition(), this.Range, this.FoundTargetCallback);
 
         this.DrawLinesToTargets();
+
+        if (ez.Time.GetGameTime() - this.lastDamageTime > ez.Time.Milliseconds(40)) {
+            
+            this.lastDamageTime = ez.Time.GetGameTime();
+
+            this.DamageAllTargets(2);
+        }
     }
 
     DrawLinesToTargets(): void {
@@ -67,6 +66,17 @@ export class Turret extends ez.TickedTypescriptComponent {
         }
 
         ez.Debug.DrawLines(lines, ez.Color.OrangeRed());
+    }
+
+    DamageAllTargets(damage: number): void {
+
+        let dmgMsg = new ez.MsgDamage();
+        dmgMsg.Damage = damage;
+
+        for (let i = 0; i < this.allTargets.length; ++i) {
+
+            this.allTargets[i].SendMessage(dmgMsg);
+        }
     }
 }
 
