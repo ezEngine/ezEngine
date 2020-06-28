d280403e34ceb53d82d572c1b257793ae3ae7615
C-Core <clemenskern@gmail.com>
2020-06-28 20:52:51 +0200

    Proper cmake config and git ignore files

--- Code/BuildSystem/CMake/FindEzRmlUi.cmake
+++ Code/BuildSystem/CMake/FindEzRmlUi.cmake
@@ -14,9 +14,9 @@ find_path(EZ_RMLUI_DIR Include/RmlUi/Core.h
 )
 
 if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-  set(RMLUI_LIB_PATH "${EZ_RMLUI_DIR}/Bin/win64")
+  set(RMLUI_LIB_PATH "${EZ_RMLUI_DIR}/vc141win64")
 else()
-  set(RMLUI_LIB_PATH "${EZ_RMLUI_DIR}/Bin/win32")
+  set(RMLUI_LIB_PATH "${EZ_RMLUI_DIR}/vc141win32")
 endif()
 
 include(FindPackageHandleStandardArgs)
@@ -24,20 +24,24 @@ find_package_handle_standard_args(EzRmlUi DEFAULT_MSG EZ_RMLUI_DIR)
 
 if (EZRMLUI_FOUND)
 
-  set(RMLUI_CONFIG "Debug")
-
   add_library(EzRmlUi::Core SHARED IMPORTED)
-  set_target_properties(EzRmlUi::Core PROPERTIES IMPORTED_LOCATION "${RMLUI_LIB_PATH}/${RMLUI_CONFIG}/RmlCore.dll")
-  set_target_properties(EzRmlUi::Core PROPERTIES IMPORTED_IMPLIB "${RMLUI_LIB_PATH}/${RMLUI_CONFIG}/RmlCore.lib")
+  set_target_properties(EzRmlUi::Core PROPERTIES IMPORTED_LOCATION "${RMLUI_LIB_PATH}/Release/RmlCore.dll")
+  set_target_properties(EzRmlUi::Core PROPERTIES IMPORTED_LOCATION_DEBUG "${RMLUI_LIB_PATH}/Debug/RmlCore.dll")
+  set_target_properties(EzRmlUi::Core PROPERTIES IMPORTED_IMPLIB "${RMLUI_LIB_PATH}/Release/RmlCore.lib")
+  set_target_properties(EzRmlUi::Core PROPERTIES IMPORTED_IMPLIB_DEBUG "${RMLUI_LIB_PATH}/Debug/RmlCore.lib")
   set_target_properties(EzRmlUi::Core PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${EZ_RMLUI_DIR}/Include")
   
   add_library(EzRmlUi::Controls SHARED IMPORTED)
-  set_target_properties(EzRmlUi::Controls PROPERTIES IMPORTED_LOCATION "${RMLUI_LIB_PATH}/${RMLUI_CONFIG}/RmlControls.dll")
-  set_target_properties(EzRmlUi::Controls PROPERTIES IMPORTED_IMPLIB "${RMLUI_LIB_PATH}/${RMLUI_CONFIG}/RmlControls.lib")
+  set_target_properties(EzRmlUi::Controls PROPERTIES IMPORTED_LOCATION "${RMLUI_LIB_PATH}/Release/RmlControls.dll")
+  set_target_properties(EzRmlUi::Controls PROPERTIES IMPORTED_LOCATION_DEBUG "${RMLUI_LIB_PATH}/Debug/RmlControls.dll")
+  set_target_properties(EzRmlUi::Controls PROPERTIES IMPORTED_IMPLIB "${RMLUI_LIB_PATH}/Release/RmlControls.lib")
+  set_target_properties(EzRmlUi::Controls PROPERTIES IMPORTED_IMPLIB_DEBUG "${RMLUI_LIB_PATH}/Debug/RmlControls.lib")
   
   add_library(EzRmlUi::Debugger SHARED IMPORTED)
-  set_target_properties(EzRmlUi::Debugger PROPERTIES IMPORTED_LOCATION "${RMLUI_LIB_PATH}/${RMLUI_CONFIG}/RmlDebugger.dll")
-  set_target_properties(EzRmlUi::Debugger PROPERTIES IMPORTED_IMPLIB "${RMLUI_LIB_PATH}/${RMLUI_CONFIG}/RmlDebugger.lib")
+  set_target_properties(EzRmlUi::Debugger PROPERTIES IMPORTED_LOCATION "${RMLUI_LIB_PATH}/Release/RmlDebugger.dll")
+  set_target_properties(EzRmlUi::Debugger PROPERTIES IMPORTED_LOCATION_DEBUG "${RMLUI_LIB_PATH}/Debug/RmlDebugger.dll")
+  set_target_properties(EzRmlUi::Debugger PROPERTIES IMPORTED_IMPLIB "${RMLUI_LIB_PATH}/Release/RmlDebugger.lib")
+  set_target_properties(EzRmlUi::Debugger PROPERTIES IMPORTED_IMPLIB_DEBUG "${RMLUI_LIB_PATH}/Debug/RmlDebugger.lib")
   
   add_library(EzRmlUi::Freetype SHARED IMPORTED)
   set_target_properties(EzRmlUi::Freetype PROPERTIES IMPORTED_LOCATION "${RMLUI_LIB_PATH}/freetype.dll")
