2de6f18dfa3e72173b02fe6ecd9ab4fb71186fe8
Robert Campbell <waprave@gmail.com>
2020-08-21 01:44:29 -0400

    Added some utility functions to the rectangle template class for contains and overlap tests and encapsulate and clip modifiers. (#305)
    Make modifiers non-const
    
    Removed include added by ide
    
    Use rect methods

--- Code/Engine/Foundation/Math/Implementation/Rect_inl.h
+++ Code/Engine/Foundation/Math/Implementation/Rect_inl.h
@@ -40,3 +40,62 @@ EZ_ALWAYS_INLINE bool ezRectTemplate<Type>::HasNonZeroArea() const
 {
   return (width > 0) && (height > 0);
 }
+
+template <typename Type>
+EZ_ALWAYS_INLINE bool ezRectTemplate<Type>::Contains(const ezVec2Template<Type>& point) const
+{
+  if (point.x >= x && point.x <= Right())
+  {
+    if (point.y >= y && point.y <= Bottom())
+      return true;
+  }
+
+  return false;
+}
+
+template <typename Type>
+EZ_ALWAYS_INLINE bool ezRectTemplate<Type>::Overlaps(const ezRectTemplate<Type>& other) const
+{
+  if (x < other.Right() && Right() > other.x && y < other.Bottom() && Bottom() > other.y)
+    return true;
+
+  return false;
+}
+
+template <typename Type>
+EZ_ALWAYS_INLINE void ezRectTemplate<Type>::Encapsulate(const ezRectTemplate<Type>& other)
+{
+  Type thisRight = Right();
+  Type thisBottom = Bottom();
+
+  if (other.x < x)
+    x = other.x;
+
+  if (other.y < y)
+    y = other.y;
+
+  if (other.Right() > thisRight)
+    width = other.Right() - x;
+  else
+    width = thisRight - x;
+
+  if (other.Bottom() > thisBottom)
+    height = other.Bottom() - y;
+  else
+    height = thisBottom - y;
+}
+
+template <typename Type>
+EZ_ALWAYS_INLINE bool ezRectTemplate<Type>::Clip(const ezRectTemplate<Type>& clipRect)
+{
+  Type newLeft = ezMath::Max<Type>(x, clipRect.x);
+  Type newTop = ezMath::Max<Type>(y, clipRect.y);
+
+  Type newRight = ezMath::Min<Type>(Right(), clipRect.Right());
+  Type newBottom = ezMath::Min<Type>(Bottom(), clipRect.Bottom());
+
+  x = newLeft;
+  y = newTop;
+  width = newRight - newLeft;
+  height = newBottom - newTop;
+}
