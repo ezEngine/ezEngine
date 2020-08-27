c81753ff3b6f154d48d14e3551534366926a56e5
Jan Krassnigg <Jan@Krassnigg.de>
2020-08-27 18:49:34 +0200

    file cleanup

---
+++ Code/Engine/RendererCore/AnimationSystem/AnimationGraph/AnimationGraph.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <RendererCore/RendererCoreDLL.h>
+
+#include <RendererCore/AnimationSystem/AnimationGraph/AnimationGraphNode.h>
+
+class EZ_RENDERERCORE_DLL ezAnimationGraph
+{
+public:
+  ezAnimationGraph();
+  ~ezAnimationGraph();
+
+
+
+};
---
+++ Code/Engine/RendererCore/AnimationSystem/AnimationGraph/Implementation/AnimationGraph.cpp
@@ -0,0 +1,6 @@
+#include <RendererCorePCH.h>
+
+#include <RendererCore/AnimationSystem/AnimationGraph/AnimationGraph.h>
+
+ezAnimationGraph::ezAnimationGraph() = default;
+ezAnimationGraph::~ezAnimationGraph() = default;
