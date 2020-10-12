6c70cbbe0c3b942c47e3d94378d6b465fb6f2b26
Jan Krassnigg <Jan@Krassnigg.de>
2020-10-12 12:14:57 +0200

    Removes the Ultralight integration

--- Code/EditorPlugins/Ultralight/EditorPluginUltralight/finalize.cmake
+++
@@ -1,6 +0,0 @@
-if (TARGET Editor AND TARGET EditorPluginUltralight)
-
-    # Make sure this project is built when the Editor is built
-    add_dependencies(Editor EditorPluginUltralight)
-
-endif()
