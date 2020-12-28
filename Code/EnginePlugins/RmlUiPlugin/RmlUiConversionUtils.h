d964941f9a9e1ccedaa60c8140b2b10a012643e1
C-Core <clemenskern@gmail.com>
2020-12-28 16:22:19 +0100

    Formatting

--- Code/EnginePlugins/RmlUiPlugin/RmlUiConversionUtils.h
+++ Code/EnginePlugins/RmlUiPlugin/RmlUiConversionUtils.h
@@ -8,4 +8,4 @@ namespace ezRmlUiConversionUtils
 {
   EZ_RMLUIPLUGIN_DLL ezVariant ToVariant(const Rml::Variant& value, ezVariant::Type::Enum targetType = ezVariant::Type::Invalid);
   EZ_RMLUIPLUGIN_DLL Rml::Variant ToVariant(const ezVariant& value);
-}
+} // namespace ezRmlUiConversionUtils
