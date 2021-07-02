be51b2eac3a31c0910714719d33ac517b3a89d35
Jan Krassnigg <Jan@Krassnigg.de>
2021-07-02 13:48:58 +0200

    more uwp fixes

--- Code/EnginePlugins/PhysXPlugin/finalize.cmake
+++ Code/EnginePlugins/PhysXPlugin/finalize.cmake
@@ -1,10 +1,2 @@
-if (TARGET PhysXPlugin)
-
-	# Make sure this project is built when the Editor is built
-	ez_add_as_runtime_dependency(PhysXPlugin)
-
-	ez_uwp_add_import_to_sources(PhysXPlugin ezPhysX::Foundation)
-	ez_uwp_add_import_to_sources(PhysXPlugin ezPhysX::Common)
-	ez_uwp_add_import_to_sources(PhysXPlugin ezPhysX::PhysX)
-	
-endif()
+# Make sure this project is built when the Editor is built
+ez_add_as_runtime_dependency(PhysXPlugin)
