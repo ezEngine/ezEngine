3e8f7c96a42bcb7c76c4772019929fba42b21170
C-Core <clemenskern@gmail.com>
2021-07-30 22:57:58 +0200

    Fixed normal mapping bug on mirrored meshes

--- Data/Base/Shaders/Materials/MaterialVertexShader.h
+++ Data/Base/Shaders/Materials/MaterialVertexShader.h
@@ -13,28 +13,28 @@ struct VS_GLOBALS
 static VS_GLOBALS G;
 
 #if defined(USE_OBJECT_POSITION_OFFSET)
-  float3 GetObjectPositionOffset(ezPerInstanceData data);
+float3 GetObjectPositionOffset(ezPerInstanceData data);
 #endif
 
 #if defined(USE_WORLD_POSITION_OFFSET)
-  float3 GetWorldPositionOffset(ezPerInstanceData data, float3 worldPosition);
+float3 GetWorldPositionOffset(ezPerInstanceData data, float3 worldPosition);
 #endif
 
 #if defined(USE_VERTEX_DEPTH_BIAS)
-  float GetVertexDepthBias();
+float GetVertexDepthBias();
 #endif
 
 #if defined(USE_SKINNING)
 
-  float3 SkinPosition(float3 ObjectSpacePosition, float4 BoneWeights, uint4 BoneIndices)
-  {
-    float3 OutPos = mul(TransformToMatrix(skinningTransforms[BoneIndices.x]), float4(ObjectSpacePosition, 1.0)).xyz * BoneWeights.x;
-    OutPos += mul(TransformToMatrix(skinningTransforms[BoneIndices.y]), float4(ObjectSpacePosition, 1.0)).xyz * BoneWeights.y;
-    OutPos += mul(TransformToMatrix(skinningTransforms[BoneIndices.z]), float4(ObjectSpacePosition, 1.0)).xyz * BoneWeights.z;
-    OutPos += mul(TransformToMatrix(skinningTransforms[BoneIndices.w]), float4(ObjectSpacePosition, 1.0)).xyz * BoneWeights.w;
+float3 SkinPosition(float3 ObjectSpacePosition, float4 BoneWeights, uint4 BoneIndices)
+{
+  float3 OutPos = mul(TransformToMatrix(skinningTransforms[BoneIndices.x]), float4(ObjectSpacePosition, 1.0)).xyz * BoneWeights.x;
+  OutPos += mul(TransformToMatrix(skinningTransforms[BoneIndices.y]), float4(ObjectSpacePosition, 1.0)).xyz * BoneWeights.y;
+  OutPos += mul(TransformToMatrix(skinningTransforms[BoneIndices.z]), float4(ObjectSpacePosition, 1.0)).xyz * BoneWeights.z;
+  OutPos += mul(TransformToMatrix(skinningTransforms[BoneIndices.w]), float4(ObjectSpacePosition, 1.0)).xyz * BoneWeights.w;
 
-    return OutPos;
-  }
+  return OutPos;
+}
 
 float3 SkinDirection(float3 ObjectSpaceDirection, float4 BoneWeights, uint4 BoneIndices)
 {
@@ -62,78 +62,77 @@ VS_OUT FillVertexData(VS_IN Input)
   float3x3 objectToWorldNormal = TransformToRotation(data.ObjectToWorldNormal);
 
   float3 objectPosition = Input.Position;
-  #if defined(USE_OBJECT_POSITION_OFFSET)
-    objectPosition += GetObjectPositionOffset(data);
-  #endif
-
-  #if defined(USE_SKINNING)
-    objectPosition = SkinPosition(objectPosition, Input.BoneWeights, Input.BoneIndices);
+#if defined(USE_OBJECT_POSITION_OFFSET)
+  objectPosition += GetObjectPositionOffset(data);
 #endif
 
-    VS_OUT Output;
-    Output.WorldPosition = mul(objectToWorld, float4(objectPosition, 1.0)).xyz;
+#if defined(USE_SKINNING)
+  objectPosition = SkinPosition(objectPosition, Input.BoneWeights, Input.BoneIndices);
+#endif
+
+  VS_OUT Output;
+  Output.WorldPosition = mul(objectToWorld, float4(objectPosition, 1.0)).xyz;
 #if defined(USE_WORLD_POSITION_OFFSET)
-    Output.WorldPosition += GetWorldPositionOffset(data, Output.WorldPosition);
-  #endif
+  Output.WorldPosition += GetWorldPositionOffset(data, Output.WorldPosition);
+#endif
 
   Output.Position = mul(GetWorldToScreenMatrix(), float4(Output.WorldPosition, 1.0));
-  
-  #if defined(USE_VERTEX_DEPTH_BIAS)
-    float depthBiasScale = 1.5f / (1 << 16);
-    Output.Position.z += GetVertexDepthBias() * depthBiasScale * Output.Position.w;
-  #endif
-  
+
+#if defined(USE_VERTEX_DEPTH_BIAS)
+  float depthBiasScale = 1.5f / (1 << 16);
+  Output.Position.z += GetVertexDepthBias() * depthBiasScale * Output.Position.w;
+#endif
+
   Output.Position.z = max(Output.Position.z, MaxZValue);
 
-  #if defined(USE_NORMAL)
-
-    float3 inputNormal = Input.Normal * 2.0 - 1.0;
-    float3 normal = inputNormal;
-
-    #if defined(USE_SKINNING)
-      normal = SkinDirection(inputNormal, Input.BoneWeights, Input.BoneIndices);
-    #endif
-
-    Output.Normal = normalize(mul(objectToWorldNormal, normal));
-  #endif
-
-  #if defined(USE_TANGENT)
-    float3 tangent = Input.Tangent.xyz * 2.0 - 1.0;
+#if defined(USE_NORMAL)
+  float3 normal = Input.Normal * 2.0 - 1.0;
 
 #  if defined(USE_SKINNING)
-    tangent = SkinDirection(tangent, Input.BoneWeights, Input.BoneIndices);
+  normal = SkinDirection(normal, Input.BoneWeights, Input.BoneIndices);
 #  endif
 
-    Output.Tangent = normalize(mul(objectToWorldNormal, tangent));
+  Output.Normal = normalize(mul(objectToWorldNormal, normal));
+#endif
 
-    float handednessCorrection = Input.Tangent.w * 2.0 - 1.0;
-    Output.BiTangent = cross(Output.Normal, Output.Tangent) * handednessCorrection;
+#if defined(USE_TANGENT)
+  float3 tangent = Input.Tangent.xyz * 2.0 - 1.0;
+
+#  if defined(USE_SKINNING)
+  tangent = SkinDirection(tangent, Input.BoneWeights, Input.BoneIndices);
+#  endif
+
+  float handednessCorrection = Input.Tangent.w * 2.0 - 1.0;
+  float3 bitangent = cross(normal, tangent) * handednessCorrection;
+
+  Output.Tangent = normalize(mul(objectToWorldNormal, tangent));
+  Output.BiTangent = normalize(mul(objectToWorldNormal, bitangent));
 #endif
 
 #if defined(USE_TEXCOORD0)
-    Output.TexCoord0 = Input.TexCoord0;
-    #if defined(USE_TEXCOORD1)
-      Output.TexCoord1 = Input.TexCoord1;
-    #endif
-  #endif
+  Output.TexCoord0 = Input.TexCoord0;
+#  if defined(USE_TEXCOORD1)
+  Output.TexCoord1 = Input.TexCoord1;
+#  endif
+#endif
 
-  #if defined(USE_COLOR0)
-    Output.Color0 = Input.Color0;
-    #if defined(USE_COLOR1)
-      Output.Color1 = Input.Color1;
-    #endif
-  #endif
-  
-  #if defined(USE_DEBUG_INTERPOLATOR)
-    Output.DebugInterpolator = float4(0.1, 0.1, 0.1, 1.0);
-    
-    #if defined(USE_SKINNING)
-      if (RenderPass == EDITOR_RENDER_PASS_BONE_WEIGHTS)
-      {   
-        Output.DebugInterpolator = Input.BoneWeights;
-      }
-    #endif
-  #endif
+#if defined(USE_COLOR0)
+  Output.Color0 = Input.Color0;
+#  if defined(USE_COLOR1)
+  Output.Color1 = Input.Color1;
+#  endif
+#endif
+
+#if defined(USE_DEBUG_INTERPOLATOR)
+  Output.DebugInterpolator = float4(0.1, 0.1, 0.1, 1.0);
+
+#  if defined(USE_SKINNING)
+  if (RenderPass == EDITOR_RENDER_PASS_BONE_WEIGHTS)
+  {
+    Output.DebugInterpolator = Input.BoneWeights;
+  }
+#  endif
+#endif
 
   Output.InstanceID = Input.InstanceID;
 
