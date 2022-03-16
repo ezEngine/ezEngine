cd012b5fca766425782031d7fafd35535942b203
Jan Krassnigg <jakra-ms@outlook.com>
2022-03-16 17:31:54 +0100

    build system fix

--- Code/BuildSystem/CMake/ezUtils.cmake
+++ Code/BuildSystem/CMake/ezUtils.cmake
@@ -201,7 +201,7 @@ function(ez_set_project_ide_folder TARGET_NAME PROJECT_SOURCE_DIR)
 
     get_filename_component (PARENT_FOLDER ${PROJECT_SOURCE_DIR} PATH)
     get_filename_component (FOLDER_NAME ${PARENT_FOLDER} NAME)
-    
+
     set(IDE_FOLDER "${FOLDER_NAME}")
     
     if (${PROJECT_SOURCE_DIR} MATCHES "${CMAKE_SOURCE_DIR}/")
@@ -209,19 +209,20 @@ function(ez_set_project_ide_folder TARGET_NAME PROJECT_SOURCE_DIR)
         set(IDE_FOLDER "")
         string(REPLACE "${CMAKE_SOURCE_DIR}/" "" PARENT_FOLDER ${PROJECT_SOURCE_DIR})
 
-        get_filename_component (PARENT_FOLDER ${PARENT_FOLDER} PATH)
-        get_filename_component (FOLDER_NAME ${PARENT_FOLDER} NAME)
-        
-        get_filename_component (PARENT_FOLDER2 ${PARENT_FOLDER} PATH)
+
+		get_filename_component (PARENT_FOLDER "${PARENT_FOLDER}" PATH)
+		get_filename_component (FOLDER_NAME "${PARENT_FOLDER}" NAME)
+		
+		get_filename_component (PARENT_FOLDER2 "${PARENT_FOLDER}" PATH)
 
         while(NOT ${PARENT_FOLDER2} STREQUAL "")
 
             set(IDE_FOLDER "${FOLDER_NAME}/${IDE_FOLDER}")
             
-            get_filename_component (PARENT_FOLDER ${PARENT_FOLDER} PATH)
-            get_filename_component (PARENT_FOLDER2 ${PARENT_FOLDER} PATH)
-            
-            get_filename_component (FOLDER_NAME ${PARENT_FOLDER} NAME)
+		get_filename_component (PARENT_FOLDER "${PARENT_FOLDER}" PATH)
+		get_filename_component (FOLDER_NAME "${PARENT_FOLDER}" NAME)
+		
+		get_filename_component (PARENT_FOLDER2 "${PARENT_FOLDER}" PATH)
         
         endwhile()
 
