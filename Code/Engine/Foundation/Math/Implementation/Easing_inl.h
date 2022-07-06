b327b9b058336c1a63a095708853eeb3c4f845ff
Jan Krassnigg <Jan@Krassnigg.de>
2022-07-06 19:12:47 +0200

    Build fix

--- Code/Engine/Foundation/Math/Implementation/Easing_inl.h
+++ Code/Engine/Foundation/Math/Implementation/Easing_inl.h
@@ -4,7 +4,6 @@
 
 namespace ezMath
 {
-
   template <typename Type>
   EZ_ALWAYS_INLINE Type EaseInLinear(Type t)
   {
@@ -265,4 +264,79 @@ namespace ezMath
              : (1.0 + EaseOutBounce<Type>(2.0 * t - 1.0)) / 2.0;
   }
 
+  template <typename Type>
+  Type GetEasingValue(ezEasingFunctions easingFunction, Type input)
+  {
+    switch (easingFunction)
+    {
+      case InLinear:
+        return EaseInLinear<Type>(input);
+      case OutLinear:
+        return EaseOutLinear<Type>(input);
+      case InSine:
+        return EaseInSine<Type>(input);
+      case OutSine:
+        return EaseOutSine<Type>(input);
+      case InOutSine:
+        return EaseInOutSine<Type>(input);
+      case InQuad:
+        return EaseInQuad<Type>(input);
+      case OutQuad:
+        return EaseOutQuad<Type>(input);
+      case InOutQuad:
+        return EaseInOutQuad<Type>(input);
+      case InCubic:
+        return EaseInCubic<Type>(input);
+      case OutCubic:
+        return EaseOutCubic<Type>(input);
+      case InOutCubic:
+        return EaseInOutCubic<Type>(input);
+      case InQuartic:
+        return EaseInQuartic<Type>(input);
+      case OutQuartic:
+        return EaseOutQuartic<Type>(input);
+      case InOutQuartic:
+        return EaseInOutQuartic<Type>(input);
+      case InQuintic:
+        return EaseInQuintic<Type>(input);
+      case OutQuintic:
+        return EaseOutQuintic<Type>(input);
+      case InOutQuintic:
+        return EaseInOutQuintic<Type>(input);
+      case InExpo:
+        return EaseInExpo<Type>(input);
+      case OutExpo:
+        return EaseOutExpo<Type>(input);
+      case InOutExpo:
+        return EaseInOutExpo<Type>(input);
+      case InCirc:
+        return EaseInCirc<Type>(input);
+      case OutCirc:
+        return EaseOutCirc<Type>(input);
+      case InOutCirc:
+        return EaseInOutCirc<Type>(input);
+      case InBack:
+        return EaseInBack<Type>(input);
+      case OutBack:
+        return EaseOutBack<Type>(input);
+      case InOutBack:
+        return EaseInOutBack<Type>(input);
+      case InElastic:
+        return EaseInElastic<Type>(input);
+      case OutElastic:
+        return EaseOutElastic<Type>(input);
+      case InOutElastic:
+        return EaseInOutElastic<Type>(input);
+      case InBounce:
+        return EaseInBounce<Type>(input);
+      case OutBounce:
+        return EaseOutBounce<Type>(input);
+      case InOutBounce:
+        return EaseInOutBounce<Type>(input);
+      case ENUM_COUNT:
+      default:
+        EZ_ASSERT_NOT_IMPLEMENTED("Easing function not implemented");
+        return 0;
+    }
+  }
 } // namespace ezMath
