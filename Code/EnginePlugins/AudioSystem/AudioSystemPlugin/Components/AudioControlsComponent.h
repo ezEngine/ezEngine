df118f3eac3606eb7d6b51ec6cddf1e85bcf165a
Axel Nana <axel.nana@aliens-group.com>
2022-08-07 16:56:10 +0100

    Add more options to ezAudioControlsComponent
    - Added an `AutoLoad` property to define if the resource should be loaded with the component initialization.
    - Added a scriptable `Load` function to manually load the controls in the Audio System.
    - Added a scriptable `Unload` function to manually unload controls.
    - Ensure the controls are unloaded with the component deinitalization.

--- Code/EnginePlugins/AudioSystem/AudioSystemPlugin/Components/AudioControlsComponent.h
+++ Code/EnginePlugins/AudioSystem/AudioSystemPlugin/Components/AudioControlsComponent.h
@@ -9,6 +9,8 @@
 typedef ezComponentManager<class ezAudioControlsComponent, ezBlockStorageType::FreeList> ezAudioControlsComponentManager;
 
 /// \brief Component used to load and unload a set of audio controls.
+///
+/// The audio controls are provided by the selected audio control collection.
 class EZ_AUDIOSYSTEMPLUGIN_DLL ezAudioControlsComponent : public ezAudioSystemComponent
 {
   EZ_DECLARE_COMPONENT_TYPE(ezAudioControlsComponent, ezAudioSystemComponent, ezAudioControlsComponentManager);
@@ -17,6 +19,7 @@ class EZ_AUDIOSYSTEMPLUGIN_DLL ezAudioControlsComponent : public ezAudioSystemCo
 
 public:
   void Initialize() override;
+  void Deinitialize() override;
   void SerializeComponent(ezWorldWriter& stream) const override;
   void DeserializeComponent(ezWorldReader& stream) override;
 
@@ -31,8 +34,18 @@ public:
   ezAudioControlsComponent();
   ~ezAudioControlsComponent() override;
 
+  /// \brief Load the audio controls from the given collection.
+  /// This is automatically called on component initialization when
+  /// the AutoLoad property is set to true.
+  bool Load();
+
+  /// \brief Unloads the audio controls.
+  bool Unload();
+
 private:
   ezString m_sControlsAsset;
+  bool m_bAutoLoad;
 
+  bool m_bLoaded;
   ezAudioControlCollectionResourceHandle m_hControlsResource;
 };
--- Code/EnginePlugins/AudioSystem/AudioSystemPlugin/Implementation/Components/AudioControlsComponent.cpp
+++ Code/EnginePlugins/AudioSystem/AudioSystemPlugin/Implementation/Components/AudioControlsComponent.cpp
@@ -14,8 +14,18 @@ EZ_BEGIN_COMPONENT_TYPE(ezAudioControlsComponent, kVersion_AudioControlsComponen
   EZ_BEGIN_PROPERTIES
   {
     EZ_MEMBER_PROPERTY("ControlsAsset", m_sControlsAsset)->AddAttributes(new ezAssetBrowserAttribute("Audio Control Collection")),
+    EZ_MEMBER_PROPERTY("AutoLoad", m_bAutoLoad),
+
+    EZ_MEMBER_PROPERTY_READ_ONLY("Loaded", m_bLoaded)->AddAttributes(new ezHiddenAttribute()),
   }
   EZ_END_PROPERTIES;
+
+  EZ_BEGIN_FUNCTIONS
+  {
+    EZ_SCRIPT_FUNCTION_PROPERTY(Load),
+    EZ_SCRIPT_FUNCTION_PROPERTY(Unload),
+  }
+  EZ_END_FUNCTIONS;
 }
 EZ_END_COMPONENT_TYPE;
 // clang-format on
@@ -24,15 +34,16 @@ void ezAudioControlsComponent::Initialize()
 {
   SUPER::Initialize();
 
-  if (m_sControlsAsset.IsEmpty())
-    return;
+  if (m_bAutoLoad)
+    Load();
+}
 
-  m_hControlsResource = ezResourceManager::LoadResource<ezAudioControlCollectionResource>(m_sControlsAsset);
-  ezResourceLock resource(m_hControlsResource, ezResourceAcquireMode::BlockTillLoaded);
-  if (resource->GetLoadingState() != ezResourceState::Loaded)
-  {
-    ezLog::Error("Failed to load audio control collection '{0}'", m_sControlsAsset);
-  }
+void ezAudioControlsComponent::Deinitialize()
+{
+  if (m_bLoaded)
+    Unload();
+
+  SUPER::Deinitialize();
 }
 
 void ezAudioControlsComponent::SerializeComponent(ezWorldWriter& stream) const
@@ -40,8 +51,11 @@ void ezAudioControlsComponent::SerializeComponent(ezWorldWriter& stream) const
   SUPER::SerializeComponent(stream);
 
   auto& s = stream.GetStream();
+
   s.WriteVersion(kVersion_AudioControlsComponent);
+
   s << m_sControlsAsset;
+  s << m_bAutoLoad;
 }
 
 void ezAudioControlsComponent::DeserializeComponent(ezWorldReader& stream)
@@ -49,11 +63,53 @@ void ezAudioControlsComponent::DeserializeComponent(ezWorldReader& stream)
   SUPER::DeserializeComponent(stream);
 
   auto& s = stream.GetStream();
-  s.ReadVersion(kVersion_AudioControlsComponent);
+
+  const ezTypeVersion uiVersion = s.ReadVersion(kVersion_AudioControlsComponent);
+  EZ_ASSERT_DEV(uiVersion <= kVersion_AudioControlsComponent, "Invalid component version.");
+
   s >> m_sControlsAsset;
+  s >> m_bAutoLoad;
 }
 
-ezAudioControlsComponent::ezAudioControlsComponent() = default;
+ezAudioControlsComponent::ezAudioControlsComponent()
+  : ezAudioSystemComponent()
+  , m_bAutoLoad(false)
+  , m_bLoaded(false)
+{
+}
+
 ezAudioControlsComponent::~ezAudioControlsComponent() = default;
 
-EZ_STATICLINK_FILE(AudioSystemPlugin, AudioSystemPlugin_Implementation_Components_AudioSystemComponent);
+bool ezAudioControlsComponent::Load()
+{
+  if (m_sControlsAsset.IsEmpty())
+    return false;
+
+  if (m_bLoaded)
+    return true;
+
+  m_hControlsResource = ezResourceManager::LoadResource<ezAudioControlCollectionResource>(m_sControlsAsset);
+  if (const ezResourceLock resource(m_hControlsResource, ezResourceAcquireMode::BlockTillLoaded); resource.GetAcquireResult() == ezResourceAcquireResult::MissingFallback)
+  {
+    ezLog::Error("Failed to load audio control collection '{0}'", m_sControlsAsset);
+    return false;
+  }
+
+  m_bLoaded = true;
+  return true;
+}
+
+bool ezAudioControlsComponent::Unload()
+{
+  if (!m_bLoaded || !m_hControlsResource.IsValid())
+    return false;
+
+  ezResourceManager::FreeAllUnusedResources();
+  m_hControlsResource.Invalidate();
+  ezResourceManager::FreeAllUnusedResources();
+
+  m_bLoaded = false;
+  return true;
+}
+
+EZ_STATICLINK_FILE(AudioSystemPlugin, AudioSystemPlugin_Implementation_Components_AudioControlsComponent);
