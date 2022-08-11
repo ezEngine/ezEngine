e011c535e13a11f82df297db2781b1006ffb76bd
Jan Krassnigg <jakra-ms@outlook.com>
2022-08-11 09:03:46 +0200

    CMake file format

--- Code/BuildSystem/CMake/FindEzOpenXR.cmake
+++ Code/BuildSystem/CMake/FindEzOpenXR.cmake
@@ -1,15 +1,14 @@
-
 # find the folder into which the OpenXR loader has been installed
 
 # early out, if this target has been created before
-if (TARGET ezOpenXR::Loader)
+if(TARGET ezOpenXR::Loader)
 	return()
 endif()
 
-set (EZ_OPENXR_LOADER_DIR "EZ_OPENXR_LOADER_DIR-NOTFOUND" CACHE PATH "Directory of OpenXR loader installation")
-set (EZ_OPENXR_HEADERS_DIR "EZ_OPENXR_HEADERS_DIR-NOTFOUND" CACHE PATH "Directory of OpenXR headers installation")
-set (EZ_OPENXR_PREVIEW_DIR "" CACHE PATH "Directory of OpenXR preview include root")
-set (EZ_OPENXR_REMOTING_DIR "" CACHE PATH "Directory of OpenXR remoting installation")
+set(EZ_OPENXR_LOADER_DIR "EZ_OPENXR_LOADER_DIR-NOTFOUND" CACHE PATH "Directory of OpenXR loader installation")
+set(EZ_OPENXR_HEADERS_DIR "EZ_OPENXR_HEADERS_DIR-NOTFOUND" CACHE PATH "Directory of OpenXR headers installation")
+set(EZ_OPENXR_PREVIEW_DIR "" CACHE PATH "Directory of OpenXR preview include root")
+set(EZ_OPENXR_REMOTING_DIR "" CACHE PATH "Directory of OpenXR remoting installation")
 mark_as_advanced(FORCE EZ_OPENXR_LOADER_DIR)
 mark_as_advanced(FORCE EZ_OPENXR_HEADERS_DIR)
 mark_as_advanced(FORCE EZ_OPENXR_PREVIEW_DIR)
@@ -17,44 +16,43 @@ mark_as_advanced(FORCE EZ_OPENXR_REMOTING_DIR)
 
 ez_pull_compiler_and_architecture_vars()
 
-if ((EZ_OPENXR_LOADER_DIR STREQUAL "EZ_OPENXR_LOADER_DIR-NOTFOUND") OR (EZ_OPENXR_LOADER_DIR STREQUAL "") OR (EZ_OPENXR_HEADERS_DIR STREQUAL "EZ_OPENXR_HEADERS_DIR-NOTFOUND") OR (EZ_OPENXR_HEADERS_DIR STREQUAL "") OR (EZ_OPENXR_REMOTING_DIR STREQUAL "EZ_OPENXR_REMOTING_DIR-NOTFOUND") OR (EZ_OPENXR_REMOTING_DIR STREQUAL ""))
+if((EZ_OPENXR_LOADER_DIR STREQUAL "EZ_OPENXR_LOADER_DIR-NOTFOUND") OR(EZ_OPENXR_LOADER_DIR STREQUAL "") OR(EZ_OPENXR_HEADERS_DIR STREQUAL "EZ_OPENXR_HEADERS_DIR-NOTFOUND") OR(EZ_OPENXR_HEADERS_DIR STREQUAL "") OR(EZ_OPENXR_REMOTING_DIR STREQUAL "EZ_OPENXR_REMOTING_DIR-NOTFOUND") OR(EZ_OPENXR_REMOTING_DIR STREQUAL ""))
 	ez_nuget_init()
 	execute_process(COMMAND ${NUGET} restore ${CMAKE_SOURCE_DIR}/Code/EnginePlugins/OpenXRPlugin/packages.config -PackagesDirectory ${CMAKE_BINARY_DIR}/packages
-	WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
-	set (EZ_OPENXR_LOADER_DIR "${CMAKE_BINARY_DIR}/packages/OpenXR.Loader.1.0.10.2" CACHE PATH "Directory of OpenXR loader installation" FORCE)
-	set (EZ_OPENXR_HEADERS_DIR "${CMAKE_BINARY_DIR}/packages/OpenXR.Headers.1.0.10.2" CACHE PATH "Directory of OpenXR headers installation" FORCE)
-    set (EZ_OPENXR_REMOTING_DIR "${CMAKE_BINARY_DIR}/packages/Microsoft.Holographic.Remoting.OpenXr.2.4.0" CACHE PATH "Directory of OpenXR remoting installation" FORCE)
+		WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+	set(EZ_OPENXR_LOADER_DIR "${CMAKE_BINARY_DIR}/packages/OpenXR.Loader.1.0.10.2" CACHE PATH "Directory of OpenXR loader installation" FORCE)
+	set(EZ_OPENXR_HEADERS_DIR "${CMAKE_BINARY_DIR}/packages/OpenXR.Headers.1.0.10.2" CACHE PATH "Directory of OpenXR headers installation" FORCE)
+	set(EZ_OPENXR_REMOTING_DIR "${CMAKE_BINARY_DIR}/packages/Microsoft.Holographic.Remoting.OpenXr.2.4.0" CACHE PATH "Directory of OpenXR remoting installation" FORCE)
 endif()
 
-if (EZ_CMAKE_PLATFORM_WINDOWS_UWP)
-	set (OPENXR_DYNAMIC ON)
+if(EZ_CMAKE_PLATFORM_WINDOWS_UWP)
+	set(OPENXR_DYNAMIC ON)
 	find_path(EZ_OPENXR_HEADERS_DIR include/openxr/openxr.h)
 
-	if (EZ_CMAKE_ARCHITECTURE_ARM)
-		if (EZ_CMAKE_ARCHITECTURE_64BIT)
+	if(EZ_CMAKE_ARCHITECTURE_ARM)
+		if(EZ_CMAKE_ARCHITECTURE_64BIT)
 			set(OPENXR_BIN_PREFIX "arm64_uwp")
 		else()
 			set(OPENXR_BIN_PREFIX "arm_uwp")
 		endif()
 	else()
-		if (EZ_CMAKE_ARCHITECTURE_64BIT)
+		if(EZ_CMAKE_ARCHITECTURE_64BIT)
 			set(OPENXR_BIN_PREFIX "x64_uwp")
 		else()
 			set(OPENXR_BIN_PREFIX "Win32_uwp")
 		endif()
 	endif()
 
-elseif (EZ_CMAKE_PLATFORM_WINDOWS_DESKTOP)
-	set (OPENXR_DYNAMIC ON)
+elseif(EZ_CMAKE_PLATFORM_WINDOWS_DESKTOP)
+	set(OPENXR_DYNAMIC ON)
 	find_path(EZ_OPENXR_HEADERS_DIR include/openxr/openxr.h)
 
-	if (EZ_CMAKE_ARCHITECTURE_64BIT)
+	if(EZ_CMAKE_ARCHITECTURE_64BIT)
 		set(OPENXR_BIN_PREFIX "x64")
-        find_path(EZ_OPENXR_REMOTING_DIR build/native/include/openxr/openxr_msft_holographic_remoting.h)      
+		find_path(EZ_OPENXR_REMOTING_DIR build/native/include/openxr/openxr_msft_holographic_remoting.h)
 	else()
 		set(OPENXR_BIN_PREFIX "Win32")
 	endif()
-
 endif()
 
 include(FindPackageHandleStandardArgs)
@@ -62,30 +60,31 @@ find_package_handle_standard_args(ezOpenXR DEFAULT_MSG EZ_OPENXR_LOADER_DIR)
 find_package_handle_standard_args(ezOpenXR DEFAULT_MSG EZ_OPENXR_HEADERS_DIR)
 find_package_handle_standard_args(ezOpenXR DEFAULT_MSG EZ_OPENXR_REMOTING_DIR)
 
-if (EZOPENXR_FOUND)
-
+if(EZOPENXR_FOUND)
 	add_library(ezOpenXR::Loader SHARED IMPORTED)
-	if (OPENXR_DYNAMIC)
+
+	if(OPENXR_DYNAMIC)
 		set_target_properties(ezOpenXR::Loader PROPERTIES IMPORTED_LOCATION "${EZ_OPENXR_LOADER_DIR}/native/${OPENXR_BIN_PREFIX}/release/bin/openxr_loader.dll")
 		set_target_properties(ezOpenXR::Loader PROPERTIES IMPORTED_LOCATION_DEBUG "${EZ_OPENXR_LOADER_DIR}/native/${OPENXR_BIN_PREFIX}/release/bin/openxr_loader.dll")
 	endif()
+
 	set_target_properties(ezOpenXR::Loader PROPERTIES IMPORTED_IMPLIB "${EZ_OPENXR_LOADER_DIR}/native/${OPENXR_BIN_PREFIX}/release/lib/openxr_loader.lib")
 	set_target_properties(ezOpenXR::Loader PROPERTIES IMPORTED_IMPLIB_DEBUG "${EZ_OPENXR_LOADER_DIR}/native/${OPENXR_BIN_PREFIX}/release/lib/openxr_loader.lib")
-	
-	set_target_properties(ezOpenXR::Loader PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${EZ_OPENXR_HEADERS_DIR}/include")
-	if (NOT EZ_OPENXR_PREVIEW_DIR STREQUAL "")
-		set_target_properties(ezOpenXR::Loader PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${EZ_OPENXR_HEADERS_DIR}/include")		
-	endif()
-    
-    if (EZ_CMAKE_PLATFORM_WINDOWS_DESKTOP AND EZ_CMAKE_ARCHITECTURE_64BIT)
-        # As this is a windows only library, we are relying on the .targets file to handle to includes / imports.
-        add_library(ezOpenXR::Remoting SHARED IMPORTED)
-        set_target_properties(ezOpenXR::Remoting PROPERTIES IMPORTED_LOCATION ${EZ_OPENXR_REMOTING_DIR}/build/native/Microsoft.Holographic.Remoting.OpenXr.targets)
-    endif()
-	ez_uwp_mark_import_as_content(ezOpenXR::Loader)
 
+	set_target_properties(ezOpenXR::Loader PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${EZ_OPENXR_HEADERS_DIR}/include")
+
+	if(NOT EZ_OPENXR_PREVIEW_DIR STREQUAL "")
+		set_target_properties(ezOpenXR::Loader PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${EZ_OPENXR_HEADERS_DIR}/include")
+	endif()
+
+	if(EZ_CMAKE_PLATFORM_WINDOWS_DESKTOP AND EZ_CMAKE_ARCHITECTURE_64BIT)
+		# As this is a windows only library, we are relying on the .targets file to handle to includes / imports.
+		add_library(ezOpenXR::Remoting SHARED IMPORTED)
+		set_target_properties(ezOpenXR::Remoting PROPERTIES IMPORTED_LOCATION ${EZ_OPENXR_REMOTING_DIR}/build/native/Microsoft.Holographic.Remoting.OpenXr.targets)
+	endif()
+
+	ez_uwp_mark_import_as_content(ezOpenXR::Loader)
 endif()
 
-unset (OPENXR_DYNAMIC)
-unset (OPENXR_BIN_PREFIX)
-
+unset(OPENXR_DYNAMIC)
+unset(OPENXR_BIN_PREFIX)
--- Code/BuildSystem/CMake/ezUtilsTargetCS.cmake
+++ Code/BuildSystem/CMake/ezUtilsTargetCS.cmake
@@ -1,76 +1,64 @@
-######################################
-### ez_create_target_cs(<LIBRARY | APPLICATION> <target-name>)
-######################################
+# #####################################
+# ## ez_create_target_cs(<LIBRARY | APPLICATION> <target-name>)
+# #####################################
 
 macro(ez_create_target_cs TYPE TARGET_NAME)
+	ez_apply_build_filter(${TARGET_NAME})
 
-    ez_apply_build_filter(${TARGET_NAME})
+	set(ARG_OPTIONS NO_EZ_PREFIX)
+	set(ARG_ONEVALUEARGS DOTNET_VERSION)
+	set(ARG_MULTIVALUEARGS "")
+	cmake_parse_arguments(ARG "${ARG_OPTIONS}" "${ARG_ONEVALUEARGS}" "${ARG_MULTIVALUEARGS}" ${ARGN})
 
-    set(ARG_OPTIONS NO_EZ_PREFIX)
-    set(ARG_ONEVALUEARGS DOTNET_VERSION)
-    set(ARG_MULTIVALUEARGS "")
-    cmake_parse_arguments(ARG "${ARG_OPTIONS}" "${ARG_ONEVALUEARGS}" "${ARG_MULTIVALUEARGS}" ${ARGN} )
+	if(ARG_UNPARSED_ARGUMENTS)
+		message(FATAL_ERROR "ez_create_target_cs: Invalid arguments '${ARG_UNPARSED_ARGUMENTS}'")
+	endif()
 
-    if (ARG_UNPARSED_ARGUMENTS)
-        message(FATAL_ERROR "ez_create_target_cs: Invalid arguments '${ARG_UNPARSED_ARGUMENTS}'")
-    endif()
+	ez_pull_all_vars()
 
-    ez_pull_all_vars()
-
-    ez_glob_source_files(${CMAKE_CURRENT_SOURCE_DIR} ALL_SOURCE_FILES)
+	ez_glob_source_files(${CMAKE_CURRENT_SOURCE_DIR} ALL_SOURCE_FILES)
 
 	# SHARED_LIBRARY means always shared
-    # LIBRARY means SHARED_LIBRARY when EZ_COMPILE_ENGINE_AS_DLL is on, otherwise STATIC_LIBRARY
+	# LIBRARY means SHARED_LIBRARY when EZ_COMPILE_ENGINE_AS_DLL is on, otherwise STATIC_LIBRARY
+	if((${TYPE} STREQUAL "LIBRARY") OR(${TYPE} STREQUAL "STATIC_LIBRARY") OR(${TYPE} STREQUAL "SHARED_LIBRARY"))
+		if((${EZ_COMPILE_ENGINE_AS_DLL} AND(${TYPE} STREQUAL "LIBRARY")) OR(${TYPE} STREQUAL "SHARED_LIBRARY"))
+			message(STATUS "Shared Library (C#): ${TARGET_NAME}")
+			add_library(${TARGET_NAME} SHARED ${ALL_SOURCE_FILES})
 
-    if ((${TYPE} STREQUAL "LIBRARY") OR (${TYPE} STREQUAL "STATIC_LIBRARY") OR (${TYPE} STREQUAL "SHARED_LIBRARY"))
+		else()
+			message(STATUS "Static Library (C#): ${TARGET_NAME}")
+			add_library(${TARGET_NAME} ${ALL_SOURCE_FILES})
+		endif()
 
-        if ((${EZ_COMPILE_ENGINE_AS_DLL} AND (${TYPE} STREQUAL "LIBRARY")) OR (${TYPE} STREQUAL "SHARED_LIBRARY"))
+		if(NOT ARG_NO_EZ_PREFIX)
+			ez_add_output_ez_prefix(${TARGET_NAME})
+		endif()
 
-            message (STATUS "Shared Library (C#): ${TARGET_NAME}")
-            add_library (${TARGET_NAME} SHARED ${ALL_SOURCE_FILES})
+	elseif(${TYPE} STREQUAL "APPLICATION")
+		message(STATUS "Application (C#): ${TARGET_NAME}")
 
-        else ()
+		add_executable(${TARGET_NAME} ${ALL_SOURCE_FILES})
 
-            message (STATUS "Static Library (C#): ${TARGET_NAME}")
-            add_library (${TARGET_NAME} ${ALL_SOURCE_FILES})
+	else()
+		message(FATAL_ERROR "ez_create_target_cs: Missing argument to specify target type. Pass in 'APPLICATION' or 'SHARED_LIBRARY' or 'STATIC_LIBRARY'.")
+	endif()
 
-        endif ()
+	# sort files into the on-disk folder structure in the IDE
+	source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR} FILES ${ALL_SOURCE_FILES})
 
-        if (NOT ARG_NO_EZ_PREFIX)
-            ez_add_output_ez_prefix(${TARGET_NAME})
-        endif()
+	# default C# settings
+	target_compile_options(${TARGET_NAME} PRIVATE "/langversion:default")
 
-    elseif (${TYPE} STREQUAL "APPLICATION")
+	# setting this turns the app into a "Windows" application, not a "Console" application
+	# use ez_make_winmain_executable if this is desired
+	# set_property(TARGET ${TARGET_NAME} PROPERTY WIN32_EXECUTABLE TRUE)
+	if(ARG_DOTNET_VERSION)
+		# message(STATUS "Custom .NET version: ${ARG_DOTNET_VERSION}")
+		set_property(TARGET ${TARGET_NAME} PROPERTY VS_DOTNET_TARGET_FRAMEWORK_VERSION "v${ARG_DOTNET_VERSION}")
+	else()
+		set_property(TARGET ${TARGET_NAME} PROPERTY VS_DOTNET_TARGET_FRAMEWORK_VERSION "v4.6.1")
+	endif()
 
-        message (STATUS "Application (C#): ${TARGET_NAME}")
-
-        add_executable (${TARGET_NAME} ${ALL_SOURCE_FILES})
-
-    else()
-
-        message(FATAL_ERROR "ez_create_target_cs: Missing argument to specify target type. Pass in 'APPLICATION' or 'SHARED_LIBRARY' or 'STATIC_LIBRARY'.")
-
-    endif()
-
-    # sort files into the on-disk folder structure in the IDE
-    source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR} FILES ${ALL_SOURCE_FILES})
-
-    # default C# settings
-    target_compile_options(${TARGET_NAME} PRIVATE "/langversion:default")
-
-    # setting this turns the app into a "Windows" application, not a "Console" application
-    # use ez_make_winmain_executable if this is desired
-    #set_property(TARGET ${TARGET_NAME} PROPERTY WIN32_EXECUTABLE TRUE)
-    
-    if (ARG_DOTNET_VERSION)
-        #message(STATUS "Custom .NET version: ${ARG_DOTNET_VERSION}")
-        set_property(TARGET ${TARGET_NAME} PROPERTY VS_DOTNET_TARGET_FRAMEWORK_VERSION "v${ARG_DOTNET_VERSION}")
-    else()
-        set_property(TARGET ${TARGET_NAME} PROPERTY VS_DOTNET_TARGET_FRAMEWORK_VERSION "v4.6.1")
-    endif()
-
-    ez_set_default_target_output_dirs(${TARGET_NAME})
-    ez_set_project_ide_folder(${TARGET_NAME} ${CMAKE_CURRENT_SOURCE_DIR})
-
-
+	ez_set_default_target_output_dirs(${TARGET_NAME})
+	ez_set_project_ide_folder(${TARGET_NAME} ${CMAKE_CURRENT_SOURCE_DIR})
 endmacro()
