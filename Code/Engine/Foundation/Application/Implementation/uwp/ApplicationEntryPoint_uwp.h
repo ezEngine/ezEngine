1476377bbd7a46e9e644e7547f3e9ef4c7cea4ed
ezEngine Automation <ezEngineProject@gmail.com>
2022-08-14 18:00:31 +0000

    Applied code formatting rules

--- Code/Engine/Foundation/Application/Implementation/uwp/ApplicationEntryPoint_uwp.h
+++ Code/Engine/Foundation/Application/Implementation/uwp/ApplicationEntryPoint_uwp.h
@@ -46,10 +46,10 @@ namespace ezApplicationDetails
 } // namespace ezApplicationDetails
 
 /// \brief Same as EZ_APPLICATION_ENTRY_POINT but should be used for applications that shall always show a console window.
-#define EZ_CONSOLEAPP_ENTRY_POINT(AppClass, ...)                                                                                            \
+#define EZ_CONSOLEAPP_ENTRY_POINT(AppClass, ...)                                                                                 \
   alignas(EZ_ALIGNMENT_OF(AppClass)) static char appBuffer[sizeof(AppClass)]; /* Not on the stack to cope with smaller stacks */ \
-                                                                                                                                            \
-  EZ_APPLICATION_ENTRY_POINT_CODE_INJECTION                                                                                                 \
+                                                                                                                                 \
+  EZ_APPLICATION_ENTRY_POINT_CODE_INJECTION                                                                                      \
   int main(int argc, const char** argv) { return ::ezApplicationDetails::EntryFunc<AppClass>(__VA_ARGS__); }
 
 /// \brief This macro allows for easy creation of application entry points (since they can't be placed in DLLs)
