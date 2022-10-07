970a4f00dbe8bb1954ea74e0b70e0b2982f34bf0
Jan Krassnigg <Jan@Krassnigg.de>
2022-10-07 10:10:37 +0200

    try to convince GCC of my hacks

--- Code/Engine/Foundation/Basics/Compiler/GCC/DisableWarning_GCC.h
+++ Code/Engine/Foundation/Basics/Compiler/GCC/DisableWarning_GCC.h
@@ -3,8 +3,12 @@
 
 #  if EZ_ENABLED(EZ_COMPILER_GCC)
 
+#    define GCC_DISABLE_WARNING(W) _Pragma GCC diagnostic ignored W
+
 #    pragma GCC diagnostic push
-#    pragma GCC diagnostic ignored EZ_GCC_WARNING_NAME
+GCC_DISABLE_WARNING(EZ_GCC_WARNING_NAME)
+
+#    undef GCC_DISABLE_WARNING
 
 #  endif
 
