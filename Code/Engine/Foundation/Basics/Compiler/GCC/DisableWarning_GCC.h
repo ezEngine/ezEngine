855d11afc03f98f2cbdae96e9e225f6891e7acc7
Jan Krassnigg <Jan@Krassnigg.de>
2022-10-07 11:09:21 +0200

    needs more magic

--- Code/Engine/Foundation/Basics/Compiler/GCC/DisableWarning_GCC.h
+++ Code/Engine/Foundation/Basics/Compiler/GCC/DisableWarning_GCC.h
@@ -3,12 +3,8 @@
 
 #  if EZ_ENABLED(EZ_COMPILER_GCC)
 
-#    define GCC_DISABLE_WARNING(W) _Pragma GCC diagnostic ignored W
-
 #    pragma GCC diagnostic push
-GCC_DISABLE_WARNING(EZ_GCC_WARNING_NAME)
-
-#    undef GCC_DISABLE_WARNING
+_Pragma(EZ_STRINGIZE(GCC diagnostic ignored EZ_GCC_WARNING_NAME))
 
 #  endif
 
