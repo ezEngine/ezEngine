72bf02df859c1dfdca3e7a7cc0eb55ac1960bb5f
ezEngine Automation <ezEngineProject@gmail.com>
2022-11-03 18:40:35 +0000

    Applied code formatting rules

--- Code/Tools/Libs/ToolsFoundation/Document/PrefabCache.h
+++ Code/Tools/Libs/ToolsFoundation/Document/PrefabCache.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <ToolsFoundation/ToolsFoundationDLL.h>
 #include <Foundation/Configuration/Singleton.h>
 #include <Foundation/Configuration/Startup.h>
 #include <Foundation/Serialization/AbstractObjectGraph.h>
 #include <Foundation/Time/Timestamp.h>
 #include <Foundation/Types/UniquePtr.h>
+#include <ToolsFoundation/ToolsFoundationDLL.h>
 
 class ezAbstractObjectGraph;
 
