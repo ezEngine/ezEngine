eef43a66a4dc4212dc2a1a265bd88c95714569aa
Jan Krassnigg <jan@krassnigg.de>
2022-11-30 15:48:42 +0100

    Added component to forward events to the game state (#818)

--- Code/Engine/GameEngine/StateMachine/Implementation/StateMachineComponent.cpp
+++ Code/Engine/GameEngine/StateMachine/Implementation/StateMachineComponent.cpp
@@ -208,7 +208,7 @@ EZ_BEGIN_COMPONENT_TYPE(ezStateMachineComponent, 1, ezComponentMode::Static)
 
   EZ_BEGIN_ATTRIBUTES
   {
-    new ezCategoryAttribute("Gameplay"),
+    new ezCategoryAttribute("Gameplay/Logic"),
   }
   EZ_END_ATTRIBUTES;
 }
