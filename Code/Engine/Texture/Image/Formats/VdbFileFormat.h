4e2ad3d8c309f5129ade8933580f815cc21e5f0b
Benjamin Thaut <code@benjamin-thaut.de>
2023-07-29 11:51:23 +0200

    VDB format WIP

---
+++ Code/Engine/Texture/Image/Formats/VdbFileFormat.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Texture/Image/Formats/ImageFileFormat.h>
+
+class EZ_TEXTURE_DLL ezVdbFileFormat : public ezImageFileFormat
+{
+public:
+  virtual ezResult ReadImageHeader(ezStreamReader& inout_stream, ezImageHeader& ref_header, ezStringView sFileExtension) const override;
+  virtual ezResult ReadImage(ezStreamReader& inout_stream, ezImage& ref_image, ezStringView sFileExtension) const override;
+  virtual ezResult WriteImage(ezStreamWriter& inout_stream, const ezImageView& image, ezStringView sFileExtension) const override;
+
+  virtual bool CanReadFileType(ezStringView sExtension) const override;
+  virtual bool CanWriteFileType(ezStringView sExtension) const override;
+};
