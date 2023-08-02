638ef67ecda59f343105c90ba4ccfffa8c5780e1
Jan Krassnigg <jan@krassnigg.de>
2023-08-02 12:19:05 +0200

    ezSimdVec4f fix

--- Code/Engine/Foundation/SimdMath/SimdVec4f.h
+++ Code/Engine/Foundation/SimdMath/SimdVec4f.h
@@ -168,7 +168,7 @@ public:
   ///\brief Generates an arbitrary vector such that Dot<3>(GetOrthogonalVector()) == 0
   [[nodiscard]] ezSimdVec4f GetOrthogonalVector() const; // [tested]
 
-  [[nodiscard]] static ezSimdVec4f ZeroVector(); // [tested]
+  /*[[deprecated("Use MakeZero() instead.")]]*/ [[nodiscard]] static ezSimdVec4f ZeroVector(); // [tested]
 
   [[nodiscard]] static ezSimdVec4f MulAdd(const ezSimdVec4f& a, const ezSimdVec4f& b, const ezSimdVec4f& c); // [tested]
   [[nodiscard]] static ezSimdVec4f MulAdd(const ezSimdVec4f& a, const ezSimdFloat& b, const ezSimdVec4f& c); // [tested]
