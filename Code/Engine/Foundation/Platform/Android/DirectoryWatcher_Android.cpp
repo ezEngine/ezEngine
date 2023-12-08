10ed29f17b3ff20fe5127b73da3487f730955f25
Jan Krassnigg <jan@krassnigg.de>
2023-12-08 10:28:37 +0100

    build fix

--- Code/Engine/Foundation/Platform/Android/DirectoryWatcher_Android.cpp
+++ Code/Engine/Foundation/Platform/Android/DirectoryWatcher_Android.cpp
@@ -1,7 +1,7 @@
 #include <Foundation/FoundationPCH.h>
 
-#if EZ_ENABLED(EZ_PLATFORM_ANDROID)
-#  if (EZ_ENABLED(EZ_SUPPORTS_DIRECTORY_WATCHER) && EZ_ENABLED(EZ_USE_POSIX_FILE_API))
+#if (EZ_ENABLED(EZ_PLATFORM_ANDROID) && EZ_ENABLED(EZ_SUPPORTS_DIRECTORY_WATCHER))
+#  if EZ_ENABLED(EZ_USE_POSIX_FILE_API)
 #    include <Foundation/Platform/Posix/DirectoryWatcher_Posix.h>
 #  else
 #    error "DirectoryWatcher not implemented."
@@ -10,4 +10,3 @@
 
 
 EZ_STATICLINK_FILE(Foundation, Foundation_Platform_Android_DirectoryWatcher_Android);
-
--- Code/Engine/Foundation/Platform/OSX/DirectoryWatcher_OSX.cpp
+++ Code/Engine/Foundation/Platform/OSX/DirectoryWatcher_OSX.cpp
@@ -1,13 +1,11 @@
 #include <Foundation/FoundationPCH.h>
 
-#if EZ_ENABLED(EZ_PLATFORM_OSX)
-#  if (EZ_ENABLED(EZ_SUPPORTS_DIRECTORY_WATCHER) && EZ_ENABLED(EZ_USE_POSIX_FILE_API))
+#if (EZ_ENABLED(EZ_PLATFORM_OSX) && EZ_ENABLED(EZ_SUPPORTS_DIRECTORY_WATCHER))
+#  if EZ_ENABLED(EZ_USE_POSIX_FILE_API)
 #    include <Foundation/Platform/Posix/DirectoryWatcher_Posix.h>
 #  else
 #    error "DirectoryWatcher not implemented."
 #  endif
 #endif
 
-
 EZ_STATICLINK_FILE(Foundation, Foundation_Platform_OSX_DirectoryWatcher_OSX);
-
