73a2a83554a8682a934143d4525765e80f224e4e
Jan Krassnigg <Jan@Krassnigg.de>
2023-12-23 19:13:18 +0100

    ezAiNavMeshPathTestComponent

--- Code/EnginePlugins/AiPlugin/Components/NavMeshPathTestComponent.h
+++ Code/EnginePlugins/AiPlugin/Components/NavMeshPathTestComponent.h
@@ -7,6 +7,13 @@
 
 using ezNavMeshPathTestComponentManager = ezComponentManagerSimple<class ezAiNavMeshPathTestComponent, ezComponentUpdateType::WhenSimulating>;
 
+/// \brief Used to test path-finding through a navmesh.
+///
+/// The component takes a reference to another game object as the destination
+/// and then requests a path from the navmesh.
+/// Various aspects of the path can be visualized for inspection.
+///
+/// This component should be used in the editor, to test whether the scene navmesh behaves as desired.
 class EZ_AIPLUGIN_DLL ezAiNavMeshPathTestComponent : public ezComponent
 {
   EZ_DECLARE_COMPONENT_TYPE(ezAiNavMeshPathTestComponent, ezComponent, ezNavMeshPathTestComponentManager);
@@ -28,11 +35,20 @@ public:
   void SetPathEndReference(const char* szReference); // [ property ]
   void SetPathEnd(ezGameObjectHandle hObject);
 
-  bool m_bVisualizePathCorridor = true;
-  bool m_bVisualizePathLine = true;
-  bool m_bVisualizePathState = true;
-  ezHashedString m_sNavmeshConfig;
-  ezHashedString m_sPathSearchConfig;
+  /// \brief Render the navmesh polygons, through which the path goes.
+  bool m_bVisualizePathCorridor = true; // [ property ]
+
+  /// \brief Render a line for the shortest path through the corridor.
+  bool m_bVisualizePathLine = true;   // [ property ]
+
+  /// \brief Render text describing what went wrong during path search.
+  bool m_bVisualizePathState = true;  // [ property ]
+
+  /// \brief Name of the ezAiNavmeshConfig to use. See ezAiNavigationConfig.
+  ezHashedString m_sNavmeshConfig;    // [ property ]
+
+  /// \brief Name of the ezAiPathSearchConfig to use. See ezAiNavigationConfig.
+  ezHashedString m_sPathSearchConfig; // [ property ]
 
 protected:
   void Update();
