5b8beb6518b87f3a61b3118fe84ad305a3c6761c
Jan Krassnigg <Jan@Krassnigg.de>
2023-12-25 14:39:42 +0100

    ezJoltConeConstraintComponent

--- Code/EnginePlugins/JoltPlugin/Constraints/JoltConeConstraintComponent.h
+++ Code/EnginePlugins/JoltPlugin/Constraints/JoltConeConstraintComponent.h
@@ -4,6 +4,9 @@
 
 using ezJoltConeConstraintComponentManager = ezComponentManager<class ezJoltConeConstraintComponent, ezBlockStorageType::Compact>;
 
+/// \brief Implements a conical physics constraint.
+///
+/// The child actor can swing in a cone with a given angle around the anchor point on the parent actor.
 class EZ_JOLTPLUGIN_DLL ezJoltConeConstraintComponent : public ezJoltConstraintComponent
 {
   EZ_DECLARE_COMPONENT_TYPE(ezJoltConeConstraintComponent, ezJoltConstraintComponent, ezJoltConeConstraintComponentManager);
@@ -15,13 +18,13 @@ public:
   virtual void SerializeComponent(ezWorldWriter& inout_stream) const override;
   virtual void DeserializeComponent(ezWorldReader& inout_stream) override;
 
-
   //////////////////////////////////////////////////////////////////////////
   // ezJoltConstraintComponent
 
 protected:
   virtual void CreateContstraintType(JPH::Body* pBody0, JPH::Body* pBody1) override;
-
+  virtual void ApplySettings() final override;
+  virtual bool ExceededBreakingPoint() final override;
 
   //////////////////////////////////////////////////////////////////////////
   // ezJoltConeConstraintComponent
@@ -29,9 +32,6 @@ protected:
 public:
   ezJoltConeConstraintComponent();
   ~ezJoltConeConstraintComponent();
-
-  virtual void ApplySettings() final override;
-  virtual bool ExceededBreakingPoint() final override;
 
   void SetConeAngle(ezAngle f);                        // [ property ]
   ezAngle GetConeAngle() const { return m_ConeAngle; } // [ property ]
