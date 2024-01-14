1beedcca599d52a2e525a49e71bac17cefa5bc20
Benjamin Thaut <code@benjamin-thaut.de>
2024-01-14 19:02:19 +0100

    EZ_3RDPARTY_TRACELOGGING_LTTNG_SUPPORT should be default off

--- Code/ThirdParty/tracelogging/init.cmake
+++ Code/ThirdParty/tracelogging/init.cmake
@@ -1,4 +1,4 @@
 ez_requires(EZ_CMAKE_PLATFORM_LINUX)
 
-set (EZ_3RDPARTY_TRACELOGGING_LTTNG_SUPPORT ON CACHE BOOL "Whether to add support for tracelogging via lttng.")
+set (EZ_3RDPARTY_TRACELOGGING_LTTNG_SUPPORT OFF CACHE BOOL "Whether to add support for tracelogging via lttng.")
 mark_as_advanced(FORCE EZ_3RDPARTY_TRACELOGGING_LTTNG_SUPPORT)
