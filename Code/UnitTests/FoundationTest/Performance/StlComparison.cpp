20f8b057717647c595861e41a96cfa8a519bc727
Jan Krassnigg <jan@krassnigg.de>
2024-02-02 08:10:10 +0100

    wip

--- Code/UnitTests/FoundationTest/Performance/StlComparison.cpp
+++ Code/UnitTests/FoundationTest/Performance/StlComparison.cpp
@@ -2,15 +2,19 @@
 
 #if EZ_ENABLED(EZ_INTEROP_STL_STRINGS)
 
+#  include <Foundation/Containers/Map.h>
 #  include <Foundation/Time/Stopwatch.h>
 #  include <filesystem>
+#  include <map>
 #  include <string>
 #  include <string_view>
 
-namespace fs = std::filesystem;
+const ezUInt32 itersDefault = 1000000;
+const ezUInt32 itersFew = 100000;
 
 EZ_CREATE_SIMPLE_TEST_GROUP(StlComparison);
 
+namespace fs = std::filesystem;
 using Func = void (*)();
 
 void Measure(const char* szTitle, Func f)
@@ -24,13 +28,58 @@ void Measure(const char* szTitle, Func f)
   const ezTime t = sw.Checkpoint();
 
   ezStringBuilder s;
-  s.SetFormat("{}: {}ms", szTitle, ezArgF(t.GetMilliseconds(), 2, false, 10));
+  s.SetFormat("  {}: {}ms", szTitle, ezArgF(t.GetMilliseconds(), 2, false, 10));
 
   ezTestFramework::Output(ezTestOutput::Details, s.GetData());
 }
 
-const ezUInt32 itersDefault = 1000000;
-const ezUInt32 itersFew = 100000;
+void Measure(const char* szTitle, ezUInt32 iters, Func fez, Func fstd)
+{
+  ezUInt32 preIters = ezMath::Min(10u, iters);
+  ezStopwatch sw;
+
+  {
+    // pre-warm
+    for (ezUInt32 i = 0; i < preIters; ++i)
+    {
+      fez();
+    }
+
+    // measure
+    sw.Checkpoint();
+
+    for (ezUInt32 i = 0; i < iters; ++i)
+    {
+      fez();
+    }
+  }
+
+  const ezTime tez = sw.Checkpoint();
+
+  {
+    // pre-warm
+    for (ezUInt32 i = 0; i < preIters; ++i)
+    {
+      fstd();
+    }
+
+    // measure
+    sw.Checkpoint();
+
+    for (ezUInt32 i = 0; i < iters; ++i)
+    {
+      fstd();
+    }
+  }
+
+  const ezTime tstd = sw.Checkpoint();
+
+  ezStringBuilder s;
+  s.SetFormat("  {}: EZ = {}ms | STL = {}ms - {}", szTitle, ezArgF(tez.GetMilliseconds(), 2, false, 10), ezArgF(tstd.GetMilliseconds(), 2, false, 10), ezArgF(tez.GetSeconds() * 100.0 / tstd.GetSeconds(), 1, false, 4));
+
+  ezTestFramework::Output(ezTestOutput::Details, s.GetData());
+}
+
 
 const char* g_Cstr[2] = {"Path/Substitution1/Test/Substitution2-42.png", "Path/Substitution1/Test/Substitution2-42.png"};
 std::string g_StdStr[2] = {g_Cstr[0], g_Cstr[1]};
@@ -181,6 +230,20 @@ EZ_CREATE_SIMPLE_TEST(StlComparison, Strings)
 {
   EZ_TEST_BLOCK(ezTestBlock::Enabled, "std::string Comparison")
   {
+    ezTestFramework::Output(ezTestOutput::Details, "std::string");
+
+    Measure(
+      "Assign", itersDefault, []()
+      {
+        ezStringBuilder sb = g_Cstr[0];
+        //
+      },
+      []()
+      {
+        std::string s = g_Cstr[0];
+        //
+      });
+
     Measure("StdAssign", StdAssign);
     Measure(" ezAssign", ezAssign);
     Measure("StdReturnString", StdReturnString);
@@ -191,6 +254,12 @@ EZ_CREATE_SIMPLE_TEST(StlComparison, Strings)
     Measure(" ezAppend2", ezAppend2);
     Measure("StdAppend3", StdAppend3);
     Measure(" ezAppend3", ezAppend3);
+  }
+
+  EZ_TEST_BLOCK(ezTestBlock::Enabled, "std::path Comparison")
+  {
+    ezTestFramework::Output(ezTestOutput::Details, "std::path");
+
     Measure("StdPath1", StdPath1);
     Measure(" ezPath1", ezPath1);
     Measure("StdPath2", StdPath2);
@@ -198,6 +267,34 @@ EZ_CREATE_SIMPLE_TEST(StlComparison, Strings)
   }
 }
 
+EZ_CREATE_SIMPLE_TEST(StlComparison, Map)
+{
+  EZ_TEST_BLOCK(ezTestBlock::Enabled, "Insert")
+  {
+    Measure(
+      "Assign", 100, []()
+      {
+        ezMap<ezInt32, ezInt32> m;
+
+        for (ezUInt32 i = 0; i < itersFew; ++i)
+        {
+          m[i] = i;
+        }
+
+        //
+      },
+      []()
+      {
+        std::map<ezInt32, ezInt32> m;
+
+        for (ezUInt32 i = 0; i < itersFew; ++i)
+        {
+          m[i] = i;
+        }
+        //
+      });
+  }
+}
 
 
 #endif
