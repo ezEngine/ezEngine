af36874a8ace8558c2bbd5c6816c53e67c914009
Daniel <bdg002004007@gmail.com>
2024-04-14 22:40:01 +0300

    Use is_base_of instead of is_convertible

--- Code/Engine/Core/World/World.h
+++ Code/Engine/Core/World/World.h
@@ -178,11 +178,11 @@ public:
   [[nodiscard]] bool TryGetComponent(const ezComponentHandle& hComponent, const ComponentType*& out_pComponent) const;
 
   /// \brief Explicitly delete TryGetComponent overload when handle type is not related to a pointer type given by out_pComponent.
-  template <typename T, typename U, std::enable_if_t<!std::disjunction_v<std::is_convertible<T*, U*>, std::is_base_of<T, U>>, bool> = true>
+  template <typename T, typename U, std::enable_if_t<!std::disjunction_v<std::is_base_of<U, T>, std::is_base_of<T, U>>, bool> = true>
   [[nodiscard]] bool TryGetComponent(const ezTypedComponentHandle<T>& hComponent, U*& out_pComponent) = delete;
 
   /// \brief Explicitly delete TryGetComponent overload when handle type is not related to a pointer type given by out_pComponent.
-  template <typename T, typename U, std::enable_if_t<!std::disjunction_v<std::is_convertible<T*, U*>, std::is_base_of<T, U>>, bool> = true>
+  template <typename T, typename U, std::enable_if_t<!std::disjunction_v<std::is_base_of<U, T>, std::is_base_of<T, U>>, bool> = true>
   [[nodiscard]] bool TryGetComponent(const ezTypedComponentHandle<T>& hComponent, const U*& out_pComponent) const = delete;
 
   /// \brief Creates a new component init batch.
