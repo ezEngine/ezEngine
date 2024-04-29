75c628343c8049fd2f3aa1355e2b64072a50cbd1
Jan Krassnigg <jan@krassnigg.de>
2024-04-29 07:59:23 +0200

    Disable Tracy memory tracking by default

--- Code/ThirdParty/tracy/init.cmake
+++ Code/ThirdParty/tracy/init.cmake
@@ -1,8 +1,13 @@
 if(EZ_CMAKE_PLATFORM_WINDOWS_DESKTOP)
     set (EZ_3RDPARTY_TRACY_SUPPORT ON CACHE BOOL "Whether to add support for profiling the engine with Tracy.")
+	set (EZ_3RDPARTY_TRACY_TRACK_ALLOCATIONS OFF CACHE BOOL "Whether Tracy should track memory allocations.")
 else()
     # Tracy currently doesn't compile on Linux
     set (EZ_3RDPARTY_TRACY_SUPPORT OFF CACHE BOOL "Whether to add support for profiling the engine with Tracy.")
 endif()
 
 mark_as_advanced(FORCE EZ_3RDPARTY_TRACY_SUPPORT)
+
+if (NOT EZ_3RDPARTY_TRACY_SUPPORT)
+	unset(EZ_3RDPARTY_TRACY_TRACK_ALLOCATIONS CACHE)
+endif()
