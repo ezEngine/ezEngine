712cbfdcf11d4b42c915111d3611ac1b29cfff20
Jan Krassnigg <jan@krassnigg.de>
2024-06-11 21:51:44 +0200

    particle orientation fix

--- Data/Base/Shaders/Particles/ParticleCommonVS.h
+++ Data/Base/Shaders/Particles/ParticleCommonVS.h
@@ -60,14 +60,17 @@ Quad CalcQuadOutputPositionWithAlignedAxis(uint vertexIndex, float3 inPosition, 
 {
   float stretch = -inTangentZ.x;
 
-  float3 axisDir = normalize(inTangentX);
-  float3 orthoDir = normalize(cross(inTangentX, GetCameraDirForwards()));
+  float3 inTangentXws = mul(ObjectToWorldMatrix, float4(inTangentX.xyz, 0));
+
+  float3 axisDir = normalize(inTangentXws);
+  float3 orthoDir = normalize(cross(inTangentXws, GetCameraDirForwards()));
 
   float3 offsetRight = orthoDir * ((QuadTexCoords[vertexIndex].x - 0.5) * inSize);
   float3 offsetUp = axisDir * ((1.0 - QuadTexCoords[vertexIndex].y) * inSize * stretch);
 
   Quad quad;
-  quad.worldPosition = mul(ObjectToWorldMatrix, float4(inPosition + offsetRight + offsetUp, 1));
+  quad.worldPosition = mul(ObjectToWorldMatrix, float4(inPosition, 1));
+  quad.worldPosition.xyz += offsetRight + offsetUp;
   quad.screenPosition = mul(GetWorldToScreenMatrix(), quad.worldPosition);
 
   float3 centerNormal = cross(axisDir, orthoDir);
