98acd87a09946083ddf74ea4fc4405043479c598
Alexander Rieder <rieder.alex1993@gmail.com>
2024-07-04 14:06:16 +0200

    Removed duplicate code that happened because of a merge

--- Code/Editor/EditorFramework/Assets/Implementation/AssetBrowserFilter.cpp
+++ Code/Editor/EditorFramework/Assets/Implementation/AssetBrowserFilter.cpp
@@ -244,14 +244,6 @@ bool ezQtAssetBrowserFilter::IsAssetFiltered(ezStringView sDataDirParentRelative
     // if so, there is a sub-folder, and thus we ignore it
     if (ezStringUtils::FindSubString(sDataDirParentRelativePath.GetStartPointer() + m_sPathFilter.GetElementCount(), "/", sDataDirParentRelativePath.GetEndPointer()) != nullptr)
       return true;
-
-    if (m_SearchFilter.IsEmpty() && (!m_bShowItemsInSubFolders || bIsFolder))
-    {
-      // do we find another path separator after the prefix path?
-      // if so, there is a sub-folder, and thus we ignore it
-      if (ezStringUtils::FindSubString(sDataDirParentRelativePath.GetStartPointer() + m_sPathFilter.GetElementCount(), "/", sDataDirParentRelativePath.GetEndPointer()) != nullptr)
-        return true;
-    }
   }
   else if(m_sPathFilter.IsEmpty() && !bIsFolder) // <Root> folder
   {
