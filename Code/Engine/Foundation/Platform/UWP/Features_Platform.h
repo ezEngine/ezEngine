81625dd2b32b3f37f533c121022afb456139ed5f
Jan Krassnigg <jan@krassnigg.de>
2024-10-22 15:32:42 +0200

    uwp fix

--- Code/Engine/Foundation/Platform/UWP/Features_Platform.h
+++ Code/Engine/Foundation/Platform/UWP/Features_Platform.h
@@ -4,27 +4,27 @@
 
 /// If set to 1, the POSIX file implementation will be used. Otherwise a platform specific implementation must be available.
 #undef EZ_USE_POSIX_FILE_API
-#define EZ_USE_POSIX_FILE_API EZ_OFF
+#define EZ_USE_POSIX_FILE_API EZ_ON
 
 /// Iterating through the file system is supported
 #undef EZ_SUPPORTS_FILE_ITERATORS
-#define EZ_SUPPORTS_FILE_ITERATORS EZ_ON
+#define EZ_SUPPORTS_FILE_ITERATORS EZ_OFF
 
 /// Getting the stats of a file (modification times etc.) is supported.
 #undef EZ_SUPPORTS_FILE_STATS
 #define EZ_SUPPORTS_FILE_STATS EZ_ON
 
-/// Directory watcher is supported on non UWP platforms.
+/// Directory watcher is supported on non uwp platforms.
 #undef EZ_SUPPORTS_DIRECTORY_WATCHER
-#define EZ_SUPPORTS_DIRECTORY_WATCHER EZ_ON
+#define EZ_SUPPORTS_DIRECTORY_WATCHER EZ_OFF
 
 /// Memory mapping a file is supported.
 #undef EZ_SUPPORTS_MEMORY_MAPPED_FILE
-#define EZ_SUPPORTS_MEMORY_MAPPED_FILE EZ_ON
+#define EZ_SUPPORTS_MEMORY_MAPPED_FILE EZ_OFF
 
 /// Shared memory IPC is supported.
 #undef EZ_SUPPORTS_SHARED_MEMORY
-#define EZ_SUPPORTS_SHARED_MEMORY EZ_ON
+#define EZ_SUPPORTS_SHARED_MEMORY EZ_OFF
 
 /// Whether dynamic plugins (through DLLs loaded/unloaded at runtime) are supported
 #undef EZ_SUPPORTS_DYNAMIC_PLUGINS
@@ -32,7 +32,7 @@
 
 /// Whether applications can access any file (not sandboxed)
 #undef EZ_SUPPORTS_UNRESTRICTED_FILE_ACCESS
-#define EZ_SUPPORTS_UNRESTRICTED_FILE_ACCESS EZ_ON
+#define EZ_SUPPORTS_UNRESTRICTED_FILE_ACCESS EZ_OFF
 
 /// Whether file accesses can be done through paths that do not match exact casing
 #undef EZ_SUPPORTS_CASE_INSENSITIVE_PATHS
@@ -40,7 +40,7 @@
 
 /// Whether starting other processes is supported.
 #undef EZ_SUPPORTS_PROCESSES
-#define EZ_SUPPORTS_PROCESSES EZ_ON
+#define EZ_SUPPORTS_PROCESSES EZ_OFF
 
 // SIMD support
 #undef EZ_SIMD_IMPLEMENTATION
@@ -53,9 +53,3 @@
 #else
 #  error "Unknown architecture."
 #endif
-
-#undef EZ_SUPPORTS_CRASH_DUMPS
-#define EZ_SUPPORTS_CRASH_DUMPS EZ_ON
-
-#undef EZ_SUPPORTS_LONG_PATHS
-#define EZ_SUPPORTS_LONG_PATHS EZ_ON
