6d859bcfee0b72ede4f5c345f385a00e484696cd
C-Core <clemenskern@gmail.com>
2025-04-13 19:38:23 +0200

    more comments

--- Code/Engine/GameEngine/Messages/ExportMessage.h
+++ Code/Engine/GameEngine/Messages/ExportMessage.h
@@ -3,6 +3,8 @@
 #include <Foundation/Communication/Message.h>
 #include <GameEngine/GameEngineDLL.h>
 
+/// \brief Message that is sent to all game objects when a scene or prefab is being exported.
+/// This message can be handled in scripts or custom components to e.g. remove editor only objects/components or save custom data.
 struct EZ_GAMEENGINE_DLL ezMsgExport : public ezMessage
 {
   EZ_DECLARE_MESSAGE_TYPE(ezMsgExport, ezMessage);
