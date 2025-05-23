aebacb493b9378374080fdf9a8137b4bf3f9e274
Jan Krassnigg <jan@krassnigg.de>
2025-05-23 08:18:42 +0200

    Enable by default, without allocation tracking

--- Code/ThirdParty/tracy/init.cmake
+++ Code/ThirdParty/tracy/init.cmake
@@ -1,6 +1,6 @@
 if(EZ_CMAKE_PLATFORM_WINDOWS_DESKTOP)
-    set (EZ_3RDPARTY_TRACY_SUPPORT OFF CACHE BOOL "Whether to add support for profiling the engine with Tracy.")
-	set (EZ_3RDPARTY_TRACY_TRACK_ALLOCATIONS OFF CACHE BOOL "Whether Tracy should track memory allocations.")
+    set (EZ_3RDPARTY_TRACY_SUPPORT ON CACHE BOOL "Whether to add support for profiling the engine with Tracy.")
+	set (EZ_3RDPARTY_TRACY_TRACK_ALLOCATIONS OFF CACHE BOOL "Whether Tracy should track memory allocations. Warning: disables C++ code reload!")
 else()
     # Tracy currently doesn't compile on Linux
     set (EZ_3RDPARTY_TRACY_SUPPORT OFF CACHE BOOL "Whether to add support for profiling the engine with Tracy.")
