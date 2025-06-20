b70cfea0dd8c9ad61ab541ece8f631ef8852b635
C-Core <clemenskern@gmail.com>
2025-06-20 15:52:44 +0200

    Comments

--- Code/EnginePlugins/GameComponentsPlugin/Effects/MeshDecalComponent.h
+++ Code/EnginePlugins/GameComponentsPlugin/Effects/MeshDecalComponent.h
@@ -21,6 +21,12 @@ EZ_DECLARE_REFLECTABLE_TYPE(EZ_GAMECOMPONENTS_DLL, ezMeshDecalDescription);
 
 using ezMeshDecalComponentManager = ezComponentManager<class ezMeshDecalComponent, ezBlockStorageType::Compact>;
 
+/// \brief A component that takes a couple of decal textures, picks a random one for each slot,
+/// adds them to runtime decal atlas and sends a custom data message with the corresponding decal indices.
+///
+/// The decals in this case are not regular projected decals, but rather mesh decals aka floaters.
+/// This requires a special shader to be used which uses the custom data in the instance data to map the UV coordinates to the decal atlas.
+/// See "Data/Samples/Testing Chambers/Materials/MeshDecalMaterial.ezShader" for an example shader.
 class EZ_GAMECOMPONENTS_DLL ezMeshDecalComponent final : public ezComponent
 {
   EZ_DECLARE_COMPONENT_TYPE(ezMeshDecalComponent, ezComponent, ezMeshDecalComponentManager);
