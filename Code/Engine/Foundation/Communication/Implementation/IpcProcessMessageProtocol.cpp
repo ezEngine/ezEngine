CryonicEngine/CryonicEngine

commit 735d5f8511573d68deab146d4b05355c3e323a78

Stutter fix in editor play the game (#1638)
* Stutter fix in editor play the game

Previously the editor rendered in two places:
1. `ezEngineProcessGameApplication::EventHandlerIPC` if a `ezSyncWithProcessMsgToEngine` message was received.
2. At the end of `ezEngineProcessGameApplication::Run`.
This meant that in one frame loop multiple calls to `ezGameApplicationBase::RunOneFrame` could happen depending on what the editor wanted to render and what the running play the game loop wanted to render. This caused stuttering as the frames were very unbalanced in terms of workload.

Now, to ensure that for each `ezSyncWithProcessMsgToEngine` message a frame is rendered, a new feature was added to cancel out of message processing via the new event message field `m_bInterruptMessageProcessing`. For each `ezSyncWithProcessMsgToEngine` message, we set `m_bInterruptMessageProcessing` to true so the message processing is interrupted and we end up at `ezEngineProcessGameApplication::Run` where  all the rendering now takes place. This ensures that rendering does not happen outside of the normal game loop.

## Misc
Fixed a crash in ImGui when closing the play the game window of the sample game project. There was no code path to ensure that at the end of the frame the ImGui frame was ended. So when the play the game window was closed, ImGui was shut down but the destructor asserted that a frame is still in progress.

* Applied code formatting rules

* clang tidy fix

* build fix

---------

Co-authored-by: ezEngine Automation <ezEngineProject@gmail.com>

Code/Engine/Foundation/Communication/Implementation/IpcProcessMessageProtocol.cpp
@@ -17,9 +17,9 @@ ezIpcProcessMessageProtocol::~ezIpcProcessMessageProtocol()
 {
   m_pChannel->SetReceiveCallback({});
 
-  ezDeque<ezUniquePtr<ezProcessMessage>> messages;
-  SwapWorkQueue(messages);
-  messages.Clear();
+  while (ezUniquePtr<ezProcessMessage> msg = PopMessage())
+  {
+  }
 }
 
 bool ezIpcProcessMessageProtocol::Send(ezProcessMessage* pMsg)
@@ -32,21 +32,20 @@ bool ezIpcProcessMessageProtocol::Send(ezProcessMessage* pMsg)
 
 bool ezIpcProcessMessageProtocol::ProcessMessages()
 {
-  ezDeque<ezUniquePtr<ezProcessMessage>> messages;
-  SwapWorkQueue(messages);
-  if (messages.IsEmpty())
+  bool messagesPresent = false;
+
+  while (ezUniquePtr<ezProcessMessage> msg = PopMessage())
   {
-    return false;
+    messagesPresent = true;
+    Event e;
+    e.m_pMessage = msg.Borrow();
+    e.m_bInterruptMessageProcessing = false;
+    m_MessageEvent.Broadcast(e);
+    if (e.m_bInterruptMessageProcessing)
+      break;
   }
 
-  while (!messages.IsEmpty())
-  {
-    ezUniquePtr<ezProcessMessage> msg = std::move(messages.PeekFront());
-    messages.PopFront();
-    m_MessageEvent.Broadcast(msg.Borrow());
-  }
-
-  return true;
+  return messagesPresent;
 }
 
 ezResult ezIpcProcessMessageProtocol::WaitForMessages(ezTime timeout)
@@ -83,11 +82,13 @@ void ezIpcProcessMessageProtocol::EnqueueMessage(ezUniquePtr<ezProcessMessage>&&
   m_IncomingQueue.PushBack(std::move(msg));
 }
 
-void ezIpcProcessMessageProtocol::SwapWorkQueue(ezDeque<ezUniquePtr<ezProcessMessage>>& messages)
+ezUniquePtr<ezProcessMessage> ezIpcProcessMessageProtocol::PopMessage()
 {
-  EZ_ASSERT_DEBUG(messages.IsEmpty(), "Swap target must be empty!");
   EZ_LOCK(m_IncomingQueueMutex);
   if (m_IncomingQueue.IsEmpty())
-    return;
-  messages.Swap(m_IncomingQueue);
+    return {};
+
+  ezUniquePtr<ezProcessMessage> front = std::move(m_IncomingQueue.PeekFront());
+  m_IncomingQueue.PopFront();
+  return front;
 }

Code/Editor/EditorEngineProcess/EngineProcGameApp.cpp
@@ -180,11 +180,12 @@ void ezEngineProcessGameApplication::Run()
     }
   } while (!bPendingOpInProgress && m_uiRedrawCountExecuted == m_uiRedrawCountReceived);
 
-  m_uiRedrawCountExecuted = m_uiRedrawCountReceived;
-
-  // Normally rendering is done in EventHandlerIPC as a response to ezSyncWithProcessMsgToEngine. However, when playing or when pending operations are in progress we need to render even if we didn't receive a draw request.
+  // If the editor enqueues a frame to be rendered, the loop above will break due to m_uiRedrawCountExecuted != m_uiRedrawCountReceived, and we will render a frame.
+  // Alternatively, a pending operations is active at which point we render as fast as we can, ignoring the editor lock step.
+  // Note there are two other cases in which we render a frame: when "FreeGalResources" or "FreeAllResources" messages are sent we must render a frame to clear pending deletions and free memory.
   SUPER::Run();
   ezRenderWorld::ClearMainViews();
+  m_uiRedrawCountExecuted = m_uiRedrawCountReceived;
 }
 
 void ezEngineProcessGameApplication::LogWriter(const ezLoggingEventData& e)
@@ -248,8 +249,8 @@ bool ezEngineProcessGameApplication::ProcessIPCMessages(bool bPendingOpInProgres
       // Only suspend and wait if no more pending ops need to be done.
       m_IPC.WaitForMessages();
     }
-    return true;
   }
+  return true;
 }
 
 void ezEngineProcessGameApplication::SendProjectReadyMessage()
@@ -297,8 +298,9 @@ void ezEngineProcessGameApplication::EventHandlerIPC(const ezEngineProcessCommun
     m_uiRedrawCountReceived = msg.m_uiRedrawCount;
 
     // We must clear the main views after rendering so that if the editor runs in lock step with the engine we don't render a view twice or request update again without rendering being done.
-    RunOneFrame();
-    ezRenderWorld::ClearMainViews();
+    // As multiple ezSyncWithProcessMsgToEngine messages could be enqueued we break out of the message processing loop to render this frame.
+    // Previously re renderer int frame on the stack but this caused stuttering so ezEngineProcessGameApplication::Run is now the only place we render in this class.
+    e.m_bInterruptMessageProcessing = true;
 
     m_IPC.SendMessage(&msg);
     return;

Code/Editor/EditorEngineProcessFramework/IPC/ProcessCommunicationChannel.cpp
@@ -53,8 +53,9 @@ void ezProcessCommunicationChannel::WaitForMessages()
   m_pProtocol->WaitForMessages().IgnoreResult();
 }
 
-void ezProcessCommunicationChannel::MessageFunc(const ezProcessMessage* pMsg)
+void ezProcessCommunicationChannel::MessageFunc(const ezIpcProcessMessageProtocol::Event& msg)
 {
+  const ezProcessMessage* pMsg = msg.m_pMessage;
   const ezRTTI* pRtti = pMsg->GetDynamicRTTI();
 
   if (m_pWaitForMessageType != nullptr && pMsg->GetDynamicRTTI()->IsDerivedFrom(m_pWaitForMessageType))
@@ -79,7 +80,9 @@ void ezProcessCommunicationChannel::MessageFunc(const ezProcessMessage* pMsg)
 
   Event e;
   e.m_pMessage = pMsg;
+  e.m_bInterruptMessageProcessing = msg.m_bInterruptMessageProcessing;
   m_Events.Broadcast(e);
+  msg.m_bInterruptMessageProcessing = e.m_bInterruptMessageProcessing;
 }
 
 ezResult ezProcessCommunicationChannel::WaitForMessage(const ezRTTI* pMessageType, ezTime timeout, WaitForMessageCallback* pMessageCallack)

Code/Editor/EditorEngineProcessFramework/IPC/ProcessCommunicationChannel.h
@@ -2,6 +2,7 @@
 
 #include <EditorEngineProcessFramework/EditorEngineProcessFrameworkDLL.h>
 #include <Foundation/Communication/Event.h>
+#include <Foundation/Communication/IpcProcessMessageProtocol.h>
 #include <Foundation/Time/Time.h>
 #include <Foundation/Types/Delegate.h>
 #include <Foundation/Types/UniquePtr.h>
@@ -32,11 +33,13 @@ public:
   struct Event
   {
     const ezProcessMessage* m_pMessage;
+    // Set to true in a message handler to cancel the ProcessMessages function and return to the caller before all messages have been processed.
+    mutable bool m_bInterruptMessageProcessing = false;
   };
 
   ezEvent<const Event&> m_Events;
 
-  void MessageFunc(const ezProcessMessage* pMsg);
+  void MessageFunc(const ezIpcProcessMessageProtocol::Event& msg);
 
 protected:
   ezUniquePtr<ezIpcProcessMessageProtocol> m_pProtocol;

Code/Engine/Foundation/Communication/IpcProcessMessageProtocol.h
@@ -30,11 +30,17 @@ public:
   ezResult WaitForMessages(ezTime timeout = ezTime::MakeZero());
 
 public:
-  ezEvent<const ezProcessMessage*> m_MessageEvent; ///< Will be sent from thread calling ProcessMessages or WaitForMessages.
+  struct Event
+  {
+    const ezProcessMessage* m_pMessage;
+    // Set to true in a message handler to cancel the ProcessMessages function and return to the caller before all messages have been processed.
+    mutable bool m_bInterruptMessageProcessing = false;
+  };
+  ezEvent<const Event&> m_MessageEvent; ///< Will be sent from thread calling ProcessMessages or WaitForMessages.
 
 private:
   void EnqueueMessage(ezUniquePtr<ezProcessMessage>&& msg);
-  void SwapWorkQueue(ezDeque<ezUniquePtr<ezProcessMessage>>& messages);
+  ezUniquePtr<ezProcessMessage> PopMessage();
   void ReceiveMessageData(ezArrayPtr<const ezUInt8> data);
 
 private:

Code/Engine/GameEngine/DearImgui/DearImgui.h
@@ -15,6 +15,7 @@
 using ezTexture2DResourceHandle = ezTypedResourceHandle<class ezTexture2DResource>;
 
 struct ImGuiContext;
+struct ezGameApplicationExecutionEvent;
 
 using ezImguiConfigFontCallback = ezDelegate<void(ImFontAtlas&)>;
 using ezImguiConfigStyleCallback = ezDelegate<void(ImGuiStyle&)>;
@@ -85,6 +86,7 @@ private:
 
   ImGuiContext* CreateContext();
   void BeginFrame(const ezViewHandle& hView);
+  void GameApplicationEventHandler(const ezGameApplicationExecutionEvent& e);
 
   ezProxyAllocator m_Allocator;
 

Code/Engine/GameEngine/DearImgui/Implementation/DearImgui.cpp
@@ -257,10 +257,13 @@ void ezImgui::Startup(ezImguiConfigFontCallback configFontCallback)
 
   const size_t id = (size_t)m_Textures.GetCount() - 1;
   m_pSharedFontAtlas->TexID = reinterpret_cast<void*>(id);
+
+  ezGameApplicationBase::GetGameApplicationBaseInstance()->m_ExecutionEvents.AddEventHandler(ezMakeDelegate(&ezImgui::GameApplicationEventHandler, this));
 }
 
 void ezImgui::Shutdown()
 {
+  ezGameApplicationBase::GetGameApplicationBaseInstance()->m_ExecutionEvents.RemoveEventHandler(ezMakeDelegate(&ezImgui::GameApplicationEventHandler, this));
   m_Textures.Clear();
 
   m_pSharedFontAtlas = nullptr;
@@ -388,6 +391,14 @@ void ezImgui::BeginFrame(const ezViewHandle& hView)
   ImGui::NewFrame();
 
   m_bImguiWantsInput = cfg.WantCaptureKeyboard || cfg.WantCaptureMouse;
+}
+
+void ezImgui::GameApplicationEventHandler(const ezGameApplicationExecutionEvent& e)
+{
+  if (e.m_Type == ezGameApplicationExecutionEvent::Type::AfterUpdatePlugins)
+  {
+    ImGui::EndFrame();
+  }
 }
 
 #endif

Code/UnitTests/RendererTest/Advanced/AdvancedFeatures.cpp
@@ -898,9 +898,9 @@ ezTestAppRun ezRendererTestAdvancedFeatures::SharedTexture()
   return ezTestAppRun::Continue;
 }
 
-void ezRendererTestAdvancedFeatures::OffscreenProcessMessageFunc(const ezProcessMessage* pMsg)
+void ezRendererTestAdvancedFeatures::OffscreenProcessMessageFunc(const ezIpcProcessMessageProtocol::Event& msg)
 {
-  if (const auto* pAction = ezDynamicCast<const ezOffscreenTest_RenderResponseMsg*>(pMsg))
+  if (const auto* pAction = ezDynamicCast<const ezOffscreenTest_RenderResponseMsg*>(msg.m_pMessage))
   {
     m_uiReceivedTextures++;
     ezStringBuilder sTemp;

ezEngine/ezEngine

commit 723a5607016da3796165996de4b6a69c8f7ee6bf

Fix for Background Asset Transformation does not start
This regression was introduced with #1638 which added the option to interrupt message processing in the queue. This broke the assumption that if `WaitForMessages` was called all messages up to that point would be processed and the `m_IncomingMessages` would only be triggered if new messages arrived. However, as `ezIpcProcessMessageProtocol::WaitForMessages` did not check if there are still messages in the incoming buffer, it would wait for mew messages to arrive before processing the already arrived messages. Due to bad timing when the other side was waiting for a message, this could cause a deadlock between the two sides.  
Fixes #1656.

Code/Engine/Foundation/Communication/Implementation/IpcProcessMessageProtocol.cpp
@@ -50,6 +50,12 @@ bool ezIpcProcessMessageProtocol::ProcessMessages()
 
 ezResult ezIpcProcessMessageProtocol::WaitForMessages(ezTime timeout)
 {
+  // Message processing can be interrupted via the m_bInterruptMessageProcessing flag. Thus, there is no guarantee that the queue is empty at this point. Only wait if the queue is empty.
+  if (ProcessMessages())
+  {
+    return EZ_SUCCESS;
+  }
+
   ezResult res = m_pChannel->WaitForMessages(timeout);
   if (res.Succeeded())
   {