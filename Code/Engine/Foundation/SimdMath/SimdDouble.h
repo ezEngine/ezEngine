8ed112d0c156425416c04bfa71a9166dffa406d1
TrevorCash <cashtrevor@msn.com>
2025-12-03 10:34:17 -0700

    remove another EZ_SIMD_IMPLEMENTATION_AVX and fix include path.

--- Code/Engine/Foundation/SimdMath/SimdDouble.h
+++ Code/Engine/Foundation/SimdMath/SimdDouble.h
@@ -97,7 +97,7 @@ public:
 
 #if EZ_SIMD_IMPLEMENTATION == EZ_SIMD_IMPLEMENTATION_SSE
 #  if EZ_SSE_LEVEL >= EZ_SSE_AVX
-#    include <Foundation/SimdMath/Implementation/SSE/SSEDouble_AVX_inl.h>
+#    include <Foundation/SimdMath/Implementation/AVX/AVXDouble_inl.h>
 #  else
 // Could also redirect to fpu version for the time being
 #    include <Foundation/SimdMath/Implementation/SSE/SSEDouble_inl.h>
