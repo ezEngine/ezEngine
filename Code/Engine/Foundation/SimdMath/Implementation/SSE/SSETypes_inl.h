215aad835cadb871596f3564781c61017437dbe4
TrevorCash <cashtrevor@msn.com>
2025-12-04 12:43:18 -0700

    SSETypes_inl.h

--- Code/Engine/Foundation/SimdMath/Implementation/SSE/SSETypes_inl.h
+++ Code/Engine/Foundation/SimdMath/Implementation/SSE/SSETypes_inl.h
@@ -33,13 +33,17 @@
 namespace ezInternal
 {
 
+
+
+#if EZ_SSE_LEVEL >= EZ_SSE_AVX
+  using QuadDouble = __m256d;
+#else
   struct QuadDouble
   {
     __m128d xy;
     __m128d zw;
   };
-
-
+#endif
 
   using QuadFloat = __m128;
   using QuadBool = __m128;
