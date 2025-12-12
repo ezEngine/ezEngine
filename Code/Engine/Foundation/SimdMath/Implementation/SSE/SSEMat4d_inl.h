5cc85744e43068de9de1dd77685b915d66aa44ff
TrevorCash <cashtrevor@msn.com>
2025-12-11 19:13:22 -0700

    AVX testing wip

--- Code/Engine/Foundation/SimdMath/Implementation/SSE/SSEMat4d_inl.h
+++ Code/Engine/Foundation/SimdMath/Implementation/SSE/SSEMat4d_inl.h
@@ -3,7 +3,11 @@
 EZ_ALWAYS_INLINE void ezSimdMat4d::Transpose()
 {
 #if EZ_SSE_LEVEL >= EZ_SSE_AVX
-  EZ_ASSERT_NOT_IMPLEMENTED
+    _MM_TRANSPOSE4_PD_AVX(
+    m_col0.m_v, 
+    m_col1.m_v, 
+    m_col2.m_v, 
+    m_col3.m_v);
 #else
   _MM_TRANSPOSE4_PD(
     m_col0.m_v.xy, m_col0.m_v.zw, 
--- Code/Engine/Foundation/SimdMath/Implementation/SSE/SSETypes_inl.h
+++ Code/Engine/Foundation/SimdMath/Implementation/SSE/SSETypes_inl.h
@@ -71,33 +71,34 @@ namespace ezInternal
 /// \brief Shuffles doubles in the same manner as _mm_shuffle_ps but for doubles using sse intrinsics on high and low parts.
 EZ_ALWAYS_INLINE void EZ_WIDE_SHUFFLE_SSE(__m128d aLow, __m128d aHigh, __m128d bLow, __m128d bHigh, int imm8, __m128d& outLow, __m128d& outHigh)
 {
-  __m128d sources[2] = {aLow, aHigh};
   int sel0 = imm8 & 3;
   int sel1 = (imm8 >> 2) & 3;
   int sel2 = (imm8 >> 4) & 3;
   int sel3 = (imm8 >> 6) & 3;
 
-  auto get_double = [&](int sel) -> __m128d {
-    int source_idx = sel / 2;
-    int double_idx = sel % 2;
-    __m128d temp0 = _mm_shuffle_pd(sources[source_idx], sources[source_idx], 0);
-    __m128d temp1 = _mm_shuffle_pd(sources[source_idx], sources[source_idx], 3);
-    __m128d mask = _mm_cmpeq_pd(_mm_set1_pd(double_idx), _mm_set1_pd(0));
-    return _mm_or_pd(_mm_andnot_pd(mask, temp1), _mm_and_pd(mask, temp0));
+  __m128d a_sources[2] = {aLow, aHigh};
+  __m128d b_sources[2] = {bLow, bHigh};
+
+  auto get_double = [&](int sel, __m128d (&sources)[2]) -> __m128d {
+    int source_idx = sel >> 1;
+    int double_idx = sel & 1;
+    __m128d src = sources[source_idx];
+    __m128d low_bcast = _mm_shuffle_pd(src, src, 0);
+    __m128d high_bcast = _mm_shuffle_pd(src, src, 3);
+    __m128d mask = _mm_cmpeq_pd(_mm_set1_pd(static_cast<double>(double_idx)), _mm_set1_pd(0.0));
+    return _mm_or_pd(_mm_andnot_pd(mask, high_bcast), _mm_and_pd(mask, low_bcast));
   };
 
-  __m128d d0 = get_double(sel0);
-  __m128d d1 = get_double(sel1);
-  __m128d d2 = get_double(sel2);
-  __m128d d3 = get_double(sel3);
+  __m128d d0 = get_double(sel0, a_sources);
+  __m128d d1 = get_double(sel1, a_sources);
+  __m128d d2 = get_double(sel2, b_sources);
+  __m128d d3 = get_double(sel3, b_sources);
 
-  outLow = _mm_shuffle_pd(d0, d1, 0);
-  outHigh = _mm_shuffle_pd(d2, d3, 0);
+  outLow = _mm_unpacklo_pd(d0, d1);
+  outHigh = _mm_unpacklo_pd(d2, d3);
 }
 
-
-
-/// \brief Shuffles doubles in the same manner as _mm_shuffle_ps but for doubles using avx1 intrinsics on high and low parts.
+/// @brief directly swizzle a 4 double __m256d to the output. AVX1
 EZ_ALWAYS_INLINE void EZ_WIDE_SWIZZLE_AVX1(__m256d a, ezSwizzle::Enum swizzle, __m256d& out)
 {
   /// \todo use direct intrinsics. - remove EZ_WIDE_SHUFFLE_SSE
@@ -109,6 +110,35 @@ EZ_ALWAYS_INLINE void EZ_WIDE_SWIZZLE_AVX1(__m256d a, ezSwizzle::Enum swizzle, _
   out = _mm256_castpd128_pd256(out_lo);
   out = _mm256_insertf128_pd(out, out_hi, 1);
 }
+
+
+/// \brief Shuffles doubles in the same manner as _mm_shuffle_ps but for doubles using avx1 intrinsics on high and low parts.
+EZ_ALWAYS_INLINE void EZ_WIDE_SHUFFLE_AVX1(__m256d a, __m256d b, int imm8, __m256d& out)
+{
+  /// \todo use direct intrinsics. - remove EZ_WIDE_SHUFFLE_SSE
+  __m128d a_lo = _mm256_castpd256_pd128(a);
+  __m128d a_hi = _mm256_extractf128_pd(a, 1);
+  __m128d b_lo = _mm256_castpd256_pd128(b);
+  __m128d b_hi = _mm256_extractf128_pd(b, 1);
+  __m128d out_lo;
+  __m128d out_hi;
+  EZ_WIDE_SHUFFLE_SSE(a_lo, a_hi, b_lo, b_hi, imm8, out_lo, out_hi);
+  out = _mm256_castpd128_pd256(out_lo);
+  out = _mm256_insertf128_pd(out, out_hi, 1);
+}
+
+
+#define _MM_TRANSPOSE4_PD_AVX(row0, row1, row2, row3) \
+do { \
+  __m256d tmp0 = _mm256_unpacklo_pd((row0), (row1)); \
+  __m256d tmp1 = _mm256_unpackhi_pd((row0), (row1)); \
+  __m256d tmp2 = _mm256_unpacklo_pd((row2), (row3)); \
+  __m256d tmp3 = _mm256_unpackhi_pd((row2), (row3)); \
+  (row0) = _mm256_permute2f128_pd(tmp0, tmp2, 0x20); \
+  (row1) = _mm256_permute2f128_pd(tmp0, tmp2, 0x31); \
+  (row2) = _mm256_permute2f128_pd(tmp1, tmp3, 0x20); \
+  (row3) = _mm256_permute2f128_pd(tmp1, tmp3, 0x31); \
+} while (0)
 
 
 #define _MM_TRANSPOSE4_PD(row0lo, row0hi, row1lo, row1hi, row2lo, row2hi, row3lo, row3hi) \
