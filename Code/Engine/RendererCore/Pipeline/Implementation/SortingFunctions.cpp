d02d390b1a9ec517a30149973880423af4596419
C-Core <clemenskern@gmail.com>
2026-01-27 18:02:46 +0100

    ByDepthOffsetOnly sorting function

--- Code/Engine/RendererCore/Pipeline/Implementation/SortingFunctions.cpp
+++ Code/Engine/RendererCore/Pipeline/Implementation/SortingFunctions.cpp
@@ -12,12 +12,19 @@ namespace
     return (uiTypeHash >> 16) ^ (uiTypeHash & 0xFFFF);
   }
 
-  EZ_FORCE_INLINE ezUInt32 CalculateDistance(const ezRenderData* pRenderData, const ezCamera& camera)
+  template <ezUInt32 Bits>
+  EZ_ALWAYS_INLINE ezUInt64 NormalizeDistance(float fDistance, float fMaxDistance)
+  {
+    const float fNormalizedDistance = ezMath::Saturate(fDistance / fMaxDistance);
+    return static_cast<ezUInt64>(fNormalizedDistance * (EZ_BIT(Bits) - 1));
+  }
+
+  template <ezUInt32 Bits>
+  EZ_FORCE_INLINE ezUInt64 CalculateDistance(const ezRenderData* pRenderData, const ezCamera& camera)
   {
     ///\todo far-plane is not enough to normalize distance
     const float fDistance = (camera.GetPosition() - pRenderData->m_vGlobalPosition).GetLength() + pRenderData->m_fSortingDepthOffset;
-    const float fNormalizedDistance = ezMath::Clamp(fDistance / camera.GetFarPlane(), 0.0f, 1.0f);
-    return static_cast<ezUInt32>(fNormalizedDistance * 65535.0f);
+    return NormalizeDistance<Bits>(fDistance, camera.GetFarPlane());
   }
 } // namespace
 
@@ -26,7 +33,7 @@ ezUInt64 ezRenderSortingFunctions::ByRenderDataThenFrontToBack(const ezRenderDat
 {
   const ezUInt64 uiTypeHash = CalculateTypeHash(pRenderData);
   const ezUInt64 uiRenderDataSortingKey64 = pRenderData->m_uiSortingKey;
-  const ezUInt64 uiDistance = CalculateDistance(pRenderData, camera);
+  const ezUInt64 uiDistance = CalculateDistance<16>(pRenderData, camera);
 
   const ezUInt64 uiSortingKey = (uiTypeHash << 48) | (uiRenderDataSortingKey64 << 16) | uiDistance;
   return uiSortingKey;
@@ -37,8 +44,18 @@ ezUInt64 ezRenderSortingFunctions::BackToFrontThenByRenderData(const ezRenderDat
 {
   const ezUInt64 uiTypeHash = CalculateTypeHash(pRenderData);
   const ezUInt64 uiRenderDataSortingKey64 = pRenderData->m_uiSortingKey;
-  const ezUInt64 uiInvDistance = 0xFFFF - CalculateDistance(pRenderData, camera);
+  const ezUInt64 uiInvDistance = 0xFFFF - CalculateDistance<16>(pRenderData, camera);
 
   const ezUInt64 uiSortingKey = (uiInvDistance << 48) | (uiTypeHash << 32) | uiRenderDataSortingKey64;
   return uiSortingKey;
+}
+
+// static
+ezUInt64 ezRenderSortingFunctions::ByDepthOffsetOnly(const ezRenderData* pRenderData, const ezCamera& camera)
+{
+  const float fMidDistance = camera.GetFarPlane() * 0.5f;
+  const float fDistance = fMidDistance + pRenderData->m_fSortingDepthOffset;
+  const ezUInt64 uiDistance = NormalizeDistance<32>(fDistance, camera.GetFarPlane());
+
+  return uiDistance;
 }
--- Code/Engine/RendererCore/Pipeline/SortingFunctions.h
+++ Code/Engine/RendererCore/Pipeline/SortingFunctions.h
@@ -9,13 +9,18 @@
 class EZ_RENDERERCORE_DLL ezRenderSortingFunctions
 {
 public:
-  /// Sorts by render data type first, then by depth front-to-back.
+  /// \brief Sorts by render data type first, then by render data sorting key, then by depth front-to-back.
   ///
   /// Used for opaque geometry to minimize state changes and benefit from early-z rejection.
   static ezUInt64 ByRenderDataThenFrontToBack(const ezRenderData* pRenderData, const ezCamera& camera);
 
-  /// Sorts by depth back-to-front, then by render data type.
+  /// \brief Sorts by depth back-to-front, then by render data type, then by render data sorting key.
   ///
   /// Used for transparent geometry to ensure correct blending order.
   static ezUInt64 BackToFrontThenByRenderData(const ezRenderData* pRenderData, const ezCamera& camera);
+
+  /// \brief Sorts only by the render data's depth offset and ignores position, type and sorting key.
+  ///
+  /// This can be used for special cases like full-screen effects where the render order needs to be fully deterministic.
+  static ezUInt64 ByDepthOffsetOnly(const ezRenderData* pRenderData, const ezCamera& camera);
 };
