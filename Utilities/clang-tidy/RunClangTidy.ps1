e5a33807889d3b86305b8f0ea0c99ae3e675767f
Jan Krassnigg <jan@krassnigg.de>
2026-03-26 09:32:41 +0100

    tidy script fix

--- Utilities/clang-tidy/RunClangTidy.ps1
+++ Utilities/clang-tidy/RunClangTidy.ps1
@@ -58,6 +58,11 @@ if (-not $LlvmInstallDir) {
 $Workspace = (Resolve-Path $Workspace).Path
 
 function New-TemporaryDirectory {
+    if(!$Env:Temp)
+    {
+        Write-Error "Environment variable 'Temp' is not set. Cannot create a temporary directory."
+        exit 1
+    }
     $tempFolderPath = Join-Path $Env:Temp $(New-Guid)
     return (New-Item -Type Directory -Path $tempFolderPath).FullName
 }
@@ -100,6 +105,10 @@ if(!$TempDir)
     $TempDir = New-TemporaryDirectory
     $DeleteTempDir = $true
     Write-Host "Temporary Directory is $TempDir"
+}
+elseif(!(Test-Path $TempDir))
+{
+    New-Item -ItemType Directory -Path $TempDir | Out-Null
 }
 
 $files = @()
